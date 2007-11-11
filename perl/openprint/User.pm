d73498720ccf326fc681f76874dbdffcb071ed61
root <root@www1.(none)>
2007-11-11 11:51:39 -0500

    fix filtering by web_active

--- perl/openprint/User.pm
+++ perl/openprint/User.pm
@@ -324,8 +324,10 @@ sub find {
 		push @values, lc $param{'email'};
 	} # end if
 	if ( exists $param{'web_active'} ) {
+		if ( ! sets::isin( $param{'web_active'}, ['Y','N'] ) ) {
 		$param{'web_active'} = 'N' if $param{'web_active'} == 0;
 		$param{'web_active'} = 'Y' if $param{'web_active'} == 1;
+		} # end if
 		$sql .= ' AND ysnaccountactivation=?';
 		push @values, $param{'web_active'};
 	} # end if
