acbd6aec339347d54dc09ebfd1712ef924df5585
root <root@www2.(none)>
2007-11-29 14:11:14 -0500

    add more information to the stitching breakdown (finished calliper and face trim size) fix case where if the stock is first a sheet, then a roll, that the height would be kept.  Now we both clear it and ignore it.

--- perl/openprint/Estimating/Printing.pm
+++ perl/openprint/Estimating/Printing.pm
@@ -589,6 +589,9 @@ $openprint::log->debug("# of colours: " . @side_one_colours );
 		my $Paper = new openprint::Paper();
 		@$Paper{'cuttable','perfecting','calliper','doublesided','gsm','grade'} = ( 'Y',($$specs{'txtSpecificStockBrand'} =~ /offset/i ? 'Y' : 'N'),@$specs{'txtSpecificStockCalliper','CustomSheetDoubleSided','txtStockGSM','StockGrade'});
 		@$Paper{'width','height','mweight','Price','type','Units','basis_width','basis_height','basis_mweight'} = @$specs{'txtSpecificStockWidth','txtSpecificStockHeight','txtCustomMWeight','CustomStockPrice','StockType','CustomStockPriceUnits','basis_width','basis_height','basis_mweight'};
+		if ( $$specs{'StockType'} eq 'Roll' ) {
+			delete $$Paper{'height'};
+		} # end if
 		$Paper->score_required( $Paper->calliper() > 0.008 );
 		push @Papers, $Paper;
 		@$Paper{'start_width','start_height'} = @$Paper{'width','height'};
