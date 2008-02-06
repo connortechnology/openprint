ff7fd3a4cfc67dd072cd3d4828b1f3593f095a8d
root <root@www2.(none)>
2008-02-06 16:18:31 -0500

    blah

--- perl/openprint/Estimating/Folding.pm
+++ perl/openprint/Estimating/Folding.pm
@@ -237,12 +237,12 @@ sub test_fold {
 			$openprint::log->debug("Fold no good due to imposition " . $I->columns() . "> ".$Equipment->specification($foldtype.'MaximumColumns' ) ) if $debug;
 			return 0;
 		} # end if
-		if ( ( $Equipment->specification($foldtype.'MinimumWidth' ) and $Equipment->specification($foldtype.'MinimumWidth' ) > ( $I->image_orientation() eq 'Vertical' ? $I->image_width() : $I->image_height() ) ) ) {
-			$openprint::log->debug("Fold no good due to Minimum Width " . ($I->image_orientation() eq 'Vertical' ? $$sig_specs{'txtWidth'} : $$sig_specs{'txtHeight'} ) . ' < ' . $Equipment->specification($foldtype.'MinimumWidth' ) ) if $debug;
+		if ( ( $Equipment->specification($foldtype.'MinimumWidth') and $Equipment->specification($foldtype.'MinimumWidth' ) > ( $I->image_orientation() eq 'Vertical' ? $I->image_width() : $I->image_height() ) ) ) {
+			$openprint::log->debug("Fold no good due to Minimum Width " . ($I->image_orientation() eq 'Vertical' ? $I->image_width() : $I->image_height() ) . ' < ' . $Equipment->specification($foldtype.'MinimumWidth' ) ) if $debug;
 			return 0;
 		}
 		if ( ( $Equipment->specification($foldtype.'MaximumWidth' ) and $Equipment->specification($foldtype.'MaximumWidth' ) < ( $I->image_orientation() eq 'Vertical' ? $I->image_width() : $I->image_height() ) ) ) {
-			$openprint::log->debug("Fold no good due to Maximum Width " . ( $I->image_orientation() eq 'Vertical' ? $$sig_specs{'txtWidth'} : $$sig_specs{'txtHeight'} ) . ' > ' . $Equipment->specification($foldtype.'MaximumWidth' ) ) if $debug;
+			$openprint::log->debug("Fold no good due to Maximum Width " . ( $I->image_orientation() eq 'Vertical' ? $I->image_width() : $I->image_height() ) . ' > ' . $Equipment->specification($foldtype.'MaximumWidth' ) ) if $debug;
 			return 0;
 		} # end if
 		if ( ( $Equipment->specification($foldtype.'MinimumHeight' ) and $Equipment->specification($foldtype.'MinimumHeight') > ( $I->image_orientation() eq 'Vertical' ? $I->image_height() : $I->image_width() ) ) ) {
