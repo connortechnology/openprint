fecdcf6f19870f0c038d9362bc00c3699d150232
root <root@pseudo.connortechnology.com>
2008-02-23 00:23:16 -0500

    more

--- perl/openprint/Estimating/ClipSealing.pm
+++ perl/openprint/Estimating/ClipSealing.pm
@@ -7,6 +7,7 @@ require openprint::service;
 
 my @variables = (
 	'SealQuantity','SealType_id',
+	'Equipment1', 'Equipment2', 'Equipment3',
 	'txtQuantity1', 'txtQuantity2', 'txtQuantity3',
 	'txtPrice1', 'txtPrice2', 'txtPrice3',
 );
