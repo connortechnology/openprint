1ff87685ef5007114cd2ee34f41201fdb5ac2a59
root <root@www2.(none)>
2008-03-18 14:25:33 -0400

    fix aditional plates when sheet work, improve printing summary, adding plate changes

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -717,7 +717,9 @@ $openprint::log->debug("Convert Impositions: Desired: $desired_signature_size, S
 		my @imps;
 		my $start = $impo > $desired_signature_size ? $desired_signature_size : $impo;
 		#foreach my $signature_size ( reverse 1 .. $start ) {
-		foreach my $signature_size ( reverse (int($start/2)-1) .. $start ) {
+		my $a = int($start/2)-1;
+$openprint::log->debug( " $a .. $start " );
+		foreach my $signature_size ( reverse $a .. $start ) {
 			next if ! $blocks{$signature_size};
 #Now figure out how to cut up the imposition
 #$openprint::log->debug("Considering sig size: $signature_size") if $debug;
