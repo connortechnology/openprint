9a0abeea45851ac93619164234581ebfe65f71b9
root <root@www2.(none)>
2008-03-18 14:26:28 -0400

    blah

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -5,7 +5,7 @@ use strict;
 
 require openprint::Imposition;
 
-my $debug = 1;
+my $debug = 0;
 
 sub fit {
 	my ( $object_width, $object_height, $space_width, $space_height ) = @_;
@@ -717,8 +717,8 @@ $openprint::log->debug("Convert Impositions: Desired: $desired_signature_size, S
 		my @imps;
 		my $start = $impo > $desired_signature_size ? $desired_signature_size : $impo;
 		#foreach my $signature_size ( reverse 1 .. $start ) {
-		my $a = int($start/2)-1;
-$openprint::log->debug( " $a .. $start " );
+		my $a = int($start/2);
+		$a -= 1 if $a % 2;
 		foreach my $signature_size ( reverse $a .. $start ) {
 			next if ! $blocks{$signature_size};
 #Now figure out how to cut up the imposition
