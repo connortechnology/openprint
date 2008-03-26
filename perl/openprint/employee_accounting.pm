f3eb96140efd0bd82aa9e2e5d406d1e9bd2fa924
root <root@www1.(none)>
2008-03-26 16:13:24 -0400

    send sales order, not invoice

--- perl/openprint/employee_accounting.pm
+++ perl/openprint/employee_accounting.pm
@@ -44,7 +44,7 @@ sub details {
 	my $Order = new openprint::Order( $order_id );
 
 	if ( $param{'btnFunction'} eq 'Send' ) {
-		openprint::order::send_invoice( $r, $log, $dbh, $order_id );
+		openprint::order::send_sales_order( $r, $log, $dbh, $order_id );
 	} elsif ( $param{'btnFunction'} eq 'Delete' ) {
 		my $payment_index = $param{'PaymentIndex'};
 		$payment_index =~ s/\D//g;
