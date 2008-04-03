93ecc356258892a0138716448f2145120a31cabf
root <root@www2.(none)>
2008-04-03 18:21:14 -0400

    rough in PARs

--- perl/openprint/employee_iso.pm
+++ perl/openprint/employee_iso.pm
@@ -9,6 +9,7 @@ use vars qw( %variable %session %param %config $log $dbh );
 *dbh = \$openprint::dbh;
 
 require openprint::CAR;
+require openprint::PAR;
 
 
 sub cars {
@@ -48,6 +49,7 @@ sub _car_view_part3 {
 		$$variable{'error'} .= $variable{'CAR'}->save( \%param );
 	} # end if
 } # end sub _car_view_part3
+
 sub _car_view_part4 {
 	$variable{'CAR'} = new openprint::CAR( $param{'car_id'} );
 	if ( $param{'btnFunction'} eq 'Save' ) {
@@ -67,6 +69,63 @@ sub _car_edit_part3 {
 }
 sub _car_edit_part4 {
 	$variable{'CAR'} = new openprint::CAR( $param{'car_id'} );
+}
+
+sub pars {
+	if ( $param{'btnFunction'} eq 'Delete' ) {
+		foreach my $par_id ( ref $param{'pars'} eq 'ARRAY' ? @{$param{'pars'}} : $param{'pars'} ) {
+			my $PAR = new openprint::PAR( $par_id );
+			$$variable{'error'} .= $PAR->delete();
+		} # end foreach par_id
+	} # end if
+} # end sub pars
+
+sub par {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
+} # end sub view_par
+sub _par_view_part1 {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
+	if ( $param{'btnFunction'} eq 'Save' ) {
+		$param{'issued_on'} = sprintf('%.4d-%.2d-%.2d', @param{'issued_on_year','issued_on_month','issued_on_day'} );
+		$param{'reply_by'} = sprintf('%.4d-%.2d-%.2d', @param{'reply_by_year','reply_by_month','reply_by_day'} );
+		$param{'part1_signed_on'} = sprintf('%.4d-%.2d-%.2d', @param{'part1_signed_on_year','part1_signed_on_month','part1_signed_on_day'} );
+		$$variable{'error'} .= $variable{'PAR'}->save( \%param );
+	} # end if
+} # end sub _par_view_part1
+sub _par_view_part2 {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
+	if ( $param{'btnFunction'} eq 'Save' ) {
+		$param{'part2_signed_on'} = sprintf('%.4d-%.2d-%.2d', @param{'part2_signed_on_year','part2_signed_on_month','part2_signed_on_day'} );
+		$$variable{'error'} .= $variable{'PAR'}->save( \%param );
+	} # end if
+} # end sub _par_view_part2
+sub _par_view_part3 {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
+	if ( $param{'btnFunction'} eq 'Save' ) {
+		$param{'part3_signed_on'} = sprintf('%.4d-%.2d-%.2d', @param{'part3_signed_on_year','part3_signed_on_month','part3_signed_on_day'} );
+		$$variable{'error'} .= $variable{'PAR'}->save( \%param );
+	} # end if
+} # end sub _par_view_part3
+
+sub _par_view_part4 {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
+	if ( $param{'btnFunction'} eq 'Save' ) {
+		$param{'part4_signed_on'} = sprintf('%.4d-%.2d-%.2d', @param{'part4_signed_on_year','part4_signed_on_month','part4_signed_on_day'} );
+		$$variable{'error'} .= $variable{'PAR'}->save( \%param );
+	} # end if
+} # end sub _par_view_part4
+
+sub _par_edit_part1 {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
+}
+sub _par_edit_part2 {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
+}
+sub _par_edit_part3 {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
+}
+sub _par_edit_part4 {
+	$variable{'PAR'} = new openprint::PAR( $param{'par_id'} );
 }
 
 1;
