45f458b4dc08185d6ea48b980ebca278cb4e98d8
root <root@www2.(none)>
2008-04-07 18:53:05 -0400

    prevent W&T on Roll

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -5,7 +5,7 @@ use strict;
 
 require openprint::Imposition;
 
-my $debug = 0;
+my $debug = 1;
 
 sub fit {
 	my ( $object_width, $object_height, $space_width, $space_height ) = @_;
@@ -650,9 +650,11 @@ sub add_imposition {
 			# this is usually evelopes or forms
 			#$log->debug(" ** Creating No Cut Imposition ** ");
 			#push @impositions, {'Imposition' => 1, 'Rows' => 1, 'Cols' => 1 };
+		} elsif ( ($Paper->type() eq 'Roll') and ($Press->specification('W&TonRoll') eq 'N') ) {
+			next if sets::isin( $run_style, ['Work & Turn','Work & Tumble'] );
 		} else {
 			foreach my $i ( calc_setup_object( $project, @$project{'image_width','image_height'}, $Paper, $run_style, $override_grain_direction, $Press ) ) {
-				if ( sets::isin( $run_style, [ 'Work & Turn', 'Work & Tumble']) ) {
+				if ( sets::isin( $run_style, ['Work & Turn','Work & Tumble']) ) {
 					if ( ($versions * 2) > $i->imposition() ) {
 #$log->debug("Nixing imposition because W&T needds 2* versions > imposition");
 						next;
