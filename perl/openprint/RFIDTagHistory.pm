7cc0c6d801e8e6dd50f8bb73df49b16aa1763079
root <root@www2.(none)>
2008-04-23 00:26:01 -0400

    blah

---
+++ perl/openprint/RFIDTagHistory.pm
@@ -0,0 +1,129 @@
+package openprint::RFIDTagHistory;
+@ISA = qw(openprint::Object);
+require openprint::Object;
+use MIME::QuotedPrint;
+
+use strict;
+use openprint ();
+use vars qw(%variable $log $dbh %config %fields %transforms %defaults );
+*variable = \%openprint::variable;
+*log = \$openprint::log;
+*dbh = \$openprint::dbh;
+*config = \%openprint::config;
+
+require sql;
+require ssi;
+require misc;
+
+my $debug = 1;
+
+%fields = (
+	'id'				=>	'id',
+	'rfidtag_id'		=>	'rfidtag_id',
+	'scanner_id'		=>	'scanner_id',
+	'location_id'		=>	'location_id',
+	'updated_on'		=>	'updated_on',
+);
+
+%transforms = (
+);
+
+%defaults = (
+);
+
+# Returns a paper object specified by the parameters
+sub find {
+	my %params = @_;
+	@params{lc keys %params} = @params{keys %params};
+	my @values;
+	my $sql = 'SELECT * FROM RFIDTagHistory WHERE 1>0';
+
+	if ( exists $params{'id'} ) {
+		if ( ref $params{'id'} eq 'ARRAY' ) {
+			$sql .= ' AND id IN ('. join(',', map {'?'} @{$params{'id'}} ) . ')';
+			push @values, @{$params{'id'}};
+		} else {
+			$sql .= ' AND id=?';
+			push @values, $params{'id'};
+		} # end if
+	} # end if
+	if ( exists $params{'rfidtag_id'} ) {
+		if ( ref $params{'rfidtag_id'} eq 'ARRAY' ) {
+			$sql .= ' AND rfidtag_id IN ('. join(',', map {'?'} @{$params{'rfidtag_id'}} ) . ')';
+			push @values, @{$params{'rfidtag_id'}};
+		} else {
+			$sql .= ' AND rfidtag_id=?';
+			push @values, $params{'rfidtag_id'};
+		} # end if
+	} # end if
+	$sql .= " ORDER BY $params{'order'}" if $params{'order'};
+	$sql .= " ORDER BY $params{'order_by'}" if $params{'order_by'};
+
+	my $data = $openprint::dbh->selectall_arrayref( $sql, { Slice => {} }, @values );
+	if ( ! $data ) {
+		$openprint::log->debug("Error loading RFIDTagHistory SQL($sql)" . DBI->errstr );
+	} elsif ( ! @$data ) {
+		$openprint::log->debug('No RFIDTagHistory loaded (' . $sql . ") (@values)" );
+	} elsif ( $debug ) {
+		$openprint::log->debug("Debug loaded RFIDTagHistory ($sql) (@values) records:" . @$data );
+	} # end if
+	return map { new openprint::RFIDTagHistory( $_->{id}, $_ ) } @$data;
+} # end sub find
+
+sub load {
+	my ( $self, $data ) = @_;
+	if ( ! $data ) {
+		$data = $openprint::dbh->selectrow_hashref( q{SELECT * FROM RFIDTagHistory WHERE id=?}, {}, $$self{'id'} );
+	} # end if
+	@$self{keys %$data} = @$data{keys %$data};
+} # end sub load
+
+sub save {
+	my ( $self, $hash ) = @_;
+
+	if ( $hash ) {
+		$self->set( $hash );
+	} # end if
+	
+	my $ac = sql::start_transaction( $openprint::dbh );
+	if ( ! $$self{'id'} ) {
+		@$self{'id'} = sql::execute( undef, undef, q{SELECT nextval('rfidtaghistory_id_seq')} );
+
+		if ( my $error = sql::insert( undef, undef, 'RFIDTagHistory', [map { $_, $$self{$_} } keys %fields ] ) ) {
+			$$self{'id'} = undef;
+			sql::end_transaction( $openprint::dbh, $ac );
+			return $error;
+		} # end if
+
+    } else {
+		if ( my $error = sql::update( undef, undef, 'RFIDTagHistory', ['id=?', $$self{id}], [map { $_, $$self{$_} } keys %fields ] ) ) {
+			sql::end_transaction( $openprint::dbh, $ac );
+			return $error;
+		} # end if
+    } # end if
+
+	sql::end_transaction( $openprint::dbh, $ac );
+	$self->load();
+	return;
+} # end sub save
+
+sub delete {
+    my $self = shift;
+    my $ac = sql::start_transaction( );
+    sql::execute( undef, undef, q{DELETE FROM RFIDTagHistory WHERE id=?}, $$self{'id'} );
+    sql::end_transaction( undef, $ac );
+} # end sub delete
+
+sub Location {
+	my ( $self ) = @_;
+	return new openprint::Location( $$self{'location_id'} );
+} # end sub Location
+
+sub Scanner {
+	my ( $self ) = @_;
+	return new openprint::RFIDScanner( $$self{'scanner_id'} );
+} # end sub Scanner
+	
+
+1;
+__END__
