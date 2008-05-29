b6d02e8814650b3ec95aa69f4596b5c8f6889092
root <root@www1.(none)>
2008-05-29 11:08:20 -0400

    fix assistant_ids saving

--- perl/openprint/administrator_managerial.pm
+++ perl/openprint/administrator_managerial.pm
@@ -144,8 +144,8 @@ sub user_profiles {
 			return misc::error( $log, $dbh, $variable, 'User already exists.', "There is already a user with the specified email address.  Please try another.");
 		} # end if
 
-		$openprint::param{'assistant_ids'} = '' if ! exists $openprint::param{'assistant_ids'};
-		$openprint::param{'csr_ids'} = '' if ! exists $openprint::param{'csr_ids'};
+		#$openprint::param{'assistant_ids'} = '' if ! exists $openprint::param{'assistant_ids'};
+		#$openprint::param{'csr_ids'} = '' if ! exists $openprint::param{'csr_ids'};
 		my $error = $User->save( \%openprint::param );
 
 		if ( $error ) {
