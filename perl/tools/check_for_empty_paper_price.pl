bf80bd7de5354cb9460aff2d40866f7b4ca0d761
root <root@www1.internal.point-one.com>
2008-07-08 16:21:15 -0400

    blah

---
+++ perl/tools/check_for_empty_paper_price.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+use lib '/etc/apache2/lib/perl';
+use strict;
+
+require sql;
+require logger;
+require openprint::Object;
+require openprint::Company;
+require openprint::User;
+require openprint::Project;
+require openprint::Quote;
+require openprint::Order;
+
+use openprint ();
+use vars qw( $log $dbh );
+*log = \$openprint::log;
+*dbh = \$openprint::dbh;
+
+$log = new logger( 'warn' );
+
+$openprint::Object::no_cache = 1;
+$dbh = sql::open_sql( $log, ('database'=>$ARGV[0], 'driver'=>'Pg','login'=>$ARGV[1], 'password'=>$ARGV[2]) );
+	
+foreach my $Paper ( openprint::Paper::find() ) {
+	next if ! $Paper->prices();
+	foreach my $Pricelist ( openprint::Pricelist::find() ) {
+		if ( ! openprint::PaperPrice::find('Paper'=>$Paper, 'Pricelist'=>$Pricelist) ) {
+			$log->warn ( 'Paper ' . $Paper->to_string() . ' does not have a price for pricelist : ' . $Pricelist->name();
+		} # end if
+	} # end foreach
+} # end foreach Paper
+$dbh->disconnect();
+1;
+__END__
