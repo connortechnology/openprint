1fab3d0e6748d9548e50319a8e227334cbed21c9
root <root@www2.(none)>
2008-07-15 13:17:30 -0400

    UI improvements, add Today button

--- www/public_html/javascripts/form_utilities.js
+++ www/public_html/javascripts/form_utilities.js
@@ -742,3 +742,9 @@ function textarea_resize( element ) {
 	element.rows = countLines(element.value,element.cols);
 } // end function textarea_resize
 
+function set_today( e_y, e_m, e_d ) {
+	var d = new Date();
+	ddm_select_by_value( e_y, d.getYear() );
+	ddm_select_by_value( e_m, d.getMonth()+1 );
+	ddm_select_by_value( e_d, d.getDate() );
+} // end function set_today
