60d5be1659e3ec6ad1d99089df83793df783f211
root <root@www2.(none)>
2008-07-17 12:30:02 -0400

    add 96pg option

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -702,6 +702,7 @@ $openprint::log->debug("Convert Impositions: Desired: $desired_signature_size, S
 			10	=>	[ [5,2], [2,5],[3,4],[4,3] ],
 			12	=>	[ [3,4], [4,3], [6,2],[2,6] ],
 			16	=>	[ [4,4] ],
+			24	=>	[ [4,6] ],
 			);
 	if ( $spread_size == 2 ) {
 			$blocks{11}	=	[ ];
