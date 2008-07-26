222bbf3635baa2aa8a4976537180188361d6c1c3
root <root@pseudo.connortechnology.com>
2008-07-25 21:58:52 -0400

    blah

---
+++ perl/openprint/StockMaterial.pm
@@ -0,0 +1,94 @@
+package openprint::StockMaterial;
+@ISA = qw(openprint::Object);
+
+use strict;
+
+require sql;
+
+use vars qw( %fields %transforms %defaults );
+%fields = ( 'name'=>'name' );
+%transforms = ();
+%defaults = ();
+
+sub find {
+	my %params = @_;
+
+	my $sql = 'SELECT * FROM StockMaterials WHERE 1>0';
+	my @values;
+
+	if ( $params{'papers'} ) {
+		$sql .= ' AND id IN (?)';
+		push @values, [map { $_->material_id(); } @{$params{'papers'}}];
+	} # end if
+	if ( $params{'project_type'} ) {
+		$sql .= ' AND id IN (SELECT DISTINCT material_id FROM papers WHERE id IN (SELECT lngPaperIndex FROM Paper_Recommendations WHERE lngprojecttypeindex=?))';
+		push @values, $params{'project_type'}->id();
+	} # end if
+	$sql .= " ORDER BY $params{'order'}" if $params{'order'};
+	my $data = $openprint::dbh->selectall_arrayref( $sql, {Slice=>{}}, @values );
+	if ( ! $data ) {
+		$openprint::log->debug("openprint::StockMaterial::find( $sql)" . $openprint::dbh->errstr);
+	} else {
+		return map { new openprint::StockMaterial( $_->{id}, $_ ); } @$data;
+	} # end if
+} # end sub find
+
+sub load {
+	my ( $self, $data ) = @_;
+
+	if ( (! $data) and $$self{'id'} ) {
+		$data = $openprint::dbh->selectrow_hashref( 'SELECT * FROM StockMaterials WHERE id=?', {}, $$self{'id'} );
+		if ( ! $data ) { $openprint::log->debug($openprint::dbh->errstr ); }
+	} # end if
+	@$self{qw/id name/} = @$data{qw/id name/};
+
+} # end sub load
+
+sub delete {
+	my $self = shift;
+    sql::execute( undef, undef, q{DELETE FROM StockMaterials WHERE id=?}, $$self{'id'} );
+} # end sub delete
+
+sub save {
+	my ( $self, $param ) = @_;
+
+	if ( $param ) {
+		$self->set( $param );
+	} # end if
+
+	my %sql;
+	foreach my $k ( keys %fields ) {
+		$sql{$k} = $$self{$k};
+	} # end foreach
+
+	my $ac = sql::start_transaction( $openprint::dbh );
+	if ( ! $$self{'id'} ) {
+		@$self{'id'} = sql::execute( undef, undef, q{SELECT nextval('stockmaterials_id_seq')});
+		$sql{'id'} = $$self{'id'};
+		if ( my $error = sql::insert( undef, undef, 'StockMaterials', \%sql ) ) {
+			sql::end_transaction( $openprint::dbh, $ac );
+			return $error;
+		} # end if
+	} else {
+		if ( my $error = sql::update( undef, undef, 'StockMaterials', ['id=?', $$self{'id'}], \%sql ) ) {
+			sql::end_transaction( $openprint::dbh, $ac );
+			return $error;
+		} # end if
+	} # end if
+	sql::end_transaction( $openprint::dbh, $ac );
+	$self->load();
+	return;
+} # end sub save
+
+sub copy {
+	my $self = shift;
+	my $new = new openprint::StockMaterial();
+	@$new{keys %$self} = @$self{keys %$self};
+	$$new{'id'} = undef;
+	return $new;
+} # end sub
+
+1;
+
+__END__
+~       
---
+++ perl/openprint/sql/StockMaterials.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS StockMaterials;
+CREATE TABLE StockMaterials (
+    id  SERIAL NOT NULL,
+    name    TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
