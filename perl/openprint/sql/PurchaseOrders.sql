48ba9947da3d70688f8913bf4bc628bd6938a7c0
root <root@www2.(none)>
2008-08-20 18:15:52 -0400

    blah

---
+++ perl/openprint/sql/PurchaseOrders.sql
@@ -0,0 +1,16 @@
+
+DROP TABLE IF EXISTS  PurchaseOrders;
+CREATE TABLE PurchaseOrders (
+	id	SERIAL NOT NULL,
+	currency_id	INTEGER NOT NULL, FOREIGN KEY (currency_id) REFERENCES Currencies (id),
+	supplier_id	INTEGER,
+	total		float,
+	created_by	INTEGER NOT NULL, FOREIGN KEY (created_by) REFERENCES Users (index),
+	created_on	TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
+	updated_on	TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
+	delivered_on	TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
+	deleted		BOOLEAN NOT NULL default false,
+	shipping_terms	TEXT,
+	shipping_method	TEXT,
+	PRIMARY KEY (id)
+);
