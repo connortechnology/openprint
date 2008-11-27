130fc0854f8fc5dc090b6d747f08f09c0f632e5f
root <root@www2.(none)>
2008-11-27 11:28:59 -0500

    remove debug line

--- perl/openprint/PurchaseOrder.pm
+++ perl/openprint/PurchaseOrder.pm
@@ -347,7 +347,6 @@ sub federaltax_charge {
 #} # end if
 		$$self{'federaltax_charge'} = 1;
 	} # end if
-$log->debug("federaltax_charge: $$self{'federaltax_charge'}");
 	return $$self{'federaltax_charge'};
 } # end sub federaltax_charge
 
