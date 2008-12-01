0949e891610a7803f7de5737434bdfaffceb879d
root <root@www2.(none)>
2008-12-01 00:02:23 -0500

    add 56pg sig

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -701,6 +701,7 @@ $openprint::log->debug("Convert Impositions: Desired: $desired_signature_size, S
 			9	=>	[ [3,3] ],
 			10	=>	[ [5,2], [2,5],[3,4],[4,3] ],
 			12	=>	[ [3,4], [4,3], [6,2],[2,6] ],
+			14	=>	[ [2,7], [7,2] ],
 			16	=>	[ [4,4] ],
 			24	=>	[ [4,6] ],
 			);
