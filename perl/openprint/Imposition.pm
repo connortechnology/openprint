ce69679e3adb8ab99d246b39f75359301ff534c7
root <root@www2.(none)>
2009-01-12 00:34:54 -0500

    fix grain direction

--- perl/openprint/Imposition.pm
+++ perl/openprint/Imposition.pm
@@ -271,6 +271,24 @@ sub pages {
 	return $$self{'spreads'} * $$self{'spread_size'};
 }
 
+sub grain_direction {
+    my $self = shift;
+    if ( $$self{'rotate_sheet'} ) {
+        if ( $$self{'image_orientation'} eq 'Vertical' ) {
+            return 'width';
+        } else {
+            return 'height';
+        } # end if
+    } else {
+        if ( $$self{'image_orientation'} eq 'Vertical' ) {
+            return 'height';
+        } else {
+            return 'width';
+        } # end if
+    } # end if
+} # end sub grain_direction
+
+
 1;
 
 __END__
