a7484b70192f96957e66edb3e0a8d32862834f16
root <root@www2.(none)>
2009-01-20 16:41:12 -0500

    updates, add misc::seconds_to_pretty_interval

--- perl/misc.pm
+++ perl/misc.pm
@@ -237,6 +237,38 @@ sub interval_to_seconds {
     return ($h*3600) + ($m*60) + $s;
 } # end sub interval_to_seconds
 
+sub seconds_to_pretty_interval {
+    my ( $seconds ) = @_;
+    my $string;
+    my $years = int($seconds / ( 60 * 60 * 24 * 365 ));
+    my $remainder = $seconds % ( 60*60*24*365 );
+    $string .= sprintf('%dy', $years) if $years;
+    return $string if ! $remainder;
+
+    my $days = int ( $remainder / ( 60* 60 * 24 ) );
+    $remainder = $remainder % ( 60 * 60 * 24 );
+    if ( sets::isin( $days, [ 28,29,30,31 ] ) ) {
+        $string .= '1 month';
+    } elsif ( $days ) {
+        $string .= sprintf('%dd', $days );
+    } # end if
+    return $string if ! $remainder;
+
+    my $hours = int( $remainder / (60*60) );
+    $remainder = $remainder % ( 60*60 );
+    my $minutes = int ( $remainder / 60 );
+    $remainder = $remainder % 60;
+
+    if ( $remainder ) {
+        $string .= sprintf('%d:%.2d:%.2d', $hours, $minutes, $remainder );
+    } else {
+        $string .= sprintf('%d:%.2d', $hours, $minutes );
+    } # end if
+    return $string;
+
+} # end sub seconds_to_pretty_interval
+
+
 sub CommaFormatted {
     my $delimiter = ','; # replace comma if desired
     my($n,$d) = split /\./,shift,2;
--- perl/openprint/Project.pm
+++ perl/openprint/Project.pm
@@ -996,6 +996,9 @@ sub status_change {
 			openprint::service::status( $$self{'id'}, $_, 'Complete' );
 		} # end foreach signature
         sql::execute( undef, undef, q{DELETE FROM Schedule WHERE ProjectIndex=?}, $self->id() );
+		foreach my $PA ( openprint::PaperAllocation::find('project_id'=>$$self{'id'}) ) {
+			$PA->delete();
+		} # end foreach AP
 
 	} elsif ( sets::isin( $new_status, ['Bindery Complete' ] ) ) {
 		foreach my $s_id ( openprint::print_project::get_services_in_category( $openprint::log, $openprint::dbh, $$self{'id'}, 'Bindery' ) ) {
@@ -1004,6 +1007,9 @@ sub status_change {
 		sql::execute( undef, undef, q{DELETE FROM Schedule WHERE ProjectIndex=?}, $$self{'id'} );
 		sql::execute( undef, undef, q{DELETE FROM Bindery_Schedule WHERE ProjectIndex=?}, $$self{'id'} );
 		$self->update_status();
+		foreach my $PA ( openprint::PaperAllocation::find('project_id'=>$$self{'id'}) ) {
+			$PA->delete();
+		} # end foreach AP
 
 	} elsif ( sets::isin( $new_status, ['Shipped','Picked Up', 'Complete'] ) ) {
 		sql::update( undef, undef, 'tbl_Project_Contents', ["lngProjectIndex=? AND strStatus != ''", $$self{id}], 'strStatus', 'Complete' );
@@ -1011,6 +1017,9 @@ sub status_change {
 		sql::execute( undef, undef, q{DELETE FROM Schedule WHERE ProjectIndex=?}, $$self{'id'} );
 		sql::execute( undef, undef, q{DELETE FROM Bindery_Schedule WHERE ProjectIndex=?}, $$self{'id'} );
 		$self->status($new_status);
+		foreach my $PA ( openprint::PaperAllocation::find('project_id'=>$$self{'id'}) ) {
+			$PA->delete();
+		} # end foreach AP
 	} # end if
 	$self->save();
 	$self->Order()->update_status();
