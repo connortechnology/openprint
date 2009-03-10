f4904d139778e62ddbaabc1d0f0709bcfe07ab8b
root <root@www1.internal.point-one.com>
2009-03-10 15:24:30 -0400

    remove debugging

--- perl/openprint/cron/MergePPF.pl
+++ perl/openprint/cron/MergePPF.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 use lib qw( /etc/apache2/lib/perl );
 use Linux::Inotify2;
 
@@ -21,7 +21,7 @@ use vars qw( $log $dbh %config );
 *config = \%openprint::config;
 
 $log = logger->new();
-$log->{level} = 'debug';
+$log->{level} = 'warn';
 
 my $source_path = $ARGV[0];
 my $dest_path = $ARGV[1];
