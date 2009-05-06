316a60da97de1e7b05c5f6005ba760a328df5a2c
root <root@www5.internal.point-one.com>
2009-05-06 13:01:40 -0400

    blah

--- perl/openprint/cron/PPF_Monitor.pl
+++ perl/openprint/cron/PPF_Monitor.pl
@@ -65,7 +65,7 @@ if ( ! @Equipment ) {
 } # end if
 foreach my $Equipment ( @Equipment ) {
 	my @filenames;
-	if ( ! open(S, "> $$Equipment{cip3_in}/lock.lck") ) {
+	if ( ! open(S, "> $$Equipment{cip3_in}/.lock.lck") ) {
 		$log->error("Unable to open semaphore\n");
 		next;
 	} # end if
