1882c04a4d26699881d9d49df2bde4fd1ceb5a24
root <root@pseudo.connortechnology.com>
2009-06-19 12:48:22 -0400

    blah

---
+++ perl/openprint/sql/Equipment_Shifts.sql
@@ -0,0 +1,12 @@
+
+DROP TABLE IF EXISTS Equipment_Shifts;
+CREATE TABLE Equipment_Shifts (
+	id			SERIAL,
+	equipment_id	INTEGER NOT NULL, FOREIGN KEY (equipment_id) REFERENCES tbl_Equipment (lngIndex),
+	starttime	time	NOT NULL,
+	duration	interval	NOT NULL,
+	name		TEXT	NOT NULL,
+	PRIMARY KEY (id)
+);
+CREATE INDEX Equipment_Shifts_idx on Equipment_Shifts (equipment_id, starttime );
+
