384abade9a76d6011f3c7aa8317d7d84ebf99485
root <root@www2.internal.point-one.com>
2009-06-19 16:57:13 -0400

    speed up highlight_paper by not looking at if it's available or not

--- perl/openprint/employee_inventory.pm
+++ perl/openprint/employee_inventory.pm
@@ -1045,7 +1045,7 @@ sub highlight_paper {
 	foreach my $P ( openprint::Paper::find(
 				'in_stock_start'	=> 1,
 				) ) {
-		next if $P->in_stock() - $P->allocated() <= 0;
+		#next if $P->available() <= 0;
 		if ( $param{Manufacturer} and ($P->manufacturer_id() != $param{Manufacturer} ) ) {
 			push @results, $P->id().'~';
 			next;
