648018b9a76f148d0f2b8bc997af13f9a3d8e887
root <root@www2.internal.point-one.com>
2009-06-19 09:46:33 -0400

    fix bump_job expecting project_id and service_id in params, since now we just pass the job_id. Fix display of stock_details when docket is empty (causes runaway firefox).

--- perl/openprint/employee_production.pm
+++ perl/openprint/employee_production.pm
@@ -24,6 +24,7 @@ require openprint::PaperInventory;
 require openprint::ProductionFeedback;
 require openprint::Shift;
 require openprint::Equipment_Shift;
+require openprint::ScheduledJob;
 
 use vars qw( $r $log $dbh %variable %param %session %config );
 *r = \$openprint::r;
@@ -1028,7 +1029,7 @@ sub _stock_checkout {
 } # end sub _stock_checkout
 
 sub _bump_job {
-	$variable{'Project'} = new openprint::Project( $param{'project_id'} );
+	$variable{'Job'} = new openprint::ScheduledJob( $param{'id'} );
 } # end sub _bump_job
 
 sub _pending_approved {
