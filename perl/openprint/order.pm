0c49540fd3da622a98c34c3c806ccf2e74baee82
root <root@www2.internal.point-one.com>
2009-06-25 09:20:27 -0400

    fix some make_order_from_order semantics

--- perl/openprint/order.pm
+++ perl/openprint/order.pm
@@ -425,6 +425,7 @@ sub information {
 	} elsif ( $openprint::param{'btnFunction'} eq 'New Order' ) {
 		# Re order situation
 		$order_id = make_order_from_order( $log, $dbh, $cookie, $order_id, $variable );
+		return if ! $order_id;
 	} elsif ( $openprint::param{'btnFunction'} eq 'ReOpen' ) {
 		delete_unfinished_orders( $log, $dbh, $cookie );
 		if ( $order_id = $openprint::param{'OrderID'} ) {
@@ -1313,12 +1314,14 @@ sub make_order_from_order {
 	my ( $log, $dbh, $cookie, $src_order_id, $variable ) = @_;
 
 	my $SRC_Order = new openprint::Order( $src_order_id );
-	return if check_credit( $log, $dbh, $variable, $SRC_Order->total() );
+	return 0 if check_credit( $log, $dbh, $variable, $SRC_Order->total() );
 
 	if ( $SRC_Order->status() eq '' ) {
-		return misc::error( $log, $dbh, $variable, 'Can\'t re-order.', 'Order does not exist.' );
+		misc::error( $log, $dbh, $variable, 'Can\'t re-order.', 'Order does not exist.' );
+		return 0;
 	} elsif ( ! sets::isin( $SRC_Order->status(), 'Complete', 'Paid',	'Shipped', 'Waiting For Pickup', 'Picked Up' ) ) {
-		return misc::error( $log, $dbh, $variable, 'Can\'t re-order.', 'The given order is not complete.' );
+		misc::error( $log, $dbh, $variable, 'Can\'t re-order.', 'The given order is not complete.' );
+		return 0;
 	} else {
 		# this goes before get_order_id so that we re-use orderids
 		delete_unfinished_orders( $log, $dbh, $cookie );
@@ -1334,6 +1337,7 @@ sub make_order_from_order {
 				$NewProduct->save();
 			} # end foreach
 		} # end if
+		return $order_id;
 	} # end if
 	return 0;
 } # end sub make_order_from_order
@@ -1347,6 +1351,7 @@ sub quantity_select_display {
 	if ( $openprint::param{'btnFunction'} eq 'New Order' ) {
 		# Re order situation
 		my $order_id = make_order_from_order( $log, $dbh, $cookie, $openprint::param{'hiddenOrderID'}, $variable );
+		return if ! $order_id;
 	} elsif ( $openprint::param{'btnFunction'} eq 'ReOpen' ) {
 		delete_unfinished_orders( $log, $dbh, $cookie );
 		if ( $order_id = $openprint::param{'OrderID'} ) {
