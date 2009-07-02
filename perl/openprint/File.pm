f3ce007ff0d5378b159ddd97dc552450f9ce018d
root <root@topknotch.com>
2009-07-02 14:17:40 -0400

    improve/update file uploading

--- perl/openprint/File.pm
+++ perl/openprint/File.pm
@@ -2,6 +2,28 @@ package openprint::File;
 @ISA = qw( openprint::Object );
 use strict;
 
+use openprint ();
+use vars qw( $log $dbh $table $serial %fields %transforms %defaults );
+
+*log = \$openprint::log;
+*dbh = \$openprint::dbh;
+
+$table = 'project_files';
+$serial = 'project_files_id_seq';
+%fields = (
+		'project_id'	=>	'project_id',
+		'filename'		=>	'filename',
+		'description'	=>	'description',
+		'id'			=>	'id',
+		'upload_id'		=>	'upload_id',
+		'size'			=>	'size',
+		'deleted'		=>	'deleted',
+		);
+%transforms = (
+);
+%defaults = (
+);
+
 my $debug = 1;
 
 sub find {
@@ -16,38 +38,39 @@ sub find {
 		$sql .= q{ AND project_id=?};
 		push @values, $params{'project_id'};
 	} # end if
+	if ( $params{'filename'} ) {
+		$sql .= q{ AND filename=?};
+		push @values, $params{'filename'};
+	} # end if
 
+	if ( exists $params{'deleted'} ) {
+		if ( ref $params{'deleted'} eq 'ARRAY' ) {
+			$sql .= ' AND (deleted IS NULL OR deleted IN (' . join(',', map {'?'} @{$params{'deleted'}}) . '))';
+			push @values, @{$params{'deleted'}};
+		} else {
+			$sql .= ' AND deleted=?';
+			push @values, $params{'deleted'};
+		} # end if
+	} else {
+		$sql .= ' AND (deleted=? OR deleted IS NULL)';
+		push @values, 0;
+	} # end if
 	$sql .= " ORDER BY $params{'order'}" if ( $params{'order'} );
 	$sql .= " LIMIT $params{'limit'}" if ( $params{'limit'} );
-	my $data = $openprint::dbh->selectall_arrayref( $sql, {Slice=>{}}, @values );
+	my $data = $dbh->selectall_arrayref( $sql, {Slice=>{}}, @values );
 	if ( ! $data ) {
-		$openprint::log->error("Error loading File: ($sql) (@values)");
+		$log->error("Error loading File: ($sql) (@values)");
 		return;
 	} elsif ( $debug ) {
-		$openprint::log->debug("Loading File: ($sql) (@values) (".@$data.')');
+		$log->debug("Loading File: ($sql) (@values) (".@$data.')');
 	} # end if
 	return map { new openprint::File( $_->{id}, $_ ); } @$data;
 } # end sub find
 
-sub load {
-    my ( $self, $data ) = @_;
-    if ( ! $data ) {
-        $data = $openprint::dbh->selectrow_hashref( 'SELECT * FROM Project_Files WHERE id=?', {}, $$self{id} );
-    } # end if
-	@$self{keys %$data} = @$data{keys %$data};
-} # end sub load
-
-sub save {
-	my $self = shift;
-	my %sql = (
-		);
-	if ( ! $$self{'id'} ) {
-		@$self{'id'} = sql::execute( undef, undef, q{SELECT nextval('Upload_id_seq')} );
-		sql::insert( $openprint::log, $openprint::dbh, 'Project_Files', 'id', $$self{'id'}, %sql );
-	} else {
-		sql::update( $openprint::log, $openprint::dbh, 'Project_Files', ['id=?', $$self{'id'}], %sql );
-	} # end if
-} # end sub save
+sub size_text {
+	my ( $self ) = @_;
+	return misc::format_bytes( $$self{'size'} );
+} #end sub size_text
 
 1;
 __END__
