44c6908bbaf935c27d94bf5169902f2d6967d544
root <root@topknotch.com>
2009-08-19 10:20:33 -0400

    blah

--- perl/openprint/Estimating/Collating.pm
+++ perl/openprint/Estimating/Collating.pm
@@ -179,7 +179,7 @@ $log->debug("COLLATING!!!!!!!!!!!!!!!!!!");
 sub display {
     my ( $log, $dbh, $variable, $project_index, $service_index ) = @_;
 
-	my @equipment = openprint::Equipment::find( 'Specifications' => {'Collating Capable'=>'Y'}, 'UseInEstimating'=>'Y','order'=>'strName');
+	my @equipment = openprint::Equipment::find( 'Specifications' => {'Collating Capable'=>['Y','When Printing']}, 'UseInEstimating'=>'Y','order'=>'strName');
 	foreach my $qty_index ( 1 .. 3 ) {	
 		$$variable{'ddmEquipment'.$qty_index} = ssi::make_drop_down( [ map { $_->id(), $_->name() } @equipment ], $$variable{'ddmEquipment'.$qty_index} );
 	} # end foreach qty_index
