837ea30c6c9b7d548ccba6e46faf8412a081eb6c
root <root@topknotch.com>
2009-10-07 15:20:13 -0400

    blah

--- perl/openprint/content_prin.pm
+++ perl/openprint/content_prin.pm
@@ -5,7 +5,7 @@ require openprint::project;
 
 sub _breakdown {
 	my ( $r, $log, $dbh, $variable ) = @_;
-openprint::project::view( $log, $dbh, $variable, $openprint::param{'project_id'} );
+	openprint::project::view( $log, $dbh, $variable, $openprint::param{'project_id'} ) if $openprint::param{'project_id'};
 }
 
 sub prin_broc {
