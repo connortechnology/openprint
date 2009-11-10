9cc17e7da9441207a3f69e58492dfd8db10d3719
root <root@topknotch.com>
2009-11-10 08:30:30 -0500

    when deleting an order, delete ordered_products as well

--- perl/openprint/Order.pm
+++ perl/openprint/Order.pm
@@ -220,6 +220,7 @@ sub delete {
 	sql::execute( $log, $dbh, q{DELETE FROM Schedule WHERE ProjectIndex IN ( SELECT lngProjectIndex FROM Order_Contents WHERE OrderIndex=?)}, $$self{'id'} );
 	sql::execute( $log, $dbh, q{DELETE FROM Order_Log WHERE order_id=?}, $$self{'id'} );
 	sql::execute( $log, $dbh, q{DELETE FROM Order_Contents WHERE OrderIndex=?}, $$self{'id'} );
+	sql::execute( $log, $dbh, q{DELETE FROM Ordered_Products WHERE order_id=?}, $$self{'id'} );
 	sql::update( undef, undef, 'tbl_Projects', [ 'order_id=?', $$self{'id'}], [ 'order_id', undef ] );
 	sql::update( undef, undef, 'payments', [ 'order_id=?', $$self{'id'}], [ 'order_id', undef ] );
 	sql::execute( $log, $dbh, q{DELETE FROM Orders WHERE Index=?}, $$self{'id'} );
