73a78c3329846c379d6bdf71936ff52ce18d55e7
root <root@www2.internal.point-one.com>
2009-11-26 10:18:57 -0500

    when copying a PO, automatically authorize it

--- perl/openprint/employee_inventory.pm
+++ perl/openprint/employee_inventory.pm
@@ -1567,6 +1567,17 @@ sub purchase_order_view {
 			$variable{'information'} .= 'PO ' . $PO->id() . ' copied to PO ' . $New->id() .'<br/>';
 			$PO = $New;
 		} # end if
+		if ( ! $PO->authorized() ) {
+			if ( $PO->total() < $Me->purchasing_limit() ) {
+				$variable{'error'} .= $PO->save({
+						'authorized'	=> 1,
+						'authorized_on'	=> 'NOW()',
+						'authorized_by'	=> $session{'user_id'},
+						});
+			} else {
+				$PO->send_approval_required_notification();
+			} # end if
+		} # end if
 	} elsif ( $param{'btnFunction'} eq 'Save' ) {
 		if ( ! $param{'po_id'} ) {
 			$variable{'error'} .= $PO->save( { 'created_by'	=>	$session{'user_id'}, 'company_id'=>$Me->company_id() } );
@@ -1665,25 +1676,7 @@ $log->debug("PO total: " . $PO->total() . ' Me total: ' . $Me->purchasing_limit(
 						'authorized_by'	=> $session{'user_id'},
 						});
 			} else {
-				my $email_template = misc::load_file( $log, $config{'SkinPath'} . '/email_template.html' );
-				my %info;
-				$info{'ReplacementText'} = "<!--#include virtual=\"/email_content/purchase_order_notification.html\"-->";
-				$info{'From'} = $Me;
-				$info{'PurchaseOrder'} = $PO;
-
-				foreach my $U ( openprint::User::find('company_id'=>$Me->company_id(),'purchasing_limit_>='=>$PO->total() ) ) {
-					next if $U->id() == $Me->id();
-
-					$_ = encode_qp( Encode::encode('utf-8', ssi::variable_substitution( undef, $log, $dbh, \$email_template, \%info ) ) );
-					my @body = ('', $_, 'text/html', 'quoted-printable');
-					my %mail = (
-							SMTP    => $config{'Mail Server'},
-							FROM    => sprintf( '"%s" <%s>', $Me->name(), $Me->email() ),
-							TO      => sprintf( '"%s" <%s>', $U->name(), $U->email() ),
-							SUBJECT => 'Purchase Order requiring approval: ' . $PO->id(),
-							);
-					misc::send_email_with_attachment( $log, \%mail, @body );
-				} # end foreach U
+				$PO->send_approval_required_notification();
 			} # end if
 		} # end if
 		if ( ( ! $variable{'error'} ) and $param{'reason'} ) {
