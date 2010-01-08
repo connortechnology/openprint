4ea2a4f22f02a5897a916c37463568c27e59345e
Isaac Connor <iconnor@connortechnology.com>
2010-01-08 11:52:21 -0500

    blah

--- perl/openprint/sql/whitelist.sql
+++ perl/openprint/sql/whitelist.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS whitelist;
 CREATE TABLE whitelist (
 	ip			inet not null,
+	description	text,
 	created_on	timestamp with time zone not null default now(),
 	PRIMARY KEY (ip)
 );
