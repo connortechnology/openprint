56c33fa9c49c951f0de03b93fc4cd21bde5bd398
root <root@mail.topknotch.com>
2010-02-19 17:33:01 -0500

    blah use sheet_width and height instead of layout in equipment fit in folding.

--- perl/openprint/cron/cleanup.pl
+++ perl/openprint/cron/cleanup.pl
@@ -121,7 +121,7 @@ if ( 1 ) {
 		sql::end_transaction( $dbh, $ac );
 	} # end if
 	@Projects = openprint::Project::find(
-			'status'=>'Deleted','order'=>'index desc',
+			'status'=>'Deleted','order'=>'id desc',
 			'created_on_end' => sprintf('%.4d-%.2d-%.2d', Date::Calc::Add_Delta_Days( Date::Calc::Today(), -180 ) ),
 			'updated_on_end' => sprintf('%.4d-%.2d-%.2d', Date::Calc::Add_Delta_Days( Date::Calc::Today(), -180 ) ),
 			);
@@ -134,7 +134,7 @@ if ( 1 ) {
 				next;
 			} # end if
 			next if $Project->docket();
-			if ( sql::execute( undef, undef, q{SELECT * FROM tbl_Quote_Details WHERE ProjectIndex=?}, $Project->id() ) ) {
+			if ( sql::execute( undef, undef, q{SELECT * FROM tbl_Quote_Details WHERE project_id=?}, $Project->id() ) ) {
 				#$log->debug('Quoted!' . $Project->id());
 				next;
 			} # end if
