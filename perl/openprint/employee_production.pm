cf3d1b800432392636f499ab1f711e02e37bdfe6
root <root@www5.internal.point-one.com>
2010-03-08 09:46:25 -0500

    blah

--- perl/openprint/employee_production.pm
+++ perl/openprint/employee_production.pm
@@ -929,7 +929,7 @@ sub complete_signature {
 
 	sql::update( $log, $dbh, 'tbl_Project_Contents', ['lngProjectIndex=? AND lngServiceIndex=?', $project_id, $service_id], 'strStatus', 'Complete' );
 # Remove from Print Schedule
-	foreach my $Job ( openprint::ScheduledJob( 'project_id'=>$project_index, 'service_id'=>$signature_service_index ) ) {
+	foreach my $Job ( openprint::ScheduledJob( 'project_id'=>$project_id, 'service_id'=>$signature_service_index ) ) {
 		$Job->delete();
 	} # end foreach Job
 # Update Bindery Schedule
