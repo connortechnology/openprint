298b38d54b7ae0e704a78dda928b4ef8e90dc14a
root <root@www2.internal.point-one.com>
2010-04-26 17:08:45 -0400

    stock popup on press schedule more work on email administrator

--- perl/openprint/Email.pm
+++ perl/openprint/Email.pm
@@ -9,9 +9,22 @@ require email;
 
 use vars qw( $table $serial %fields %transforms %defaults $log $dbh %session %config );
 *log = \$openprint::log;
-*dbh = \$openprint::dbh;
 *session = \%openprint::session;
 *config = \%openprint::config;
+
+$table = 'mailbox';
+
+%fields = (
+	'id'		=>	'username',
+	'password'	=>	'password',
+	'name'		=>	'name',
+	'maildir'	=>	'maildir',
+	'quota'		=>	'quota',
+	'domain'	=>	'domain',
+	'created_on'	=>	'created',
+	'updated_on'	=>	'modified',
+	'active'		=>	'active',
+);
 
 sub send {
 	my ( $self, %params ) = @_;
@@ -65,3 +78,26 @@ $log->debug("Email: Recipients @recipients");
 
 } # end sub send
 
+sub find {
+	my %params = @_;
+
+	my $sql = 'SELECT * FROM mailbox WHERE 1>0';
+	my @values;
+
+	if ( $params{'active'} ) {
+		$sql .= ' AND active = ?';
+		push @values, $params{'active'};
+	} # end if
+	$sql .= " ORDER BY $params{'order'}" if $params{'order'};
+	my $data = $dbh->selectall_arrayref( $sql, {Slice=>{}}, @values );
+	if ( ! $data ) {
+		$log->debug("openprint::Email::find($sql)" . $dbh->errstr);
+	} else {
+		return map { new openprint::Email( $_->{username}, $_ ); } @$data;
+	} # end if
+} # end sub find
+
+sub delete {
+	
+	#sql::execute( undef, $dbh, 'DELETE FROM mailbox WHERE username=?', $_[0]{'id'} );
+} # end sub delete
