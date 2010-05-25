744d5cfbd4aa018c23f6311142860ed4a2429275
root <root@www2.internal.point-one.com>
2010-05-25 11:50:49 -0400

    rough in hosts in it section

---
+++ perl/openprint/employee_it.pm
@@ -0,0 +1,49 @@
+package openprint::employee_it;
+use openprint;
+use vars qw( %variable %session %param %config $log $dbh $r );
+*variable = \%openprint::variable;
+*session = \%openprint::session;
+*param = \%openprint::param;
+*config = \%openprint::config;
+*log = \$openprint::log;
+*dbh = \$openprint::dbh;
+*r = \$openprint::r;
+
+require openprint::Host;
+
+use strict;
+
+sub hosts {
+	if ( $param{'btnFunction'} eq 'Delete' ) {
+		foreach my $host_id ( ref $param{'host_id'} eq 'ARRAY' ? @{$param{'host_id'}} : $param{'host_id'} ) {
+			my $Host = new openprint::Host( $host_id );
+			$variable{'error'} .= $Host->delete();
+		} # end foreach host_id
+	} elsif ( $param{'btnFunction'} eq 'Save' ) {
+		my $Host = new openprint::Host( $param{'host_id'} );
+		$variable{'error'} .= $Host->save(\%param);
+		%param = ();
+	} # end if
+	ssi::setup_date_select( '/employee/it/hosts.html', 'created_on', 0 );
+	if ( ! exists $session{'/employee/it/hosts.html?assigned'} ) {
+		$session{'/employee/it/hosts.html?assigned'} = 1;
+	} # end if
+	if ( ! exists $session{'/employee/it/hosts.html?notassigned'} ) {
+		$session{'/employee/it/hosts.html?notassigned'} = 1;
+	} # end if
+
+} # end sub hosts
+
+sub _hosts {
+	ssi::save_params( '/employee/it/hosts.html', 'created_on', 'updated_on' );
+} # end sub _hosts
+
+sub host {
+	$variable{'Host'} = new openprint::Host( $param{'host_id'} );
+	if ( $param{'btnFunction'} eq 'Resolve' ) {
+		$variable{'error'} .= $variable{'Host'}->save({'hostname'=> $variable{'Host'}->resolve()});
+	} # end if
+} # end sub view_host
+
+1;
+__END__
