9bd4204ee6b830a88cfbbbeb2d8247edd781f983
Isaac Connor <iconnor@connortechnology.com>
2010-08-31 10:02:05 -0400

    blah

--- perl/openprint/sql/user_purchaseorder_limits.sql
+++ perl/openprint/sql/user_purchaseorder_limits.sql
@@ -1,7 +1,7 @@
 
-DROP TABLE IF EXISTS user_purchaseorder_limit;
+DROP TABLE IF EXISTS user_purchaseorder_limits;
 
-CREATE TABLE user_purchaseorder_limit (
+CREATE TABLE user_purchaseorder_limits (
 	user_id	INTEGER NOT NULL, FOREIGN KEY (user_id) REFERENCES users (index),
 	type_id	INTEGER NOT NULL, FOREIGN KEY (type_id) REFERENCES PurchaseOrder_COntentTypes (id),
 	po_limit	INTEGER,
