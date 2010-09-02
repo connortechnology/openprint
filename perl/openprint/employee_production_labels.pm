fe3bcfda854114da0a56b86d41681fec60ead42f
root <root@www2.internal.point-one.com>
2010-09-02 09:30:37 -0400

    only save a label if properly specified.

--- perl/openprint/employee_production_labels.pm
+++ perl/openprint/employee_production_labels.pm
@@ -24,12 +24,10 @@ sub _label {
 		$Label->set_data($param{'field'}=>$param{'value'});
 		$Label->save();
 		$variable{'PageContent'} = join('',$Label->get_data($param{'field'}));
-$openprint::log->debug("_label get " . join('',$Label->get_data($param{'field'})));
 	} elsif ( $param{'action'} eq 'get' ) {
 		$variable{'PageContent'} = join('',$Label->get_data($param{'field'}));
 	} elsif ( $param{'action'} eq 'getnohtml' ) {
 		$variable{'PageContent'} = join('',$Label->get_data($param{'field'}));
-$openprint::log->debug('filtering');
 		$variable{'PageContent'} =~ s/<br\/>/\n/ig;
 	} # end if
 } # end sub _label
@@ -38,6 +36,4 @@ sub label {
 } # end sub label
 
 1;
-
 __END__
-~	   
