a35060ecb189c425bf38e7d7ec4f7e32bb76fdd8
root <root@fileserver.internal.point-one.com>
2010-09-22 09:31:49 -0400

    fix logging of invalid passwowrd attempts

--- perl/openprint/login.pm
+++ perl/openprint/login.pm
@@ -61,7 +61,9 @@ sub verify_login {
 		# user not found.	Let's see if we got the password wrong, or the email wrong.
 		if ( @Users = openprint::User->find('email'=>$email) ) {
 			$$variable{'information'} = "The password you entered was not correct.	Please try again.";
-			openprint::logs::insertLogRecord(78,'Invalid Password', $Users[0]->id() );
+			foreach my $U ( @Users ) {
+				openprint::logs::insertLogRecord(78,'Invalid Password', $U->id(), $U->company_id() );
+			} # end foreach U
 		} elsif ( @Users = openprint::User->find('email'=>$email,'deleted'=>1) ) {
 			$$variable{'information'} = "\"$email\" Has been deleted.  Please contact us to have your account re-instated.";
 			openprint::logs::insertLogRecord(78,'Account Deleted', $Users[0]->id() );
