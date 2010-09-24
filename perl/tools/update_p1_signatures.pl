55733035c435c97c25f50f8854459c18a7a22a5b
root <root@www2.internal.point-one.com>
2010-09-24 14:25:19 -0400

    blah

--- perl/tools/update_p1_signatures.pl
+++ perl/tools/update_p1_signatures.pl
@@ -171,17 +171,29 @@ $log->warn("Updating sig $sig_id of project $$Project{'id'} adding SignatureInde
 			} # end foreach ssid
 		} # end if
 	} # end foreach
-	if ( $$services{'Folding'} ) {
-	foreach my $service ( @{$$services{'Folding'}} ) {
-		my $specs = openprint::service::get_specs_ref( $Project, $service );
-		foreach my $qty_index ( $Project->quantity_indexes() ) {
-			if ( $$specs{"ddmEquipment-0-$qty_index"} and ! $$specs{"ddmEquipment-1-$qty_index"} ) {
-				openprint::service::insert_service_spec( $log, $dbh, $Project->id(), $service, "ddmEquipment-1-$qty_index", $$specs{"ddmEquipment-0-$qty_index"} );
-				openprint::service::delete_service_spec( $Project->id(), $service, $$specs{"ddmEquipment-0-$qty_index"} );
-			} # end if
-		}
-	} # end foraech service
-	} # end if
+			if ( $$services{'Proofs'} ) {
+				foreach my $service ( @{$$services{'Proofs'}} ) {
+					my $specs = openprint::service::get_specs_ref( $Project, $service );
+					foreach my $key ( keys %{$specs} ) {
+						if ( $key =~ /^txtProofIndex-0-(\d*)-(\d*)$/ ) {
+							my ( $proof_index, $qty_index ) = ( $1, $2 );
+
+							foreach my $spec ( 
+									'txtProofWidth',
+									'txtProofHeight',
+									'txtProofQuantity',
+									'ddmProofType',
+									'txtProofUnitPrice',
+									'txtProofIndex',
+									'chkOverride',) {
+
+								openprint::service::insert_service_spec( $log, $dbh, $Project->id(), $service, "$spec-1-$proof_index-$qty_index", $$specs{"$spec-0-$proof_index-$qty_index"} );
+								openprint::service::delete_service_spec( $Project->id(), $service, $$specs{"$spec-0-$proof_index-$qty_index"} );
+							} # end foreach spec
+						} # end if
+					} # end foreach key
+				} # end foraech service
+			} # end if Proofs
 	my $summary = $Project->summary();
 
 	sql::update( undef, undef, 'Projects', ['id=?', $Project->id()], 'summary', $summary );
@@ -212,8 +224,40 @@ if ( 1 ) {
 			foreach my $qty_index ( $Project->quantity_indexes() ) {
 				openprint::service::insert_service_spec( $log, $dbh, $Project->id(), $$services{''}[0], 'txtPrice'.$qty_index, 0 );
 			} # end foreach
-			# attempt to free memory
-			#openprint::service::init_cache();
+			if ( $$services{'Folding'} ) {
+				foreach my $service ( @{$$services{'Folding'}} ) {
+					my $specs = openprint::service::get_specs_ref( $Project, $service );
+					foreach my $qty_index ( $Project->quantity_indexes() ) {
+						if ( $$specs{"ddmEquipment-0-$qty_index"} and ! $$specs{"ddmEquipment-1-$qty_index"} ) {
+							openprint::service::insert_service_spec( $log, $dbh, $Project->id(), $service, "ddmEquipment-1-$qty_index", $$specs{"ddmEquipment-0-$qty_index"} );
+							openprint::service::delete_service_spec( $Project->id(), $service, $$specs{"ddmEquipment-0-$qty_index"} );
+						} # end if
+					}
+				} # end foraech service
+			} # end if
+			if ( $$services{'Proofs'} ) {
+				foreach my $service ( @{$$services{'Proofs'}} ) {
+					my $specs = openprint::service::get_specs_ref( $Project, $service );
+					foreach my $key ( keys %{$specs} ) {
+						if ( $key =~ /^txtProofIndex-0-(\d*)-(\d*)$/ ) {
+							my ( $proof_index, $qty_index ) = ( $1, $2 );
+
+							foreach my $spec ( 
+									'txtProofWidth',
+									'txtProofHeight',
+									'txtProofQuantity',
+									'ddmProofType',
+									'txtProofUnitPrice',
+									'txtProofIndex',
+									'chkOverride',) {
+
+								openprint::service::insert_service_spec( $log, $dbh, $Project->id(), $service, "$spec-1-$proof_index-$qty_index", $$specs{"$spec-0-$proof_index-$qty_index"} );
+								openprint::service::delete_service_spec( $Project->id(), $service, $$specs{"$spec-0-$proof_index-$qty_index"} );
+							} # end foreach spec
+						} # end if
+					} # end foreach key
+				} # end foraech service
+			} # end if Proofs
 		} # end foreach Project
 	} # end if Type
 	$dbh->do(q`UPDATE project_types set url=NULL where url='prin/prin_broc.html'`);
