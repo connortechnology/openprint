94121d01a8472d4dd896057d1f5271b535535f97
root <root@www2.internal.point-one.com>
2010-10-25 12:55:19 -0400

    append House Stock instead of replace, smartly

--- perl/openprint/employee_production.pm
+++ perl/openprint/employee_production.pm
@@ -1728,7 +1728,10 @@ sub _li {
 
 	my $Job = $variable{'Job'} = new openprint::ScheduledJob( $param{'schedule_id'} );
 	if ( $param{'action'} eq 'House Stock' ) {
-		$Job->save({stock=>$Job->stock().' House Stock'});	
+		my $stock = $Job->stock();
+		if ( ! ( $stock =~ /House Stock/ ) ) {
+			$Job->save({stock=>$stock.' House Stock'});	
+		} # end if
 	} # end if
 } # end sub _li
 
