29432fbf4f597171ca834b501c55a6dc12ff7a93
root <root@mail.topknotch.com>
2010-11-11 10:31:08 -0500

    add duplicate company detection and merging

--- perl/openprint/administrator_managerial.pm
+++ perl/openprint/administrator_managerial.pm
@@ -17,6 +17,9 @@ require openprint::Company;
 require openprint::customer_credit;
 require openprint::Tax;
 require openprint::Email;
+		require openprint::Invoice;
+		require openprint::Payment;
+		require openprint::Timetrack;
 
 use vars qw( $r $log $dbh %variable %param %session %config );
 *r = \$openprint::r;
@@ -324,6 +327,31 @@ sub company_profiles {
 		if ( $param{'txtSearchAccountNum'} ne '' ) {
 			( $index ) = sql::execute( $log, $dbh, 'SELECT id from Company WHERE strAccountNum=?',$param{'txtSearchAccountNum'}); 
 		} # end if 
+	} elsif ( $param{'btnFunction'} eq 'merge' ) {
+		my $Company = new openprint::Company( $index );
+		foreach my $type ( 'User','Order','Quote','Project', 'Claim', 'Log','Timetrack' ) {
+			eval q`
+				foreach ( openprint::`.$type.q`::find('company_id'=>$param{'merge_company_id'}) ) {
+					$_->save({'company_id'=>$Company->id()});
+				} # end foreach
+			`;
+		} # end foreach type
+		foreach my $Timetrack ( openprint::Timetrack::find('owner_id'=>$param{'merge_company_id'}) ) {
+			$Timetrack->save({'owner_id'=>$Company->id()}) if $Timetrack->owner_id() == $Company->id();
+		} # end foreach Timetrack
+		foreach ( openprint::Invoice::find('invoicer_id'=>$param{'merge_company_id'}) ) {
+			$_->save({'invoicer_id'=>$Company->id()});
+		} # end foreach 
+		foreach ( openprint::Invoice::find('invoicee_id'=>$param{'merge_company_id'}) ) {
+			$_->save({'invoicee_id'=>$Company->id()});
+		} # end foreach 
+		foreach my $Payment ( openprint::Payment::find('payor_id'=>$param{'merge_company_id'}) ) {
+			$Payment->save({'payor_id'=>$Company->id()}) if $Payment->payor_id() == $Company->id();
+		} # end foreach  Payment
+		foreach my $Payment ( openprint::Payment::find('recipient_id'=>$param{'merge_company_id'}) ) {
+			$Payment->save({'recipient_id'=>$Company->id()}) if $_->recipient_id() == $Company->id();
+		} # end foreach  Payment
+		new openprint::Company( $param{'merge_company_id'} )->delete();
 	} elsif ( $param{'btnFunction'} eq 'Save' ) {
 
 		$index = $param{'company_id'};
--- perl/tools/detect_duplicate_companies.pl
+++ perl/tools/detect_duplicate_companies.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 use lib '/etc/apache2/lib/perl';
+use utf8;
 use strict;
+use Encode;
 
 require sql;
 require logger;
@@ -19,17 +21,18 @@ use vars qw( $log $dbh );
 $log = new logger( 'warn' );
 
 $openprint::Object::no_cache = 1;
-$dbh = sql::open_sql( $log, ('database'=>$ARGV[0], 'driver'=>'Pg','login'=>$ARGV[1], 'password'=>$ARGV[2]) );
+$dbh = sql::open_sql( $log, ('database'=>$ARGV[0], 'driver'=>'Pg','login'=>$ARGV[1], 'password'=>$ARGV[2], 'host'=>$ARGV[3]) );
 my %company_names;
+$log->warn("Checking for accented names");
 foreach my $C ( openprint::Company::find() ) {
-	my $unaccented = Text::Unaccent::unac_string('LATIN1', $C->name() );
+	my $unaccented = Encode::encode('utf-8',Text::Unaccent::unac_string('LATIN1', $C->name() ) );
 	if ( $unaccented ne $C->name() ) {
 $log->error('Unaccenting ' . $C->name() . ' to ' . $unaccented );
-		$C->name( $unaccented );
-		$C->save();
+		#$C->name( $unaccented );
+		#$C->save();
 	} # end i
 	$company_names{$C->name()} = $C->postalcode();
-#$log->error('Company ' . $C->name() . ' to ' . $unaccented );
+#$log->error('Company ' . $C::name() . ' to ' . $unaccented );
 } # end foreach C
 
 foreach my $name ( sort keys %company_names ) {
@@ -45,7 +48,7 @@ foreach my $name ( sort keys %company_names ) {
 			my @quotes = openprint::Quote::find('company_id'=>$C->id());
 			if ( (@projects < 5 ) and (@orders == 0 ) and (@quotes == 0) ) {
 				$log->error('Deleting');
-				$C->delete();
+				#$C->delete();
 				next;
 			} # end if
 			$log->error("COMPANY #".$C->id() . ' ' . $C->name() . " Projects: " . scalar @projects . " Quotes: " . scalar @quotes . ' Orders: ' . scalar @orders . " Users in dup #$count");
