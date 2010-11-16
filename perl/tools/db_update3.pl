fd42f2747438d83b39925cca14c98a53cee24627
root <root@succubus.connortechnology.com>
2010-11-16 09:32:57 -0500

    blah

---
+++ perl/tools/db_update3.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use lib '/var/www/testing/perl';
+use strict;
+
+require sql;
+require logger;
+require openprint::Object;
+
+use openprint ();
+use vars qw( $log $dbh %config );
+*log = \$openprint::log;
+*dbh = \$openprint::dbh;
+*config = \%openprint::config;
+
+$openprint::Object::no_cache = 1;
+
+$log = new logger( 'debug' );
+
+$ARGV[1] = $ARGV[0] if ! $ARGV[1];
+$ARGV[2] = $ARGV[0] if ! $ARGV[2];
+
+$dbh = sql::open_sql( $log, ('database'=>$ARGV[0], 'driver'=>'Pg','login'=>$ARGV[1], 'password'=>$ARGV[2], 'host'=>$ARGV[3]) );
+configuration::init_cache( $log, $dbh );
+
+my @tables = sql::execute( undef, undef, q`SELECT table_name FROM information_schema.tables where table_schema='public'`);
+my @sequences = sql::execute( undef, undef, q`SELECT sequence_name FROM information_schema.sequences where sequence_schema='public'`);
+
+$dbh->disconnect();
+1;
+__END__
