65c24de152207ef1fa7d05c7b7a518637a48267b
root <root@www2.internal.point-one.com>
2010-12-02 11:41:42 -0500

    blah

---
+++ perl/openprint/User_Notification.pm
@@ -0,0 +1,46 @@
+use strict;
+require openprint::Object;
+package openprint::User_Notification_Type;
+our @ISA = qw(openprint::Object);
+use vars qw( $debug $table $serial %fields %defaults %transforms );
+$debug = 1;
+$table = 'user_notification_types';
+$serial = 'user_notification_types_id_seq';
+%fields = (
+	'id'	=>	'id',
+	'name'	=>	'name',
+);
+
+package openprint::User_Notification;
+our @ISA = qw(openprint::Object);
+
+use vars qw( $debug $table @identified_by %fields %find_fields %defaults %transforms );
+$debug = 1;
+$table = 'user_notifications';
+@identified_by = ( 'user_id', 'type_id' );
+
+%fields = (
+	'user_id'	=>	'user_id',
+	'type_id'	=>	'type_id',
+	'value'		=>	'value',
+);
+%find_fields = (
+	'type'		=>	'(SELECT name from user_notification_types WHERE id=type_id)',
+);
+
+sub User {
+	return new openprint::User( $_[0]{'user_id'} );
+}
+sub type {
+	if ( @_ > 1 ) {
+		my $Type = openprint::User_Notification->find_one('name_lc'=>lc $_[1]);
+		if ( ! $Type->id() ) {
+			$Type->save('name'=>$_[1]);
+		} # end if
+		$_[0]{'type_id'} = $Type;
+		return $Type->name();
+	} # end if
+	return new openprint::User_Notification( $_[0]{'type_id'} );
+} # end sub type
+1;
+__END__
