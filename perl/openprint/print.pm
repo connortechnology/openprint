5db3075f4e55ea5f3b19c5be7b5bb18a66b5a8b2
Isaac Connor <iconnor@succubus.connortechnology.com>
2010-12-07 23:57:19 -0500

    blah

--- perl/openprint/print.pm
+++ perl/openprint/print.pm
@@ -202,13 +202,13 @@ sub view_services {
 				foreach my $service_id ( ref $openprint::param{'service_id'} eq 'ARRAY' ? @$openprint::param{'service_id'} : ( $openprint::param{'service_id'} ) ) {
 				openprint::print_project::delete_service( $log, $dbh, $project_index, $service_id );
 				} # end if
-			} elsif ( $openprint::param{'btnFunction'} eq 'Continue Project' ) {
+			} elsif ( $openprint::param{'btnFunction'} eq 'Recalculate Project' ) {
 				$openprint::session{'project_id'} = $project_index;
 				$Project->currency_id( $openprint::session{Currency_id} );
 				my $status = openprint::service::internal_calc( $log, $dbh, $variable, $project_index, $$services{''}[0], $Project->Type()->type() );
 				if ( $status ne 'uncalculated' ) {
-				openprint::Estimating::MultiPage::calculate_signatures( $log, $dbh, $variable, $project_index );
-				openprint::service::auto_calculate( $r, $log, $dbh, $variable, $project_index, undef );
+					openprint::Estimating::MultiPage::calculate_signatures( $log, $dbh, $variable, $project_index );
+					openprint::service::auto_calculate( $r, $log, $dbh, $variable, $project_index, undef );
 				} # end if
 				$Project->summary(undef);
 				$Project->save();
