66dfdb9f6272ffb4ddd76c49694f7d647bfd6711
root <root@mail.topknotch.com>
2010-12-24 09:31:12 -0500

    fix off by 100 in price calcs

--- www/public_html/administrator/stock/stock.js
+++ www/public_html/administrator/stock/stock.js
@@ -108,8 +108,8 @@ function calc_price( element ) {
 		form.elements['priceperm-'+index].value = do_decimals( priceperm, 2 ); 
 
 		if ( form.elements['wpsi'] )  {
-			form.elements['costcwt-'+index].value = do_decimals( costperm / (form.elements['wpsi'].value * form.elements['width'].value * form.elements['height'].value * 1000), 2);
-			form.elements['pricecwt-'+index].value = do_decimals( priceperm / (form.elements['wpsi'].value * form.elements['width'].value * form.elements['height'].value * 1000), 2);
+			form.elements['costcwt-'+index].value = do_decimals( costperm / (form.elements['wpsi'].value * form.elements['width'].value * form.elements['height'].value * 10), 2);
+			form.elements['pricecwt-'+index].value = do_decimals( priceperm / (form.elements['wpsi'].value * form.elements['width'].value * form.elements['height'].value * 10), 2);
 			if ( form.elements['costperfoot-'+index] ) {
 			form.elements['costperfoot-'+index].value = do_decimals( costperm / (form.elements['wpsi'].value * 144 * 1000), 2);
 			form.elements['priceperfoot-'+index].value = do_decimals( priceperm / (form.elements['wpsi'].value * 144 * 1000), 2);
