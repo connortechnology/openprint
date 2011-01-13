85edb478a1714ee7c6dd1973df85008b6cfa92b4
root <root@www2.internal.point-one.com>
2011-01-13 15:06:25 -0500

    more setup_date updates

--- perl/openprint/employee_inventory.pm
+++ perl/openprint/employee_inventory.pm
@@ -1133,6 +1133,16 @@ sub rfidtags {
 		} # end foreach rfidtag_id
 	} else {
 		ssi::save_params( '/employee/inventory/rfidtags.html', 'Type', 'created_on_start_year','created_on_start_month','created_on_start_day','created_on_end_year','created_on_end_month','created_on_end_day','updated_on_start_year','updated_on_start_month','updated_on_start_day','updated_on_end_year','updated_on_end_month','updated_on_end_day', 'assigned', 'notassigned','valid' );
+		ssi::setup_date_select( '/employee/inventory/rfidtags.html', 'created_on_start', 0 );
+		ssi::setup_date_select( '/employee/inventory/rfidtags.html', 'created_on_end', '' );
+		ssi::setup_date_select( '/employee/inventory/rfidtags.html', 'updated_on_start', '' );
+		ssi::setup_date_select( '/employee/inventory/rfidtags.html', 'updated_on_end', '' );
+		if ( ! exists $session{'/employee/inventory/rfidtags.html?assigned'} ) {
+			$session{'/employee/inventory/rfidtags.html?assigned'} = 1;
+		} # end if
+		if ( ! exists $session{'/employee/inventory/rfidtags.html?notassigned'} ) {
+			$session{'/employee/inventory/rfidtags.html?notassigned'} = 1;
+		} # end if
 	} # end if
 } # end sub rfidtags
 
@@ -1481,6 +1491,8 @@ sub manifests {
 		} # end foreach manifest_id
 	} # end if
 	ssi::save_params( '/employee/inventory/manifests.html', ( 'received_on_start_year','received_on_start_month','received_on_start_day','received_on_end_year','received_on_end_month','received_on_end_day','supplier_id' ) );
+	ssi::setup_date_select( '/employee/inventory/manifests.html', 'received_on_start', -7 );
+	ssi::setup_date_select( '/employee/inventory/manifests.html', 'received_on_end', '' );
 } # end sub manifests
 
 sub _manifests {
@@ -2032,6 +2044,9 @@ sub purchase_orders {
 		$variable{'error'} .= $PO->send_to_vendor();
 		delete $param{'po_id'};
 	} # end if
+	ssi::setup_date_select( '/employee/inventory/purchase_orders.html', 'starting_start', -7 );
+	ssi::setup_date_select( '/employee/inventory/purchase_orders.html', 'starting_end', '' );
+
 } # end sub purchase_orders
 
 sub _purchase_orders {
