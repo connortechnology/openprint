a052dd09db75c3212da5367b84465e992498f419
Isaac Connor <iconnor@pseudo.connortechnology.com>
2011-01-13 16:46:24 -0500

    blah

--- perl/openprint/Object.pm
+++ perl/openprint/Object.pm
@@ -343,7 +343,7 @@ sub find {
 	my $sql = 'SELECT ';
 	$sql .= 'DISTINCT' if $params{'distinct'};
 	delete $params{'distinct'};
-	$sql .= ' * FROM '.$table.' WHERE 1>0';
+	$sql .= ' * FROM '.$table;
 	my @values;
 	my $local_dbh = $params{'dbh'} ? $params{'dbh'} : $openprint::dbh;
 	delete $params{'dbh'};
