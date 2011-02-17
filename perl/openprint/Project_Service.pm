2bc9f87349e63d2caba453322096d1755144ad44
root <root@mail.topknotch.com>
2011-02-17 10:50:14 -0500

    blah Most significant is the backport of Object from testing

--- perl/openprint/Project_Service.pm
+++ perl/openprint/Project_Service.pm
@@ -1,23 +1,27 @@
-package openprint::Project_Service;
-@ISA = qw(openprint::Object);
-
 use strict;
+package openprint::Project_Service;
+our @ISA = qw(openprint::Object);
+
 use openprint ();
 
 require openprint::Project;
 require openprint::User;
+require openprint::ServiceType;
 
-use vars qw( $log $dbh %fields %transforms %defaults $table $serial @identified_by );
+use vars qw( $log $dbh $debug %fields %find_fields %transforms %defaults $table $serial @identified_by );
 *log = \$openprint::log;
 *dbh = \$openprint::dbh;
 
 %fields = (
-	'id'			=>	'lngserviceindex',
+	'service_iid'	=>	'lngserviceindex',
 	'project_id'	=>	'lngprojectindex',
 	'operator_id'	=>	'operator_id',
 	'status'		=>	'strstatus',
 	'servicetype_id'	=>	'servicetype_id',
 	'created_on'	=>	'dtmlastmodified',
+);
+%find_fields = (
+    'category'  =>  '(SELECT ServiceType_Categories.name FROM ServiceType_Categories,Service_Types WHERE ServiceType_Categories.id=Service_Types.category_id AND Service_Types.id=servicetype_id)',
 );
 %transforms = (
 );
@@ -27,7 +31,7 @@ use vars qw( $log $dbh %fields %transforms %defaults $table $serial @identified_
 );
 $table = 'tbl_project_contents';
 $serial = 'ContentsServiceIndex_seq';
-@identified_by = ( 'project_id', 'id' );
+@identified_by = ( 'project_id', 'service_id' );
 
 sub Project {
 	return new openprint::Project( $_[0]{'project_id'} );
