fed79de1d5060d6f4c8c6086a0683d1d3a2c3fc3
root <root@mail.topknotch.com>
2011-02-23 13:44:28 -0500

    Docket improvements

--- perl/openprint/MaterialPrice.pm
+++ perl/openprint/MaterialPrice.pm
@@ -28,6 +28,9 @@ $serial = 'materialprices_id_seq';
 );
 
 sub find {
+	if ( $_[0] eq 'openprint::MaterialPrice' ) {
+		shift;
+	}
 	my %params = @_;
 	my $sql = 'SELECT * FROM tbl_Material_Prices WHERE 1>0';
 	my @values;
@@ -114,14 +117,23 @@ sub next {
 
 sub price {
 	if ( @_ > 1 ) {
-		$_[0]{'price'} = @_[1];
+		$_[0]{'price'} = $_[1];
 	} # end if
 	if ( ! defined $_[0]{'price'} ) {
 		$_[0]{'price'} = sprintf( '%.2f', $_[0]{'cost'} * ( 1+($_[0]{'markup'}/100) ) );
 	} # end if
 	return $_[0]{'price'};
 } # end sub price
-1;
 
+sub Pricelist {
+	return new openprint::Pricelist( $_[0]{'pricelist_id'} );
+}
+sub Equipment {
+	return new openprint::Equipment( $_[0]{'equipment_id'} );
+} # end sub Equipment
+
+sub Material {
+	return new openprint::Material( $_[0]{'material_id'} );
+} # end sub Material
+1;
 __END__
-~       
