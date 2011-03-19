79bdf4ea21cab1dd0634f342cd599d3d98ef7062
root <root@www2.internal.point-one.com>
2011-03-19 09:03:32 -0400

    don't copy quote for info unless owners are the same

--- perl/openprint/main_quote.pm
+++ perl/openprint/main_quote.pm
@@ -84,12 +84,14 @@ sub details {
 					] );
 		} # end while
 
-		my %for;
 		my %by;
 		openprint::quote::get_user_by_info( $log, $dbh, \%by, $Quote->id() );
-		openprint::quote::get_user_for_info( $log, $dbh, \%for, $Quote->id() );
 		$NewQuote->store_user_by_info( \%by );
-		$NewQuote->store_user_for_info( \%for );
+		if ( $Quote->company_id() == $NewQuote->company_id() ) {
+			my %for;
+			openprint::quote::get_user_for_info( $log, $dbh, \%for, $Quote->id() );
+			$NewQuote->store_user_for_info( \%for );
+		} # end if
 		$NewQuote->add_log( 'Copied from quote ' . $Quote->id() );
 		$Quote->add_log( 'Copied to quote ' . $NewQuote->id() );
 		$quote_id = $NewQuote->id();
