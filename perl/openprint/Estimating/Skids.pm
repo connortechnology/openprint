8577d10123067b0667122f25fd2cdc68a853fe81
root <root@mail.topknotch.com>
2011-03-25 09:56:18 -0400

    test for actual number in txtFinishCalliper

--- perl/openprint/Estimating/Skids.pm
+++ perl/openprint/Estimating/Skids.pm
@@ -109,7 +109,7 @@ sub calc {
 	if ( $$specs{'chkOverrideFinishedCalliper'} ne 'Y' ) {
 		$$specs{'txtFinishedCalliper'} = openprint::print::get_finished_calliper( $project_index );
 	} # end if
-	if ( ! $$specs{'txtFinishedCalliper'} ) {
+	if ( ! ( 1*$$specs{'txtFinishedCalliper'} ) ) {
 		$$specs{'alert'} .= 'Unable to calculate the calliper of the project.  Please recalculate printing services.';
 		return $$specs{'Status'} = 'uncalculated';
 	} # end if
@@ -183,7 +183,7 @@ $log->debug("Materials: " . map { $_->name() } @Materials ) if $debug;
 							$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Max Length: %d<br/>', $l );
 							$items_by_size = int($l/$item_length);
 						} else {
-				$$specs{'hdnBreakdown'.$qty_index} .= 'Cant Roll<br/>';
+							$$specs{'hdnBreakdown'.$qty_index} .= 'Cant Roll<br/>';
 						} # end if
 					} # end if
 
