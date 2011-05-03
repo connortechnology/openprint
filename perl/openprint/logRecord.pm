ebd9868e78ccd95f1b15b7712a651c79455bb970
root <root@www2.internal.point-one.com>
2011-05-03 09:52:33 -0400

    add last 10 select company

--- perl/openprint/logRecord.pm
+++ perl/openprint/logRecord.pm
@@ -40,7 +40,11 @@ sub load {
 sub find {
 	my %params = @_;
 	my @values;
-	my $sql = q{SELECT * FROM log WHERE 1>0};
+	my $sql = 'SELECT ';
+	$sql .= 'DISTINCT ' if $params{'distinct'};
+	delete $params{'distinct'};
+	$sql .= '* FROM log WHERE 1>0';
+
 	if ( $params{'id'} ) {
         if ( ref $params{'id'} eq 'ARRAY' ) {
             $sql .= q{ AND id IN (}.join(',', map {'?'} @{$params{'id'}} ).')';
