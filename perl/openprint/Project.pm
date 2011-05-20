85d0e3c921f8cedd53e568e64acbc0aa100edf00
root <root@fileserver.internal.point-one.com>
2011-05-20 08:48:12 -0400

    blah

--- perl/openprint/Project.pm
+++ perl/openprint/Project.pm
@@ -1414,6 +1414,9 @@ $openprint::log->debug("Project::recalculate");
 	$self->summary(undef);
 	return $self->save();
 } # end sub recalculate
+sub Project {
+	return $_[0];
+} # end sub Proejct;
 
 1;
 __END__
