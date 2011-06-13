c218c5f94ddf65fd8b4e87466ce333dd07223217
Isaac Connor <iconnor@connortechnology.com>
2011-06-13 18:34:40 -0400

    blah

--- perl/openprint/User_Profile_Field.pm
+++ perl/openprint/User_Profile_Field.pm
@@ -2,7 +2,8 @@ use strict;
 package openprint::User_Profile_Field;
 our @ISA = qw( openprint::Object );
 
-use vars qw( $table $serial %fields %transforms %defaults );
+use vars qw( $debug $table $serial %fields %transforms %defaults );
+$debug = 1;
 $table = 'user_profile_fields';
 $serial = 'user_profile_fields_id_seq';
 %fields = (
