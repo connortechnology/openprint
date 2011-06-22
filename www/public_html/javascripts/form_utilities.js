746afc245e50031a72b7e0dde96294a2b55eeca3
root <root@www2.internal.point-one.com>
2011-06-22 16:30:32 -0400

    blah

--- www/public_html/javascripts/form_utilities.js
+++ www/public_html/javascripts/form_utilities.js
@@ -240,6 +240,21 @@ function ddm_select_by_text( ddm, value, defaultValue ) {
 		alert( "null ddm passed to ddm_select_by_text" );
 	} // end if
 } // end function ddm_select_by_text( ddm, value );
+function ddm_select_by_text_case_insensitive( ddm, value, defaultValue ) {
+	var lowervalue = value.toLowerCase();
+    if ( ddm ) {
+        for ( var index = 0, len = ddm.options.length; index < len; index += 1 ) {
+            if ( ddm.options[index].text.toLowerCase() == lowervalue ) {
+                ddm_select_by_index( ddm, index );
+                return;
+            } // end if
+        } // end for
+        ddm_select_by_index( ddm, defaultValue );
+    } else {
+        alert( "null ddm passed to ddm_select_by_text" );
+    } // end if
+} // end function ddm_select_by_text( ddm, value );
+
 
 function filterDDM( filter, ddm ) {
 	if ( ! filter.value.length ) {
@@ -1011,3 +1026,13 @@ function getValues( form, element_names ) {
 	} // end for
 	return results;
 } // end function getValues
+function trim (str) {
+	str = str.replace(/^\s+/, '');
+	for (var i = str.length - 1; i >= 0; i--) {
+		if (/\S/.test(str.charAt(i))) {
+			str = str.substring(0, i + 1);
+			break;
+		}
+	}
+	return str;
+}
