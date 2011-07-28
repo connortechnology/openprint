42f79c341d4278ce3b435482fcceafe7c269ad48
root <root@www2.internal.point-one.com>
2011-07-28 11:00:18 -0400

    fix _lc

--- perl/openprint/Object.pm
+++ perl/openprint/Object.pm
@@ -397,7 +397,7 @@ sub find {
 				if ( exists $params{$k.'_lc'} ) {
 					$sql .= " AND lower($$f{$k}) = ?";
 					push @values, lc $params{$k.'_lc'};
-					delete $params{$k.' lc'};
+					delete $params{$k.'_lc'};
 				} elsif ( exists $params{$k.' lc'} ) {
 					$sql .= " AND lower($$f{$k}) = ?";
 					push @values, lc $params{$k.' lc'};
