974ed4d72c619f6dc2a3f23a367994ad05a92bec
root <root@www2.internal.point-one.com>
2011-08-05 12:47:29 -0400

    when there is only 1 user in the company, auto-populate prepared for field

--- perl/openprint/main_quote.pm
+++ perl/openprint/main_quote.pm
@@ -249,6 +249,11 @@ $openprint::log->debug("No for info");
 # pull information to pre-fill input fields
                 @$variable{'ForCompanyName', 'ForAddress1', 'ForAddress2', 'ForCity', 'ForStateProvince', 'ForPostalCode', 'ForCountry', 'ForPhone','ForExtension', 'ForFax' } = (
 				$Company->business_name(), $Company->address1(), $Company->address2(), $Company->city(), $Company->state(), $Company->postalcode(), $Company->country(), $Company->phone(), $Company->extension(), $Company->fax() );
+
+				my @Users = openprint::User::find('company_id'=>$openprint::session{'company_id'},'limit'=>2);
+				if ( @Users == 1 ) {
+					@$variable{'ForFirstName','ForLastName','ForTitle','ForEmail','ForSalutation'} = $Users[0]->get('firstname','lastname','title','email','salutation');
+				} # end if
             } # end if
         } # end if
     } # end if
