7532cd934e31f16d5cff8334d00294da57e4065b
root <root@fileserver.internal.point-one.com>
2011-09-05 17:14:40 -0400

    blah

--- perl/openprint/administrator_managerial.pm
+++ perl/openprint/administrator_managerial.pm
@@ -804,6 +804,7 @@ sub upload_log {
 	ssi::save_params( '/administrator/managerial/upload_log.html', ( 
 		( map { 'uploaded_on_start_'.$_ } ( 'year', 'month', 'day' ) ),
 		( map { 'uploaded_on_end_'.$_ } ( 'year', 'month', 'day' ) ),
+		'company_id','type',
 	) );
 
 	ssi::setup_date_select( '/administrator/managerial/upload_log.html', 'uploaded_on_start', -7 );
