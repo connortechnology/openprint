277fa5aa3d9e83a3a08186136cf8686afe8325b8
root <root@www2.internal.point-one.com>
2011-09-05 15:28:04 -0400

    fix

--- perl/openprint/Shift.pm
+++ perl/openprint/Shift.pm
@@ -448,7 +448,7 @@ sub get_Shifts {
 	# Three cases, no shifts, shifts before, shifts after.
 
 	# Case #1 Shift before
-	if ( my $LastShift = openprint::Shift::find(
+	if ( my $LastShift = openprint::Shift::find_one(
 			'equipment_id'      =>  $Equipment->id(),
 			'starttime_<'       =>  $parser->format_datetime( $start_dt ),
 			'order'             =>  'starttime DESC',
