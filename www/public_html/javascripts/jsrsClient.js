4f536d6890691319138316d879810be9e9d1780d
root <root@www2.internal.point-one.com>
2011-09-14 10:46:09 -0400

    fix problem with jsrs on newer firefox. Must get rid of this dirty hack.

--- www/public_html/javascripts/jsrsClient.js
+++ www/public_html/javascripts/jsrsClient.js
@@ -199,13 +199,20 @@ function contextGetPayload(){
 		case 'NS':
 			return this.container.document.forms['jsrs_Form'].elements['jsrs_Payload'].value;
 		case 'IE':
-		if ( ! this.container.document.forms['jsrs_Form']['jsrs_Payload'] ) {
-			//alert(this.container);
-		} else {
-			return this.container.document.forms['jsrs_Form']['jsrs_Payload'].value;
-		} // end if
-	case 'MOZ':
+			if ( ! this.container.document.forms['jsrs_Form']['jsrs_Payload'] ) {
+				//alert(this.container);
+			} else {
+				return this.container.document.forms['jsrs_Form']['jsrs_Payload'].value;
+			} // end if
+		case 'MOZ':
+			if ( ! window.frames[this.container.name] ) {
+				//alert('No frame for ' + this.container.name);
+				var frame = $(this.container.name);
+				var d = frame.document ? frame.document : frame.contentDocument;
+				return d.forms['jsrs_Form']['jsrs_Payload'].value;
+			} else {
 			return window.frames[this.container.name].document.forms['jsrs_Form']['jsrs_Payload'].value; 
+			}
 		case 'OPR':
 			var textElement = window.frames[this.container.name].document.getElementById("jsrs_Payload");
 			return textElement.value;
