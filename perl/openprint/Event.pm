c63fd4015aafa0390457e1d32f0509356862ecc9
Isaac Connor <iconnor@connortechnology.com>
2011-10-04 22:53:50 -0400

    blah

--- perl/openprint/Event.pm
+++ perl/openprint/Event.pm
@@ -115,5 +115,28 @@ sub Attendance {
 	return @{$_[0]{'Attendance'}};
 } # end sub Attendance
 
+sub Created_By {
+	return new openprint::User( $_[0]{'created_by'} );
+}
+
+sub html {
+	my $Event = $_[0];
+	my $html = sprintf(q`
+			<div class="Event">
+			<a class="thumbnail" href="/event/view.html?event_id=%1$d"><img alt="" src="%2$s"/></a>
+			<span class="name"><a href="/event/view.html?event_id=%1$d">%3$s</a></span>
+			<span class="when">%4$s</span>
+			`, $Event->id(),
+			$Event->Asset()->thumbnail_url(),
+			ssi::htmlize($Event->name()),
+			( $Event->starting() ? Date::Format::time2str($openprint::config{'DateTimeFormat'}, Date::Parse::str2time( $Event->published_on() ) ) : '' ),
+
+			);
+	my @Comments = $Event->Comments();
+	$html .= sprintf(q`<div class="comments">This event has %s.</div>`, ( @Comments == 1 ? '1 comment' : @Comments . ' comments' ) );
+	$html .= '</div>';
+	return $html;
+} # end  sub html
+
 1;
 __END__
