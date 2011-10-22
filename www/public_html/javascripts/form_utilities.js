cacde1181acd9f0f1a9788acb0b1b0a1fda8babc
Isaac Connor <iconnor@connortechnology.com>
2011-10-22 00:44:08 -0400

    fixes

--- www/public_html/javascripts/form_utilities.js
+++ www/public_html/javascripts/form_utilities.js
@@ -728,7 +728,7 @@ function Country_onchange( country_ddm, state ) {
 function Location_onchange( parent_element, child_element, type ) {
 	if ( parent_element.getValue() ) {
 		// only do anything if we have selected something	
-		new Ajax.Request( '_location_ddm.json', { 
+		new Ajax.Request( '/account/_location_ddm.json', { 
 			parameters: { 
 					type: type,
 					parent_element: parent_element.id,
