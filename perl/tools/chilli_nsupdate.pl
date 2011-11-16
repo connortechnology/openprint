d112ce85e50d53f161d33edba2758f54c116efb9
root <root@mail2.point-one.com>
2011-11-16 10:49:48 -0500

    blah

---
+++ perl/tools/chilli_nsupdate.pl
@@ -0,0 +1,137 @@
+#!/usr/bin/perl
+use strict;
+#use warnings;
+use lib '/var/www/testing/perl';
+
+use Getopt::Long;
+use File::Basename qw(basename);
+use openprint;
+
+require misc;
+require logger;
+require sql;
+require openprint::Host;
+
+use vars qw( $log $dbh %config);
+*dbh = \$openprint::dbh;
+*config = \%openprint::config;
+
+my $program = basename($0);
+
+my $opts = {};
+GetOptions($opts, 'help', 
+    'db_name=s', 'db_host=s', 'db_user=s', 'db_pass=s','log_level=s',
+ );
+
+*log = \$openprint::log;
+$log = logger->new('debug');
+# Get our configuration information
+if (my $err = ReadCfg('/etc/chilli_nsupdate.conf')) {
+    die $err;
+} # end if
+
+foreach my $param ( 'db_name','db_user','db_pass' ) {
+	$CFG::Config{$param} = $$opts{$param} if $$opts{$param};
+	if ( ! $CFG::Config{$param} ) {
+		die "$program: missing required --$param parameter";
+	}
+} # end foreach required-param
+
+foreach my $param ( 'db_host', 'log_file', 'log_level' ) {
+	$CFG::Config{$param} = $$opts{$param} if $$opts{$param};
+} # end foreach non-required param
+
+$CFG::Config{'log_level'} = 'debug' if ! $CFG::Config{'log_level'};
+$log = logger->new( {'file'=>$CFG::Config{'log_file'}, 'level'=>$CFG::Config{'log_level'}} );
+
+if ($CFG::Config{help}) {
+    usage();
+    exit 0;
+}
+
+if ( $CFG::Config{'log_level'} eq 'debug' ) {
+foreach my $k ( keys %ENV ) {
+$log->debug("Environment: $k => $ENV{$k}");
+}
+}
+
+if ( $ENV{'CALLING_STATION_ID'} ) {
+	if ( $CFG::Config{'db_name'} ) {
+		$dbh = sql::open_sql( $log,
+				'host'      => $CFG::Config{'db_host'},
+				'database'  => $CFG::Config{'db_name'},
+				'driver'    => 'Pg',
+				'login'     => $CFG::Config{'db_user'},
+				'password'  => $CFG::Config{'db_pass'},
+				);
+		die 'Error opening db' if ! $dbh;
+	} else {
+		$log->warn("Must specify database name in order to look up hosts.\n");
+	} # end if
+	my $Host = openprint::Host->find_one('mac any'=>$ENV{'CALLING_STATION_ID'});
+	if ( $Host ) {
+		if ( $Host->ip() != $ENV{'FRAMED_IP_ADDRESS'} ) {
+			$Host->save({'ip'=>$ENV{'FRAMED_IP_ADDRESS'}});
+		} # end if
+		my $hostname = $Host->hostname();
+		$hostname .= '.internal.point-one.com' if $hostname !~ /.internal.point-one.com$/;
+
+		if ( open NSUPDATE, "| nsupdate" ) {
+			print NSUPDATE "server localhost\n";
+			print NSUPDATE "update delete $hostname. IN A\n";
+			print NSUPDATE "update add $hostname. 86400 IN A $ENV{'FRAMED_IP_ADDRESS'}\n";
+			print NSUPDATE "send\n";
+			close NSUPDATE;
+		} # end if 
+
+	} # end if Hosts
+	$dbh->disconnect();
+} else {
+	$log->error("No CALLING_STATION_ID");
+} # end if
+exit(0);
+
+# Read a configuration file
+#   The arg can be a relative or full path, or
+#   it can be a file located somewhere in @INC.
+sub ReadCfg {
+    my $file = $_[0];
+
+    our $err;
+
+    {   # Put config data into a separate namespace
+        package CFG;
+		use vars qw( %Config );
+
+        # Process the contents of the config file
+        my $rc = do($file);
+
+        # Check for errors
+        if ($@) {
+            $::err = "ERROR: Failure compiling '$file' - $@";
+        } elsif (! defined($rc)) {
+            $::err = "ERROR: Failure reading '$file' - $!";
+        } elsif (! $rc) {
+            $::err = "ERROR: Failure processing '$file'";
+        }
+    }
+
+    return ($err);
+}
+
+sub usage {
+	print <<EOH;
+
+usage: chilli_nsupdate.pl [--help] 
+
+The purpose of this script is to do a dns update when someone connects to the chilli hotspot
+
+Command-line options:
+
+	--help		Displays this message.
+
+EOH
+} # end sub usage
+
+1;
+__END__
