7220f4accba6c6abb89f95062cc733d9e024ba02
Isaac Connor <iconnor@perry.internal.point-one.com>
2011-12-05 19:09:22 -0500

    blah

--- perl/openprint/article.pm
+++ perl/openprint/article.pm
@@ -190,7 +190,14 @@ sub edit {
 
 sub list {
 	my $Category = $variable{'Category'} = new openprint::Article_Category( $param{'category_id'} );
+	_list();
+	$session{'/article/list.html?paging_per_page'} = 5;
+	$session{'/article/list.html?paging_page'} = 0;
 } # end sub list
+
+sub _list {
+	ssi::save_params('/article/list.html', 'paging_page','category_id' );
+} # end sub _list
 
 sub category {
 	my $Category = $variable{'Category'} = new openprint::Article_Category( $param{'category_id'} );
