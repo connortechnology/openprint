a715d71827585b31cd65e12d4f021c40728472b3
Isaac Connor <iconnor@connortechnology.com>
2012-02-23 17:17:09 -0500

    blah

--- perl/openprint/Asset.pm
+++ perl/openprint/Asset.pm
@@ -116,7 +116,7 @@ sub thumbnail_url {
 } # end sub thumbnail_url
 
 sub thumbnail_html {
-	return sprintf('<img src="%s" alt="%s"/>', $_[0]->thumbnail_url(), $_[0]->name() );
+	return sprintf('<img src="%1$s" alt="%2$s" title="%2$s" />', $_[0]->thumbnail_url(), $_[0]->name() );
 } # end sub thumbnail_html
 
 sub thumbnail_path {
