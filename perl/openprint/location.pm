6ca8edd81148955e419be5c6e24b416a1bb584fa
Isaac Connor <iconnor@connortechnology.com>
2012-03-26 22:41:58 -0400

    blah

--- perl/openprint/location.pm
+++ perl/openprint/location.pm
@@ -85,6 +85,7 @@ sub view {
 					'url'			=>	$param{'url'},
 					'latitude'		=>	$param{'latitude'},
 					'longitude'		=>	$param{'longitude'},
+					'type_id'		=>	$param{'type_id'},
 					});
 			(new openprint::Log())->save({'action'=>($param{'location_id'} ? 'Update Location' : 'Create Location'), 'object'=>'Location','object_id'=>$Location->id()});
 		} # end if
