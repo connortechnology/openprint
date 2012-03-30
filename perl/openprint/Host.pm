e80f90cc0be31af5dd46e84e8be07bae50a22a61
root <root@www2.internal.point-one.com>
2012-03-30 14:10:15 -0400

    blah

--- perl/openprint/Host.pm
+++ perl/openprint/Host.pm
@@ -55,6 +55,7 @@ $serial = 'hosts_id_seq';
 	'online'		=>	'online',
 	'type_id'		=>	'type_id',
 	'type'			=>	undef,
+	'state_changed_on'	=>	'state_changed_on',
 );
 %transforms = (
 );
@@ -70,6 +71,7 @@ $serial = 'hosts_id_seq';
 	'deleted'	=>	0,
 	'online'	=>	undef,
 	'type_id'	=>	undef,
+	'state_changed_on'	=>	undef,
 );
 sub resolve {
 	my ( $self ) = @_;
