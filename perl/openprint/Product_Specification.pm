ffa890b8823a487de29cdb3d234b2f1558189ca8
Isaac Connor <iconnor@connortechnology.com>
2012-03-30 11:46:51 -0400

    blah

---
+++ perl/openprint/Product_Specification.pm
@@ -0,0 +1,21 @@
+use strict;
+package openprint::Product_Specification;
+our @ISA = qw( openprint::Object );
+use vars qw( $debug $table %fields %transforms %defaults $serial );
+
+$debug = 1;
+$serial = 'product_specifications_id_seq';
+$table = 'product_specifications';
+
+%fields = (
+		'id'				=>	'id',
+		'product_id'		=>	'product_id',
+		'name'				=>	'name',
+		'value'				=>	'value',
+);
+
+%defaults = (
+);
+
+1;
+__END__
