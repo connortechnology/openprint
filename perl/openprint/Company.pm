eab922097a5925f3be66cb12ee55f3675381a5b1
root <root@www2.internal.point-one.com>
2012-04-04 15:11:11 -0400

    blah

--- perl/openprint/Company.pm
+++ perl/openprint/Company.pm
@@ -342,7 +342,7 @@ sub start_year {
 } # end sub start_year
 
 sub Credit {
-	return new openprint::Company_credit( { 'company_id'=>$_[0]{id}, 'supplier_id'=>$_[1] } );
+	return new openprint::Company_Credit( { 'company_id'=>$_[0]{id}, 'supplier_id'=>$_[1] } );
 } # end sub Credit
 
 sub dropdown {
