a35aef704a601eea5d0564664a517ed90a525301
Isaac Connor <iconnor@connortechnology.com>
2012-05-22 09:10:14 -0400

    deal with bots asking for photo with no album_id or asset_id

--- perl/openprint/photo_albums.pm
+++ perl/openprint/photo_albums.pm
@@ -177,10 +177,20 @@ $log->debug("$id , $filename ");
 sub view_photo {
 	$param{'asset_id'} =~ s/\D//g;
 	$param{'album_id'} =~ s/\D//g;
+	if ( ! $param{'asset_id'} and $param{'album_id'} ) {
+		# Search engines, etc might get here
+		return;
+	} # end if
+
 	my $Photo = openprint::Photo_in_Album->find_one( 'asset_id' => $param{'asset_id'}, 'album_id'=> $param{'album_id'} );
+	if ( ! $Photo ) {
+		$log->warn("No photo for album $param{album_id} phto: $param{asset_id}");
+		return;
+	} # end if
+
 	my $Album = $Photo->Album();
 
-	if ( $Photo and ( $Album->can_edit() ) ) {
+	if ( $Album->can_edit() ) {
 
 		if ( $param{'action'} eq 'set as thumbnail' ) {
 			$variable{'error'} .= $Album->save({'thumbnail_id'=>$param{'asset_id'}});
