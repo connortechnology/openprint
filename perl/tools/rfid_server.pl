ac6d2fc118c2e16bb7f790109adce151f0ee2db9
root <root@fileserver.internal.point-one.com>
2012-05-22 14:42:14 -0400

    blah

--- perl/tools/rfid_server.pl
+++ perl/tools/rfid_server.pl
@@ -49,7 +49,7 @@ sub Checkout_Skid {
 sub process_request {
 	my $self = shift;
 
-	$dbh = sql::open_sql( $log, ('database'=>'point-one', 'driver'=>'Pg','login'=>'point-one', 'password'=>'point-one','host'=>'www4') );
+	$dbh = sql::open_sql( $log, ('database'=>'point-one', 'driver'=>'Pg','login'=>'point-one', 'password'=>'point-one','host'=>'database') );
 
 	# Have to reload scanner here
 
