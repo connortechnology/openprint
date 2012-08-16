6557ec2e2b397a8b3509a7b3e5d4e7a50090a794
Isaac Connor <iconnor@connortechnology.com>
2012-08-16 14:37:11 -0400

    add manufacturer to products del with no asset thumbnail sizes being defined

--- perl/openprint/Asset.pm
+++ perl/openprint/Asset.pm
@@ -114,6 +114,10 @@ sub sized_url {
 				} elsif ( $size eq 'large' ) {
 					$width = $openprint::config{'Large Asset Width'};
 				} # end if
+				if ( ! $width ) {
+					$openprint::log->error("No asset size in config for $size");
+					return '/assets/'.$filename;
+				} # end if	
 				$openprint::log->debug("Creating $size at ${width} x $src $dest");
 				if ( system(qq`convert -adaptive-resize ${width}x "$src" "$dest"`) ) {
 					$openprint::log->error("ERror creating sized image. Reason: $1");
@@ -132,6 +136,10 @@ sub sized_url {
 				} elsif ( $size eq 'large' ) {
 					$width = $openprint::config{'Large Asset Width'};
 				} # end if
+				if ( ! $width ) {
+					$openprint::log->error("No asset size in config for $size");
+					return '/assets/'.$blah.'.jpg';
+				} # end if	
 				$openprint::log->debug("Creating $size at ${width}x $src $dest");
 				
 				$_ = `mplayer -frames 1 -nosound -quiet -zoom -vf scale=$width:-3 -vo jpeg:outdir=/tmp -ss 60 $src`;
--- perl/tools/db_update3.pl
+++ perl/tools/db_update3.pl
@@ -1083,8 +1083,8 @@ if ( ! sets::isin('object_views', \@tables ) ) {
 	die if $dbh->errstr();
 } # end if
 
-if ( ! sets::isin('projects', \@tables ) ) {
-	$dbh->do( misc::load_file( $log, q{../openprint/sql/Projects.sql}) );
+if ( ! sets::isin('products', \@tables ) ) {
+	$dbh->do( misc::load_file( $log, q{../openprint/sql/Products.sql}) );
 	die if $dbh->errstr();
 } else {
 	my $data = $openprint::dbh->selectall_hashref( "SELECT column_name, data_type, column_default, is_nullable FROM information_schema.columns WHERE table_name='products'", 'column_name');
@@ -1092,6 +1092,11 @@ if ( ! sets::isin('projects', \@tables ) ) {
 		$dbh->do('ALTER TABLE products ADD album_id INTEGER');
 		$dbh->do('ALTER TABLE products ADD FOREIGN KEY (album_id) REFERENCES Photo_Albums (id)');
 	} # end if
+	if ( ! exists $$data{'manufacturer_id'} ) {
+		$dbh->do('ALTER TABLE products ADD manufacturer_id INTEGER');
+		$dbh->do('ALTER TABLE products ADD FOREIGN KEY (manufacturer_id) REFERENCES Manufacturers (id)');
+	} # end if
+	
 } # end if
 $dbh->disconnect();
 1;
