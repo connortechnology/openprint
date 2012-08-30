f1d243dfe3f3cd8be47cd18b053cdc12b12624f8
root <root@www2.internal.point-one.com>
2012-08-30 12:59:43 -0400

    add locking to vacation updates

--- perl/openprint/employee_project.pm
+++ perl/openprint/employee_project.pm
@@ -71,6 +71,10 @@ sub view {
 	} # end if
 
 	my $Project = new openprint::Project( $project_index );
+	if ( ! $$Project{id} ) {
+		$variable{error} .= "Project $project_index not found.<br/>";
+		return;
+	} # end if
 	my $order_id = $param{'OrderID'};
 	$order_id = $Project->order_id() if ! $order_id;
 	if ( $project_index and ( ! $order_id ) and $param{'Docket'} ) {
