8241b9a86097eed716971c857364a77e17537f6f
Isaac Connor <iconnor@connortechnology.com>
2012-09-16 13:30:28 -0400

    move this out of employee

--- perl/openprint/marketing.pm
+++ perl/openprint/marketing.pm
@@ -15,7 +15,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 use strict;
-package openprint::employee_marketing;
+package openprint::marketing;
 
 require openprint::EmailCampaign;
 require openprint::MarketingCategory;
