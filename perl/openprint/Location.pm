97bafb8a98d9effe41759220b7464b58e80941d6
Isaac Connor <iconnor@connortechnology.com>
2012-09-28 16:37:00 -0400

    blah

--- perl/openprint/Location.pm
+++ perl/openprint/Location.pm
@@ -510,10 +510,10 @@ sub filters {
 	} elsif ( ref $selected eq 'ARRAY' ) {
 		( $country_id, $state_id, $city_id ) = @$selected;
 	} # end if	
+    my @Countries = openprint::Location->find(order=>'lower(name)',type=>'country');
 
     my $html = '<li><label>Country</label>';
-    my @Countries = openprint::Location->find(order=>'lower(name)',type=>'country');
-    $html .= ssi::select( [ '', 'All', map { $_->id(), $_->name() } @Countries ], $country_id, { name=>'country_id', id=>'country_id', onchange=>q`Location_onchange( this, 'country' );"` } );
+    $html .= ssi::select( [ '', 'All', map { $_->id(), $_->name() } @Countries ], $country_id, { name=>'country_id', id=>'country_id', onchange=>q`Location_onchange( this, 'country' );` } );
 
     $html .= '</li><li><label>';
 	my $Country = new openprint::Location($country_id);
@@ -528,13 +528,13 @@ sub filters {
     my @States = openprint::Location->find(order=>'lower(name)',type=>'state',
 			( sets::isin( $country_id, [ map { $_->id() } @Countries ] ) ? ( 'parent_id'=>$country_id ) : () ),
 			);
-    $html .= ssi::select( [ '', 'All', map { $_->id(), $_->name() } @States ], $state_id, { name=>'state_id', id=>'state_id', onchange=>q`Location_onchange( this, 'state' );"` } );
+    $html .= ssi::select( [ '', 'All', map { $_->id(), $_->name() } @States ], $state_id, { name=>'state_id', id=>'state_id', onchange=>q`Location_onchange( this, 'state' );` } );
 
     $html .= '</li><li><label>City</label>';
     my @Cities = openprint::Location->find('order'=>'lower(name)','type'=>'city',
         ( sets::isin( $state_id, [ map { $_->id() } @States ] ) ? ( 'parent_id'=>$state_id ) : () ),
     );
-    $html .= ssi::select( [ '', 'All', map { $_->id(), $_->name() } @Cities ], $city_id, { name=>'city_id', id=>'city_id', onchange=>q`Location_onchange( this, 'city' );"` } );
+    $html .= ssi::select( [ '', 'All', map { $_->id(), $_->name() } @Cities ], $city_id, { name=>'city_id', id=>'city_id', onchange=>q`Location_onchange( this, 'city' );` } );
 	$html .= '</li>';
 
     return $html;
