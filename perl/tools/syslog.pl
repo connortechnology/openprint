cce80dfec63329611910abd3e4fd2db9dd317fa3
root <root@mail2.point-one.com>
2012-10-22 12:36:18 -0400

    blah

--- perl/tools/syslog.pl
+++ perl/tools/syslog.pl
@@ -26,50 +26,50 @@ my $program = basename($0);
 
 my $opts = {};
 Getopt::Long::GetOptions($opts, 'fifo=s', 'help', 'config=s',
-    'log_file=s', 'log_level=s',
-    'pid_file=s', 'db_name=s', 'db_host=s', 'db_user=s', 'db_pass=s',
-	'port=s',
- );
+	'log_file=s', 'log_level=s',
+	'pid_file=s', 'db_name=s', 'db_host=s', 'db_user=s', 'db_pass=s',
+	'port=s','debug=s',
+);
 
 if ($opts->{help}) {
-    usage();
-    exit 0;
+	usage();
+	exit 0;
 } # end if
 
 $log = new logger('level'=>'debug');
 # Get our configuration information
 if (my $err = configuration::from_file('/etc/openprint-syslog.conf')) {
-    die $err;
+	die $err;
 } # end if
 
 foreach my $param ( 'db_name','db_user','db_pass', ) {
-    $config{$param} = $$opts{$param} if $$opts{$param};
-    if ( ! $config{$param} ) {
-        die "$program: missing required --$param parameter";
-    }
+	$config{$param} = $$opts{$param} if $$opts{$param};
+	if ( ! $config{$param} ) {
+		die "$program: missing required --$param parameter";
+	}
 } # end foreach required-param
 
 configuration::merge($opts);
-$config{port} = 514 if ! $config{port};
+$config{port} = 10514 if ! $config{port};
 
 if ( $config{'pid_file'} ) {
-    my $pidh;
-    if (open($pidh, '> '.$config{'pid_file'} ) ) {
-        print $pidh $$."\n";
-        close($pidh);
-    } else {
-        die "Unable to open pid file";
-    } # end if
+	my $pidh;
+	if (open($pidh, '> '.$config{'pid_file'} ) ) {
+		print $pidh $$."\n";
+		close($pidh);
+	} else {
+		die "Unable to open pid file";
+	} # end if
 } # end if
 
 $log = logger->new( {'file'=>$config{'log_file'}, 'level'=>$config{'log_level'}} );
 $log->info("Opening SQL connection");
 $dbh = sql::open_sql( $log,
-    'host'      => $config{'db_host'},
-    'database'  => $config{'db_name'},
-    'driver'    => 'Pg',
-    'login'     => $config{'db_user'},
-    'password'  => $config{'db_pass'},
+	'host'		=> $config{'db_host'},
+	'database'	=> $config{'db_name'},
+	'driver'	=> 'Pg',
+	'login'	 => $config{'db_user'},
+	'password'	=> $config{'db_pass'},
 );
 die if ! $dbh;
 
@@ -90,7 +90,7 @@ my @re = (
 		'^(\w{3} [ :0-9]{11}) [\._a-zA-Z0-9\-]+ proftpd\[[0-9]+\]: [\.\-A-Za-z0-9]+ \([\.\-A-Za-z0-9]+\[([.:a-zA-Z0-9]+)\]\) \- Maximum login attempts \([0-9]+\) exceeded, connection refused$',
 		'^(\w{3} [ :0-9]{11}) [\._a-zA-Z0-9\-]+ proftpd\[[0-9]+\]: [\.\-A-Za-z0-9]+ \([\.\-A-Za-z0-9]+\[([.:a-zA-Z0-9]+)\]\) \- USER [\.\-A-Za-z0-9]+: no such user fround from [0-9.]+\[[0-9.]+\] to [.:a-zA-Z0-9]+$',
 		'^(\w{3} [ :0-9]{11}) [\._a-zA-Z0-9\-]+ sshd\[[0-9]+\]: Failed keyboard-interactive/pam for invalid user [\.\-A-Za-z0-9]+ from ([.:a-zA-Z0-9]+) port [0-9]+ ssh2$',
-		'^(\w{3} [ :0-9]{11}) [\._a-zA-Z0-9\-]+ dovecot: pop3-login: Disconnected \(auth failed, 1 attempts\): user=<[a-zA-Z@\.0-9]+>, method=PLAIN, rip=([\.0-9]+), lip=[\.0-9]+(, session=<[^>]+)?$',
+		'^(\w{3} [ :0-9]{11}) [\._a-zA-Z0-9\-]+ dovecot: pop3-login: Disconnected \(auth failed, 1 attempts\): user=<[a-zA-Z@\.0-9]*>, method=PLAIN, rip=([\.0-9]+), lip=[\.0-9]+(, session=<[^>]+)?$',
 		q`^(\w{3} [ :0-9]{11}) [\._a-zA-Z0-9\-]+ named\[[0-9]+\]: client ([0-9.]+)#[0-9]+: query \(cache\) '\./NS/IN' denied$`,
 );
 
@@ -109,11 +109,11 @@ my $buf;
 do{
 	if ( ! $dbh->ping() ) {
 		$dbh = sql::open_sql( $log,
-				'host'      => $config{db_host},
-				'database'  => $config{db_name},
-				'driver'    => 'Pg',
-				'login'     => $config{db_user},
-				'password'  => $config{db_pass},
+				host		=> $config{db_host},
+				database	=> $config{db_name},
+				driver		=> 'Pg',
+				login		=> $config{db_user},
+				password	=> $config{db_pass},
 				);
 		if ( ! $dbh ) {
 			$log->error("Cannot connect to db! Sleeping");
@@ -123,18 +123,20 @@ do{
 	} # end if
 	
 	my %host_counts;
-	# Every hour, we update and
 
+	# Every hour, we update and
 	if ( $last_update < (time-3600) ) {
 		$last_update = time;
 
-		%whitelist = map{ $_->ip(), $_ } openprint::Host->find('whitelist'=>1,'ip is null'=>1);
+		%whitelist = map{ $_->ip(), $_ } openprint::Host->find( whitelist => 1, 'ip is null' => 0 );
+		$openprint::log->debug(join("\n", map { 'whitelist: ' . $_->ip() } openprint::Host->find( whitelist => 1, 'ip is null' => 0 ) ) ) if $$opts{debug};
 
-		if ( $opts->{'blacklist'} ) {
-			if ( ! open( FH, '>'.$opts->{'blacklist'} ) ) {
+		# If a blacklist is specified, update it on start
+		if ( $opts->{blacklist} ) {
+			if ( ! open( FH, '>'.$opts->{blacklist} ) ) {
 				die 'Unable to open blacklist: ' . $opts->{'blacklist'} . "\n";
 			} else {
-				foreach my $Host ( openprint::Host->find('blacklist'=>1,'order'=>'ip') ) {
+				foreach my $Host ( openprint::Host->find( blacklist => 1,'order'=>'ip') ) {
 					my $macs = $Host->mac();
 					if ( $macs and @{$macs} ) {
 						foreach my $mac ( @{$macs} ) {
@@ -169,13 +171,17 @@ do{
 	# Without the multiline flag, will do one line at a time, nice.
 	$buf=~/<(\d+)>(.*?):(.*)/;
 	foreach my $re ( @re ) {
+
+		$log->debug("Line: $re") if $opts->{debug};
+
+
 		if ( $buf =~ /$re/ ) {
 			my ($when, $source) = ( $1, $2 );
-			print "match for $source\n" if $opts->{debug};
+			$log->debug( "match for $source") if $opts->{debug};
 			my ( $ip, $hostname );
 			if ( $source =~ /^\d+\.\d+\.\d+\.\d+$/ ) {
 # Is an IP
-				print "$source is an ip\n" if $opts->{debug};
+				$log->debug( "$source is an ip" ) if $opts->{debug};
 				$ip = $source;
 			} else {
 # is a hostname
@@ -183,15 +189,15 @@ do{
 				$ip = gethostbyname($source);
 				if ( defined $ip ) {
 					$ip = Socket::inet_ntoa($ip);
-					print "Got $ip for $source\n" if $opts->{debug};
+					$log->debug( "Got $ip for $source" ) if $opts->{debug};
 				} # end if
 			} # end if
 
 			if ( $ip and $whitelist{$ip} ) {
-				print "$ip is whitelisted\n" if $opts->{debug};
+				$log->debug( "$ip is whitelisted" ) if $opts->{debug};
 			} # end if
 			if ( ! $ip ) {
-				print "No ip for $source\n" if $opts->{debug};
+				$log->debug( "No ip for $source" ) if $opts->{debug};
 				next;
 			} # end if
 
@@ -212,7 +218,7 @@ do{
 				$host_counts{$ip}{'count'} += 1;
 				$host_counts{$ip}{'update'} = 1;
 			} else {
-				print "Not counting because too old " . $host_counts{$ip}{updated_on} . " >= $when" if $opts->{debug};
+				$log->debug( "Not counting because too old " . $host_counts{$ip}{updated_on} . " >= $when" ) if $opts->{debug};
 			} # end if
 			last;
 		} # end if line matches re
@@ -228,13 +234,13 @@ do{
 		if ( $_ ) {
 			print $_ . "\n";
 		} # end if
-		print "$ip $host_counts{$ip}{ip} $host_counts{$ip}{count}\n" if $opts->{debug};
+		$log->debug( "$ip $host_counts{$ip}{ip} $host_counts{$ip}{count}" ) if $opts->{debug};
 	} # end foreach ip
 
 } while(1);
 
 sub usage {
-    print <<EOH;
+	print <<EOH;
 
 usage: syslog.pl [--help] 
 
@@ -242,7 +248,7 @@ The purpose of this script is to monitor the AuthLog looking for attacks.
 
 Command-line options:
 
-    --help      Displays this message.
+	--help		Displays this message.
 
 EOH
 } # end sub usage
