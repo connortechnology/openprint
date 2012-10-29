b0232c7a52afb6d62be6b18351f2f92cfe2528c3
Isaac Connor <iconnor@connortechnology.com>
2012-10-29 10:24:21 -0400

    blah

--- perl/openprint/Photo_in_Album.pm
+++ perl/openprint/Photo_in_Album.pm
@@ -9,10 +9,14 @@ $debug = 0;
 $table = 'photos_in_albums';
 $serial = 'photos_in_albums_id_seq';
 %fields = (
-	'id'		=>	'id',
-	'album_id'	=>	'album_id',
-	'asset_id'	=>	'asset_id',
-	'keywords'	=>	undef,
+	id			=>	'id',
+	album_id	=>	'album_id',
+	asset_id	=>	'asset_id',
+	keywords	=>	undef,
+);
+%defaults = (
+	album_id	=>	undef,
+	asset_id	=>	undef,
 );
 
 
