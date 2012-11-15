54e3a3f54646e6d87cb7136e488de176eca10898
root <root@www4.internal.point-one.com>
2012-11-15 16:27:32 -0500

    blah

--- perl/openprint/employee_production.pm
+++ perl/openprint/employee_production.pm
@@ -49,7 +49,7 @@ sub jobs_by_csr {
 } # end sub jobs_by_csr
 
 sub _jobs_by_csr {
-	my @params = ( 'Equipment', 'category_id', 'salesrep_id','status',
+	my @params = ( 'Equipment', 'category_id', 'salesrep_id','status','show_feedback',
 			( map { 'schedule_start_'.$_ } ( 'year','month','day' ) ),
 			( map { 'schedule_end_'.$_ } ( 'year','month','day' ) ),
 		);
