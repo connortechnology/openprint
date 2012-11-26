e73b8fc52e77e7e6946211106d5779da54be5049
Isaac Connor <iconnor@connortechnology.com>
2012-11-25 20:26:41 -0500

    blah

--- perl/openprint/Comment.pm
+++ perl/openprint/Comment.pm
@@ -29,6 +29,7 @@ $serial = 'comments_id_seq';
 	'approved'		=>	0,
 );
 sub Object {
+	require $_[0]->object_type().'.pm';
 	return $_[0]->object_type()->new( $_[0]{'object_id'} );
 } # end sub Object
 
