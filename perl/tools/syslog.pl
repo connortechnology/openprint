1f2e1077207b95e483690692ea28f97d1a5f63ec
root <root@mail1.point-one.com>
2012-11-28 12:05:39 -0500

    blah

--- perl/tools/syslog.pl
+++ perl/tools/syslog.pl
@@ -40,17 +40,15 @@ $log = new logger('level'=>'debug');
 if (my $err = configuration::from_file('/etc/openprint/syslog.conf')) {
 	die $err;
 } # end if
+configuration::merge($opts);
 
-foreach my $param ( 'db_name','db_user','db_pass', ) {
-	$config{$param} = $$opts{$param} if $$opts{$param};
-	if ( ! $config{$param} ) {
-		die "$program: missing required --$param parameter";
-	}
+foreach my $param ( 'db_name','db_user','db_pass' ) {
+	die "$program: missing required --$param parameter" if ! $config{$param};
 } # end foreach required-param
 
-configuration::merge($opts);
-$config{port} = 10514 if ! $config{port};
-
+my %defaults = (
+	port	=>	10514,
+);
 
 $log = logger->new( {'file'=>$config{'log_file'}, 'level'=>$config{'log_level'}} );
 $log->info("Opening SQL connection");
@@ -62,9 +60,10 @@ $dbh = sql::open_sql( $log,
 	password	=> $config{db_pass},
 );
 die "Couldn't connect to db: $$dbh{errstr}" if ! $dbh;
-configuration::init( \%config );
+configuration::init();
 configuration::from_file('/etc/openprint/syslog.conf');
 configuration::merge($opts);
+configuration::merge_defaults(\%defaults);
 
 my @re = (
 		'^(\w{3} [ :0-9]{11}) [\._a-zA-Z0-9\-]+ sshd\[[0-9]+\]: pam_\w+\(sshd:auth\): authentication failure; logname= uid=0 euid=0 tty=ssh ruser= rhost=([\._a-zA-Z0-9\-]+)\s*$',
@@ -122,9 +121,10 @@ do{
 			sleep(10);
 			next;
 		} # end if
-		configuration::init( \%config );
+		configuration::init( );
 		configuration::from_file('/etc/openprint/syslog.conf');
 		configuration::merge($opts);
+		configuration::merge_defaults(\%defaults);
 	} # end if
 	
 	my %host_counts;
