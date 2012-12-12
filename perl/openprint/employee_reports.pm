d2076ee3428a41f11497e36d7e0101437346d2bc
root <root@www4.internal.point-one.com>
2012-12-12 13:06:04 -0500

    blah

--- perl/openprint/employee_reports.pm
+++ perl/openprint/employee_reports.pm
@@ -5,6 +5,7 @@ require ssi;
 require openprint::Company;
 require openprint::Project;
 require openprint::Project_Log;
+require openprint::Order_Status;
 
 use openprint qw();
 use vars qw( $r $log $dbh %variable %session %param );
@@ -285,6 +286,70 @@ sub _order_history_results {
 	} # end if
 	%{$variable{'Companies'}} = %companies;
 }
+
+sub order_performance {
+} # end sub order_performance
+
+sub _order_performance {
+	my %parameters; 
+	if ( ( $session{'user_type'} ne 'A' ) and ! openprint::usergroup::is_user_in( ['Sales Admin','Reporting','Accounting'], $session{'user_id'} ) ) {
+		$parameters{'SalesPerson'} = $session{'user_id'};
+		$parameters{'or'} = "Index=(SELECT CompanyIndex FROM Users WHERE Index=$session{'user_id'})";
+	} elsif ( $param{'CSR'} ) {
+		$parameters{'SalesPerson'} = $param{'CSR'};
+	} # end if
+	#$parameters{'order'} = 'lower(strname)';
+	my @Companies = openprint::Company::find( %parameters );
+	my %companies = map { int($_->id()), $_->name() } @Companies;
+	@{$variable{Orders}} = ();
+	if ( %companies ) {
+		foreach my $Order ( openprint::Order->find(
+			'company_id' => ( ($param{'company_id'} and exists $companies{$param{'company_id'}} ) ? $param{'company_id'} : [ keys %companies ] ),
+			ssi::date_filter( 'created_on_start', 'created_on_start', \%param ),
+			ssi::date_filter( 'created_on_end', 'created_on_end', \%param ),
+			( $param{'status'} ? (
+				'status' =>
+				( ref $param{'status'} eq 'ARRAY' ? $param{'status'} : [ split(',', $param{'status'} ) ] )
+				) : () ),
+			'order' => ($param{'order'} ? $param{'order'} : 'Index'),
+			'user_id' => ($param{'Estimator'} eq 'Non Employee' ? q{NOT IN (SELECT Index FROM Users WHERE chrType IN ('E','A') AND Index IN (SELECT user_id FROM users_in_usergroups WHERE usergroup_id = (SELECT id FROM usergroups WHERE name='Sales')))} : $param{'Estimator'}),
+		) ) {
+			if ( $param{'reprint'} ) {
+				my $reprint = 0;
+				foreach my $Project ( $Order->Projects() ) {
+					if ( $Project->reprint() eq 'Y' ) {
+						$reprint=1;
+						last;
+					} # end if
+				} # end foreach Project
+				next if ( $param{'reprint'} eq 'Y' ) and ! $reprint;
+				next if ( $param{'reprint'} eq 'N' ) and $reprint;
+			} # end if reprint
+			if ( $param{'press_id'} ) {
+				my $Press = new openprint::Equipment( $param{'press_id'} );
+				my $on_press = 0;
+				foreach my $Project ( $Order->Projects() ) {
+					foreach my $sig_id ( $Project->signatures() ) {
+						my $sig_specs = openprint::service::get_specs_ref( $Project, $sig_id );
+						if ( ! $$sig_specs{'UsePress'} ) {
+							$$sig_specs{'UsePress'} = $$sig_specs{'ddmPress'.$Project->ordered_quantity_index()};
+						} # end if
+						if ( $$sig_specs{'UsePress'} eq $Press->strid() ) {
+							$on_press = 1;
+						} # end if
+						last if $on_press;
+					} # end foreach sig
+					last if $on_press;
+				} # end foreach Project
+				next if ! $on_press;
+			} # end if
+			push @{$variable{'Orders'}}, $Order;
+		} # end foreach Order
+	} else {
+		$variable{'error'} .= 'There were no companies to filter on.<br/>';
+	} # end if
+	%{$variable{'Companies'}} = %companies;
+} # end sub _order_performance
 
 sub stock {
 	_stock();
