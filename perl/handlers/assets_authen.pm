9aae248c7d5113e2a5d7192efdc0b63be1aa26b7
root <root@server1.connortechnology.com>
2013-01-16 21:43:56 -0500

    blah

--- perl/handlers/assets_authen.pm
+++ perl/handlers/assets_authen.pm
@@ -1,10 +1,9 @@
 use strict;
-package handlers::assets;
+package handlers::assets_authen;
 
 use Apache2::Request ();
 use Apache2::RequestRec ();
-use Apache2::Const -compile => qw(REDIRECT HTTP_INTERNAL_SERVER_ERROR OK DECLINED HTTP_NOT_FOUND HTTP_FORBIDDEN);# Offers OK, Error,etc for web server.
-use APR::Const   -compile => 'SUCCESS';
+use Apache2::Const -compile => qw(HTTP_UNAUTHORIZED OK HTTP_NOT_FOUND);# Offers OK, Error,etc for web server.
 use Apache2::Log ();
 use Time::HiRes qw{ time gettimeofday tv_interval }; 
 
@@ -25,17 +24,10 @@ use vars qw( $r %session %config $log $dbh );
 use constant DEBUG => 0;
 
 sub cleanup {
-    if ( $r->connection->aborted( ) ) {
-$log->debug("Was aborted");
-    } else {
-#$log->debug("cleanup");
-    } # end if
     if ( $dbh ) {
         $session{lastupdated} = time;
         untie %session;
         $dbh->disconnect();
-	} else {
-$log->error("No dbh in cleanup");
     } # end if
 } # end sub cleanup
 
@@ -70,7 +62,6 @@ sub handler {
 
 		# The asset filename form is id_title.extension, path is either assets or thumbnails
 		my ( $path, $id, $filename ) = $r->uri() =~ /^\/(.*)\/(\d+)_(.+)$/;
-$log->debug("Path: $path $id uri:" . $r->uri());
 		$path =~ s/^assets\///;
 		if ( $id ) {
 			my $Asset = new openprint::Asset( $id );
@@ -83,46 +74,13 @@ $log->debug("Path: $path $id uri:" . $r->uri());
 							last;
 						} # end if
 					} # end foreach Album
-					if ( 1 or $can_view ) {
-						#$r->headers_out->set('Last-Modified'=>Date::Format::time2str( '%a, %d %b %Y %H:%M:%S %Z', Date::Parse::str2time( $Asset->updated_on() ) ));
-						if ( $path eq 'thumbnails' ) {
-							$r->sendfile( $Asset->thumbnail_path() );
-						} elsif ( $path eq 'medium' ) {
-							$r->sendfile( $Asset->medium_path() );
-						} elsif ( $path eq 'large' ) {
-							$r->sendfile( $Asset->large_path() );
-						} elsif ( $path eq 'small' ) {
-							$r->sendfile( $Asset->small_path() );
-						} elsif ( $path eq 'videos' ) {
-							if ( -e $config{AssetPath}.'videos/'.$id.'_'.$filename ) {
-								#$r->content_type( $Asset->content_type( $id.'_'.$filename ) );
-#$r->rflush;
-								$log->debug('Sending ' . $config{AssetPath}.'videos/'.$id.'_'.$filename );
-								$return_code = $request->sendfile( $config{AssetPath}.'videos/'.$id.'_'.$filename );
-$log->debug( "sendfile has failed" ) unless $return_code == APR::Const::SUCCESS;
-$log->debug("Return code: $return_code");
-							} else {
-								$log->error("DOes not exist at: " . $config{AssetPath}.'videos/'.$id.'_'.$filename );
-							} # en dif
-						} else {
-$log->debug("Sending ... " . $Asset->on_disk_path() );
-							$r->sendfile( $Asset->on_disk_path() );
-						} # end if
-$log->error( "Eval error sending image Reason: " . $@ ) if $@;
+					if ( $can_view ) {
+						return Apache2::Const::OK;
 					} else {
-$log->error("FORBIDDEN");
-						$return_code = Apache2::Const::HTTP_FORBIDDEN;
+						return Apache2::Const::HTTP_UNAUTHORIZED;
 					} # end if
 				} else {
-					$r->headers_out->set('Last-Modified'=>Date::Format::time2str( '%a, %d %b %Y %H:%M:%S %Z', Date::Parse::str2time( $Asset->updated_on() ) ));
-					if ( $path eq 'thumbnails' ) {
-						$r->sendfile( $Asset->thumbnail_path() );
-					} elsif ( $path eq 'medium' ) {
-						$r->sendfile( $Asset->medium_path() );
-					} else {
-# No album means has to be an article image, or a generic site image.
-						$r->sendfile( $Asset->on_disk_path() );
-					} # end if
+					return Apache2::Const::OK;
 				} # end if
 			} else {
 $log->error("NOT FOUND");
