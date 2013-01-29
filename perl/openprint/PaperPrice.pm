7ba762bfacd6e428d911bdca6e5f38e15770b662
Isaac Connor <pseudo.connortechnology.com>
2013-01-29 13:32:31 -0500

    update to fineuploader

--- perl/openprint/PaperPrice.pm
+++ perl/openprint/PaperPrice.pm
@@ -2,7 +2,7 @@ use strict;
 package openprint::PaperPrice;
 our @ISA = qw(openprint::Object);
 
-require Math::Round::nearest;
+require Math::Round;
 
 use vars qw( $debug $table $serial %find_fields %fields %transforms %defaults );
 $debug = 0;
