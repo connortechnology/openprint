f8f9cd8c54d385c9310470d4f7ce8f76787057ae
root <root@www4.internal.point-one.com>
2013-03-26 15:35:32 -0400

    turn off debuggging

--- perl/openprint/PaperAllocation.pm
+++ perl/openprint/PaperAllocation.pm
@@ -23,7 +23,7 @@ require openprint::Manufacturer;
 require openprint::Email;
 require openprint::InventoryCondition;
 
-$debug = 1;
+$debug = 0;
 
 $table = 'paper_allocations';
 $serial = 'paper_allocation_id_seq';
