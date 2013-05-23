d1d32f011126c4bb352d32c69cb3410a22ffa7e2
root <root@mail.topknotch.com>
2013-05-23 10:58:00 -0400

    blah

--- perl/openprint/ProjectTypeCategory.pm
+++ perl/openprint/ProjectTypeCategory.pm
@@ -19,5 +19,19 @@ $serial = 'ProjectType_Categories_id_seq';
 	'sort'	=>	undef,
 );
 
+sub ProjectTypes {
+	my %params;
+	if ( @_ > 1 ) {
+		if ( ref $_[1] eq 'HASH' ) {
+			%params = %{$_[1]}
+		} else {
+			shift @_;
+			%params = @_;
+		} # end if
+	} # en dif	
+	$params{category_id} = $_[0]{id};
+	return openprint::ProjectType->find(%params);
+} # end sub ProjectTypes
+
 1;
 __END__
