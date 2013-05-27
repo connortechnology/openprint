6426dc6055892970eacee073f411fcd47cc83cc7
root <root@www4.internal.point-one.com>
2013-05-27 11:25:05 -0400

    show equipment strid as well as name at top of press schedule only show presses when not in prending approved, pending

--- perl/openprint/ScheduledJob.pm
+++ perl/openprint/ScheduledJob.pm
@@ -447,7 +447,7 @@ sub get_li {
 				$html .= qq` $day</span>`;
 		} # end if
 		if ( sets::isin( $$self{'servicetype_id'}, \@printing_service_type_ids ) ) {
-			my @presses = sort( sets::union( map { $_->equipment_id() ? $_->Equipment()->strid() : () } find( project_id=>$$self{project_id}, servicetype_id=>\@printing_service_type_ids ) ) );
+			my @presses = sort( sets::union( map { $_->equipment_id() ? $_->Equipment()->strid() : () } find( project_id=>$$self{project_id}, servicetype_id=>\@printing_service_type_ids, 'starttime_null' => 0 ) ) );
 			$html .= '<span class="Presses">'.join(' + ', @presses ).'</span>' if @presses > 1;
 		} # end if
 
