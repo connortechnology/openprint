73df088b2242d4d56b456d671536d5c203379f32
root <root@server1.connortechnology.com>
2013-06-07 11:43:22 -0400

    updates

--- perl/tools/db_update.pl
+++ perl/tools/db_update.pl
@@ -418,7 +418,7 @@ if ( ! sets::isin( 'photo_albums', \@tables ) ) {
 } # end if
 
 if ( ! sets::isin( 'invoices', \@tables ) ) {
-	$dbh->do( misc::load_file( $log, q{../openprint/sql/Invoices.sql}) );
+	$dbh->do( misc::load_file( $log, q{../openprint/sql/Invoices.sql}) ) or die $dbh->errstr();
 } else {
 	my $data = $openprint::dbh->selectall_hashref( "SELECT column_name, data_type, column_default, is_nullable FROM information_schema.columns WHERE table_name='invoices'", 'column_name');
 	if ( ! exists $$data{num} ) {
