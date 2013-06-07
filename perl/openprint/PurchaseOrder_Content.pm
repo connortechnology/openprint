c6e00504544cb8521949bd0f271d5a0c4acf7743
root <root@server1.connortechnology.com>
2013-06-07 11:19:45 -0400

    blah

--- perl/openprint/PurchaseOrder_Content.pm
+++ perl/openprint/PurchaseOrder_Content.pm
@@ -138,7 +138,7 @@ sub Order {
 
 sub Orders {
 	my @dockets = split( /\D/, $_[0]{docket} );
-	return openprint::Order->find(docket=>\@dockets) if @dockets
+	return openprint::Order->find(docket=>\@dockets) if @dockets;
 	return ();
 } # end sub Orders
 
