9ed34b4b7e18d0be4605ec1771961bc2fcb93b5f
root <root@www1.internal.point-one.com>
2013-06-11 18:07:36 -0400

    blah

---
+++ perl/tools/redo_log.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/perl
+use lib '/etc/apache2/lib/perl';
+use strict;
+
+require sql;
+require logger;
+require openprint::Object;
+require openprint::Skid;
+require openprint::ManifestContent;
+require openprint::StockFinish;
+require openprint::Paper;
+require openprint::PaperInventory;
+require openprint::Manifest_Content_Type;
+
+use openprint ();
+use vars qw( $log $dbh );
+*log = \$openprint::log;
+*dbh = \$openprint::dbh;
+
+$log = new logger( 'debug' );
+
+$openprint::Object::no_cache = 1;
+$dbh = sql::open_sql( $log, ('database'=>$ARGV[0], 'driver'=>'Pg','login'=>$ARGV[1], 'password'=>$ARGV[2], 'host'=>'database') );
+
+while ( my $line = <STDIN> ) {
+	if ( $line =~ /INSERT INTO PaperFinishes \(longname,shortname\) VALUES \(([^,]+),.+\)/ ) {
+		next if sql::execute(undef,undef, 'SELECT * FROM PaperFinishes WHERE longname=?', $1 );
+		my $name = $1;
+		$line = <STDIN>;
+		my ( $id ) = $line =~ /\(SELECT id,longname FROM PaperFinishes WHERE longname=$name\) Results:(\d+),$name/;
+		die if ! $id;
+		my $Finish = new openprint::StockFinish();
+		$_ = $Finish->save({id=>$id, shortname=>$name, longname=>$name}, 1);
+		die $_ if $_;
+	} elsif ( $line =~ /INSERT INTO PaperWeights \(longname,shortname\) VALUES \(([^,]+),.+\)/ ) {
+		next if sql::execute(undef,undef, 'SELECT * FROM PaperWeights WHERE longname=?', $1 );
+		my $name = $1;
+		$line = <STDIN>;
+		my ( $id ) = $line =~ /\(SELECT id,longname FROM PaperWeights WHERE longname=$name\) Results:(\d+),$name/;
+		die if ! $id;
+		my $Weight = new openprint::StockWeight();
+		$_ = $Weight->save({id=>$id, shortname=>$name, longname=>$name}, 1);
+		die $_ if $_;
+	} elsif ( $line =~ /INSERT INTO PaperColours \(longname,shortname\) VALUES \(([^,]+),.+\)/ ) {
+		next if sql::execute(undef,undef, 'SELECT * FROM PaperColours WHERE longname=?', $1 );
+		my $name = $1;
+		$line = <STDIN>;
+		my ( $id ) = $line =~ /\(SELECT id,longname FROM PaperColours WHERE longname=$name\) Results:(\d+),$name/;
+		die if ! $id;
+		my $Colour = new openprint::StockColour();
+		$_ = $Colour->save({id=>$id, shortname=>$name, longname=>$name}, 1);
+		die $_ if $_;
+	} elsif ( $line =~ /SELECT nextval\('paper_id_seq'\)\) Results:(\d+)/ ) {
+		my $Paper = new openprint::Paper( $1 );
+		next if $Paper->id();
+		$line = <STDIN>;
+		my ( $fields, $values ) = $line =~ /INSERT INTO Papers \(([^\)]+)\) VALUES \(([^\)]+)\)/;
+		die 'No fields in ' . $line if ! $fields;
+		my %sql;
+		@sql{split(',', $fields)} = map { $_ eq 'undef' ? undef : $_ } split(',',$values);
+		sql::insert( undef, undef, 'papers', %sql );
+	} elsif ( $line =~ /INSERT INTO paper_inventory \(skid_id,updated_on,id,docket,delta,user_id,paper_id,instock,poindex,comment,units\) VALUES \(\d+,NOW\(\),(\d+),[^,]+,[^,]+,\d+,(\d+),[^,]+,[^,]+,[^,]+,[^,]+\)\)/ ) {
+		my ( $id, $paper_id ) = ($1, $2);
+		my $PI = new openprint::PaperInventory( $id );
+		if ( ! $PI->id() ) {
+			die 'PI not foudn for id ' . $id;
+		} elsif ( ! $PI->paper_id() ) {
+			sql::update( undef, undef, 'paper_inventory', [ 'id=?', $id ], 'paper_id', $paper_id );
+		} elsif ( $PI->paper_id() != $paper_id ) {
+			die "PI has different paper_id $$PI{paper_id} != $paper_id";
+		} # end if
+	} elsif ( $line =~ /UPDATE Skid_Contents SET purpose_id = ,skid_id = \d+,quantity = \d+,id = (\d+),condition_id = ,paper_id = (\d+),units =  WHERE id=\d+/ ) {
+		my ( $id, $paper_id ) = ($1, $2);
+		my $PI = new openprint::SkidContent( $id );
+		if ( ! $PI->id() ) {
+			die 'SC not foudn for id ' . $id;
+		} elsif ( ! $PI->paper_id() ) {
+			sql::update( undef, undef, 'skid_Contents', [ 'id=?', $id ], 'paper_id', $paper_id );
+		} elsif ( $PI->paper_id() != $paper_id ) {
+			$log->debug( "SC has different paper_id $$PI{paper_id} != $paper_id" );
+		} # end if
+	} elsif ( $line =~ /UPDATE manifest_content_types SET po_id = \d+,id = (\d+),po_content_id = ,cost = ,manifest_id = \d+,docket = \d+,type = [^,]+,paper_id = (\d+),manufacturers_name = ,item_count = ,supplier_invoice =  WHERE id=\d+/ ) {
+		my ( $id, $paper_id ) = ($1, $2);
+		my $PI = new openprint::Manifest_Content_Type( $id );
+		if ( ! $PI->id() ) {
+			die 'MCT not foudn for id ' . $id;
+		} elsif ( ! $PI->paper_id() ) {
+			sql::update( undef, undef, 'manifest_Content_types', [ 'id=?', $id ], 'paper_id', $paper_id );
+		} elsif ( $PI->paper_id() != $paper_id ) {
+			$log->debug( "MCT has different paper_id $$PI{paper_id} != $paper_id" );
+		} # end if
+	
+	} # end if
+}
+
+$dbh->disconnect();
+1;
+__END__
