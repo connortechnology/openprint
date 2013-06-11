8360abf1bdb9b26f7190c42528db2f19f4cbf971
root <root@mail2.topknotch.com>
2013-06-11 14:25:52 -0400

    make helpdesk look on itself This fixes issues with the back button and captcha

--- perl/openprint/support.pm
+++ perl/openprint/support.pm
@@ -100,99 +100,99 @@ sub confirmation_returns {
 } # end sub rma
 
 sub help_desk {
-} # end sub help_desk
 
-sub confirmation_help_desk {
-	my ( $r, $log, $dbh, $variable ) = @_;
-
-	my $error = '';
-	$error .= 'Missing First Name<br/>' if $openprint::param{'txtFirstName'} eq '';
-	$error .= 'Missing Last Name<br/>' if $openprint::param{'txtLastName'} eq '';
-	$error .= 'Missing Address<br/>' if $openprint::param{'txtAddress1'} eq '';
-	$error .= 'Missing City<br/>' if $openprint::param{'txtCity'} eq '';
-	$error .= 'Missing Postal Code<br/>' if $openprint::param{'txtPostalCode'} eq '';
-	$error .= 'Missing Phone<br/>' if $openprint::param{'txtPhone'} eq '';
-	$error .= 'Missing/Invalid E-mail<br/>' if ( ! $openprint::param{'txtEmail'} ) or ( ! Email::Valid->address( $openprint::param{'txtEmail'} ) );
-	$error .= 'Missing Question or Comment<br/>' if $openprint::param{'txtQuestion-Quote'} eq '';
-	if ( $config{'UseCaptchaOnRegistration'} eq 'Y' ) {
-		# Remove spaces, because some people want to put spaces between the characters, etc.
-		$openprint::param{'Captcha'} =~ s/\s//g;
-		require Authen::Captcha;
-		my $Captcha = new Authen::Captcha('data_folder' => '/tmp', 'output_folder' => $config{'SkinPath'}.'/images/captcha');
-		if ( 1 != $Captcha->check_code( $openprint::param{'Captcha'}, $openprint::param{'MD5SUM'} ) ) {
-			$error .= 'Validation Code incorrect.  Please try again.';
+	if ( $param{btnSubmit} ) {
+		my $error = '';
+		$error .= 'Missing First Name<br/>' if $openprint::param{'txtFirstName'} eq '';
+		$error .= 'Missing Last Name<br/>' if $openprint::param{'txtLastName'} eq '';
+		$error .= 'Missing Address<br/>' if $openprint::param{'txtAddress1'} eq '';
+		$error .= 'Missing City<br/>' if $openprint::param{'txtCity'} eq '';
+		$error .= 'Missing Postal Code<br/>' if $openprint::param{'txtPostalCode'} eq '';
+		$error .= 'Missing Phone<br/>' if $openprint::param{'txtPhone'} eq '';
+		$error .= 'Missing/Invalid E-mail<br/>' if ( ! $openprint::param{'txtEmail'} ) or ( ! Email::Valid->address( $openprint::param{'txtEmail'} ) );
+		$error .= 'Missing Question or Comment<br/>' if $openprint::param{'txtQuestion-Quote'} eq '';
+		if ( $config{'UseCaptchaOnRegistration'} eq 'Y' ) {
+# Remove spaces, because some people want to put spaces between the characters, etc.
+			$openprint::param{'Captcha'} =~ s/\s//g;
+			require Authen::Captcha;
+			my $Captcha = new Authen::Captcha('data_folder' => '/tmp', 'output_folder' => $config{'SkinPath'}.'/images/captcha');
+			if ( 1 != $Captcha->check_code( $openprint::param{'Captcha'}, $openprint::param{'MD5SUM'} ) ) {
+				$error .= 'Validation Code incorrect.  Please try again.';
+			} # end if
 		} # end if
+
+		if ( $error ) {
+			$variable{error} = $error;
+			return;
+		} # end if
+
+		my ( $index ) = sql::execute( $log, $dbh, q{SELECT nextval('HelpDesk_Id_seq')} );
+		if ( ! $index ) {
+			return misc::error( $log, $dbh, \%variable, 'System Error', 'Unable to create helpdesk entry.' );
+		} # end if
+
+		sql::insert( $log, $dbh, 'Helpdesk',
+				'Id', $index,
+				'company_id', $openprint::session{'company_id'},
+				'user_id',     $openprint::session{'user_id'},
+				'strCompanyName',	$openprint::param{'txtCompanyName'},
+				'strTitle',			$openprint::param{'txtTitle'},
+				'strFirstName',		$openprint::param{'txtFirstName'},
+				'strLastName',		$openprint::param{'txtLastName'},
+				'strAddress',		$openprint::param{'txtAddress1'},
+				'strAddress2',		$openprint::param{'txtAddress2'},
+				'strCity',			$openprint::param{'txtCity'},
+				'strStateProv',		$openprint::param{'ddmStateProvince'},
+				'strPostalCode',	$openprint::param{'txtPostalCode'},
+				'strCountry',		$openprint::param{'ddmCountry'},
+				'strPhone',			$openprint::param{'txtPhone'},
+				'strExtension',		$openprint::param{'txtExtension'},
+				'strEmail',			$openprint::param{'txtEmail'},
+				'blbdescription',	$openprint::param{'txtQuestion-Quote'},
+				'chrMethod',		$openprint::param{'rdbMethod'},
+				'dtmRequestDate',	'NOW()',
+				);
+
+		my %info = ( 
+				'HelpDeskIndex' => $index,
+				'txtSalutation'	=>	$openprint::param{'rdbSalutation'},
+				'txtFirstName'	=>	$openprint::param{'txtFirstName'},
+				'txtLastName'	=>	$openprint::param{'txtLastName'},
+				);
+
+		foreach my $key ( keys %openprint::param ) {
+			$info{$key} = $openprint::param{$key};
+		} # end foreach
+
+		$info{'SecureSiteURL'} = $r->dir_config('ExternalSecureSiteURL');
+		$info{'siteURL'} = $r->dir_config('ExternalSiteURL');
+
+
+		my $template = misc::load_file( $log, $ENV{'DOCUMENT_ROOT'}.'/email_content/helpdesk_notification.html' );
+		$template = ssi::variable_substitution( \$template, \%info );
+
+		my %mail = (
+				SMTP	=> $config{'Mail Server'},
+				FROM	=> sprintf('"%s %s" <%s>', @openprint::param{'txtFirstName','txtLastName','txtEmail'} ),
+				TO		=> $config{'HelpdeskEmail'},
+				SUBJECT => 'Online Helpdesk Submission.'
+				);
+		misc::send_email_with_attachment( $log, \%mail, ( '', encode_qp($template), 'text/html', 'quoted-printable' ) );
+
+		$info{'ReplacementText'} = misc::load_file( $log, $ENV{'DOCUMENT_ROOT'} . '/email_content/helpdesk_confirmation.html' );
+		$info{'ReplacementText'} = ssi::variable_substitution( \$info{'ReplacementText'}, \%info );
+		my $email_template = misc::load_file( $log, $config{'SkinPath'}. '/email_template.html' );
+		$email_template = ssi::variable_substitution( \$email_template, \%info );
+
+		my %mail = (
+				SMTP	=> $config{'Mail Server'},
+				TO		=> sprintf('"%s %s" <%s>', @openprint::param{'txtFirstName','txtLastName','txtEmail'} ),
+				FROM	=> $config{'HelpdeskEmail'},
+				SUBJECT => 'Online Helpdesk Submission.'
+				);
+		misc::send_email_with_attachment( $log, \%mail, ( '', encode_qp($email_template), 'text/html', 'quoted-printable' ) );
+		%param = ();
 	} # end if
-
-	if ( $error ) {
-		return misc::error( $log, $dbh, $variable, 'Bad Field', $error );
-	} # end if
-
-    my ( $index ) = sql::execute( $log, $dbh, q{SELECT nextval('HelpDesk_Id_seq')} );
-    if ( ! $index ) {
-        return misc::error( $log, $dbh, $variable, 'System Error', 'Unable to create helpdesk entry.' );
-    } # end if
-
-	sql::insert( $log, $dbh, 'Helpdesk',
-			'Id', $index,
-			'company_id', $openprint::session{'company_id'},
-			'user_id',     $openprint::session{'user_id'},
-			'strCompanyName',	$openprint::param{'txtCompanyName'},
-			'strTitle',			$openprint::param{'txtTitle'},
-			'strFirstName',		$openprint::param{'txtFirstName'},
-			'strLastName',		$openprint::param{'txtLastName'},
-			'strAddress',		$openprint::param{'txtAddress1'},
-			'strAddress2',		$openprint::param{'txtAddress2'},
-			'strCity',			$openprint::param{'txtCity'},
-			'strStateProv',		$openprint::param{'ddmStateProvince'},
-			'strPostalCode',	$openprint::param{'txtPostalCode'},
-			'strCountry',		$openprint::param{'ddmCountry'},
-			'strPhone',			$openprint::param{'txtPhone'},
-			'strExtension',		$openprint::param{'txtExtension'},
-			'strEmail',			$openprint::param{'txtEmail'},
-			'blbdescription',	$openprint::param{'txtQuestion-Quote'},
-			'chrMethod',		$openprint::param{'rdbMethod'},
-			'dtmRequestDate',	'NOW()',
-			);
-
-	my %info = ( 
-			'HelpDeskIndex' => $index,
-			'txtSalutation'	=>	$openprint::param{'rdbSalutation'},
-			'txtFirstName'	=>	$openprint::param{'txtFirstName'},
-			'txtLastName'	=>	$openprint::param{'txtLastName'},
-			);
-
-	foreach my $key ( keys %openprint::param ) {
-		$info{$key} = $openprint::param{$key};
-	} # end foreach
-
-    $info{'SecureSiteURL'} = $r->dir_config('ExternalSecureSiteURL');
-    $info{'siteURL'} = $r->dir_config('ExternalSiteURL');
-
-	
-	my $template = misc::load_file( $log, $ENV{'DOCUMENT_ROOT'}.'/email_content/helpdesk_notification.html' );
-	$template = ssi::variable_substitution( \$template, \%info );
-
-	my %mail = (
-			SMTP	=> $config{'Mail Server'},
-			FROM	=> sprintf('"%s %s" <%s>', @openprint::param{'txtFirstName','txtLastName','txtEmail'} ),
-			TO		=> $config{'HelpdeskEmail'},
-			SUBJECT => 'Online Helpdesk Submission.'
-			);
-	misc::send_email_with_attachment( $log, \%mail, ( '', encode_qp($template), 'text/html', 'quoted-printable' ) );
-
-	$info{'ReplacementText'} = misc::load_file( $log, $ENV{'DOCUMENT_ROOT'} . '/email_content/helpdesk_confirmation.html' );
-	$info{'ReplacementText'} = ssi::variable_substitution( \$info{'ReplacementText'}, \%info );
-	my $email_template = misc::load_file( $log, $config{'SkinPath'}. '/email_template.html' );
-    $email_template = ssi::variable_substitution( \$email_template, \%info );
-
-	my %mail = (
-		SMTP	=> $config{'Mail Server'},
-		TO		=> sprintf('"%s %s" <%s>', @openprint::param{'txtFirstName','txtLastName','txtEmail'} ),
-		FROM	=> $config{'HelpdeskEmail'},
-		SUBJECT => 'Online Helpdesk Submission.'
-		);
-	misc::send_email_with_attachment( $log, \%mail, ( '', encode_qp($email_template), 'text/html', 'quoted-printable' ) );
 
 } # end sub helpdesk
 
