37805689cec2d4d3eed686f8db0edf3d6f9a1675
root <root@www4.internal.point-one.com>
2013-07-05 13:28:39 -0400

    fix paper allocation from skid

--- perl/openprint/employee_inventory.pm
+++ perl/openprint/employee_inventory.pm
@@ -948,7 +948,10 @@ $log->debug('sacing');
 			} # end if
 		} # end if
 		foreach my $skid_id ( @skid_ids ) {
-			allocate( $skid_id, @param{'paper_id', 'Quantity','Project','Docket'} );
+			foreach my $condition_id ( sets::union( map { $_->condition_id() } openprint::SkidContent->find(skid_id=>$skid_id, paper_id=>$param{paper_id},'quantity >' =>0 ) ) ) {
+				next if ! $param{'quantity-'.$condition_id};
+				allocate( $skid_id, @param{'paper_id','quantity-'.$condition_id,'Project','Docket','specific','reason'}, $condition_id );
+			} # end foreach condition
 		} # end foreach
 	} elsif ( $param{'btnFunction'} eq 'Delete Allocation' ) {
 		if ( $param{'allocation_id'} ) {
