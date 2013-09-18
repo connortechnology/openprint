ecbce7bb1dcd1e7071cdc232138a34779d79037c
root <root@www4.internal.point-one.com>
2013-09-18 12:33:57 -0400

    fix alert responses to MultiPage. Add a check for more than 500 pages. Add check for more than 125 spreads, probably should be 250 or should check spread size.

--- perl/openprint/Estimating/Printing.pm
+++ perl/openprint/Estimating/Printing.pm
@@ -931,6 +931,10 @@ $openprint::log->debug("Grabbing UV Specs");
 # We only specify GF spreads 1 at a time.
 				$$specs{'txtUnspecifiedSpreadQuantity'.$qty_index} = 1;
 			} # end if
+			if ( $$specs{'txtUnspecifiedSpreadQuantity'.$qty_index} > 125 ) {
+				$$specs{'alert'} .= 'There are far too many pages required. We will not be able to calculate this.<br/>';
+				return $$specs{'Status'} = 'uncalculated';
+			} # end if
 
 		} # end if
 
