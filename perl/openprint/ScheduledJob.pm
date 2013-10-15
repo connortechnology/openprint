85df56fefabd50feeffe02d5f1cc416c8048a320
root <root@mail.topknotch.com>
2013-10-15 15:40:48 -0400

    blah

--- perl/openprint/ScheduledJob.pm
+++ perl/openprint/ScheduledJob.pm
@@ -461,14 +461,16 @@ sub impressions {
 		my $impressions = 0;
 
 		my $Project = $self->Project();
-		foreach my $sig_id ( $$self{pertains_id} ? @{$self->pertains_id()} : @{$self->service_id()} ) {
-			my $sig_specs = openprint::service::get_specs_ref( $Project, $sig_id );
-			if ( ! $$sig_specs{'ImpressionQuantity'} ) {
-				$$sig_specs{'ImpressionQuantity'} = $$sig_specs{'hdnImpressionQuantity'.$Project->ordered_quantity_index()};
-				openprint::service::insert_service_spec( $log, $dbh, $$self{'project_id'}, $sig_id, 'ImpressionQuantity', $$sig_specs{'ImpressionQuantity'} );
-			} # end if
-			$impressions += $$sig_specs{'ImpressionQuantity'};
-		} # end foreach sig
+		if ( $self->service_id() ) {
+			foreach my $sig_id ( $$self{pertains_id} ? @{$$self{pertains_id}} : @{$self->service_id()} ) {
+				my $sig_specs = openprint::service::get_specs_ref( $Project, $sig_id );
+				if ( ! $$sig_specs{'ImpressionQuantity'} ) {
+					$$sig_specs{'ImpressionQuantity'} = $$sig_specs{'hdnImpressionQuantity'.$Project->ordered_quantity_index()};
+					openprint::service::insert_service_spec( $log, $dbh, $$self{'project_id'}, $sig_id, 'ImpressionQuantity', $$sig_specs{'ImpressionQuantity'} );
+				} # end if
+				$impressions += $$sig_specs{'ImpressionQuantity'};
+			} # end foreach sig
+		} # end if
 		$$self{impressions} = $impressions;
 	} # end if
 	return $$self{impressions};
