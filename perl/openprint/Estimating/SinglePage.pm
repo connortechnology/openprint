45bf37adcf69002206dc9795caa0591695fc65ab
root <root@mail.topknotch.com>
2013-11-21 13:14:36 -0500

    bilah

--- perl/openprint/Estimating/SinglePage.pm
+++ perl/openprint/Estimating/SinglePage.pm
@@ -197,6 +197,7 @@ $openprint::log->debug( "Signature: @signatures");
 						my $Imposition = shift @{$$sig_specs{'Additional Impositions'.$qty_index}};
 						my $price = $$Imposition{price};
 
+						$Imposition->save( \%specs, $qty_index );
 						openprint::Estimating::Printing::save_price( $Project, \%specs, $price, $Imposition, $qty_index );
 						foreach my $k ( keys %{$price} ) {
 							$openprint::log->debug("Price: $k $$price{$k}");
