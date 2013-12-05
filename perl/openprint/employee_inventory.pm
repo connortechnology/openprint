099f4e8783c8aefa1283673cec8e9cb5bc6202ea
root <root@www4.internal.point-one.com>
2013-12-05 10:22:33 -0500

    blah

--- perl/openprint/employee_inventory.pm
+++ perl/openprint/employee_inventory.pm
@@ -14,6 +14,7 @@ require openprint::StockName;
 require openprint::StockFinish;
 require openprint::StockColour;
 require openprint::StockQuality;
+require openprint::StockPurpose;
 require openprint::InventoryCondition;
 require openprint::RFIDTag;
 require openprint::RFIDTagType;
