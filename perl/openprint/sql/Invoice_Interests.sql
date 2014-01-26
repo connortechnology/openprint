4969680ed6381b596cc1027978b8ef0f735cb4f6
root <root@mail.connortechnology.com>
2014-01-26 15:20:42 -0500

    blah

---
+++ perl/openprint/sql/Invoice_Interests.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE invoice_interests (
+    invoice_id integer NOT NULL,
+    description text,
+    amount numeric(10,2),
+    created_on timestamp with time zone DEFAULT now() NOT NULL,
+    updated_on timestamp with time zone DEFAULT now() NOT NULL,
+    id integer DEFAULT nextval('invoice_interests_id_seq'::regclass) NOT NULL,
+    compounded_on date
+);
+
+
+ALTER TABLE ONLY invoice_interests
+    ADD CONSTRAINT invoice_interests_pkey PRIMARY KEY (id);
+
+
+ALTER TABLE ONLY invoice_interests
+    ADD CONSTRAINT invoice_interest_invoice_id_fkey FOREIGN KEY (invoice_id) REFERENCES invoices(id);
+
+
