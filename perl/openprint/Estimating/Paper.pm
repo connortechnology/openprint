799aec4e05544eff44ca7ce7bbcdca27e88620b9
root <root@www6.internal.point-one.com>
2014-01-28 15:46:10 -0500

    add paper message to stock summary

--- perl/openprint/Estimating/Paper.pm
+++ perl/openprint/Estimating/Paper.pm
@@ -386,7 +386,7 @@ sub summary {
 		} # end foreach key
 		return \@summaries;
 	} # end if
-	return \@keys;
+	return [ map { $Papers{$_}->message() ? $_ . '<br/>' . $Papers{$_}->message() : $_ } @keys ];
 } # end sub summary
 
 sub save {
