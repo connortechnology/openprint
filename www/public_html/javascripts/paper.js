812b1129abd52357e03eb4d7dacf7d7e8f6937e7
root <root@www1.internal.point-one.com>
2014-02-04 15:11:50 -0500

    blah

--- www/public_html/javascripts/paper.js
+++ www/public_html/javascripts/paper.js
@@ -34,7 +34,7 @@ function filter_onChange( element, id, selected ) {
 			} // end if
         } // end if filter exists
     } // end for 
-    new Ajax.Request( '_stock.json', { parameters: h, evalScripts: true } );
+    new Ajax.Request( '/employee/inventory/_stock.json', { parameters: h, evalScripts: true } );
 } // end function Name_onChange()
 
 function cbStockFillResults( results ) {
