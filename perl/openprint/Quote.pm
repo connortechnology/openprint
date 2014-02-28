7ff3619f87a8ae9ef6ef186bc419e33a8f2e8c11
root <root@www6.internal.point-one.com>
2014-02-28 13:17:26 -0500

    blah

--- perl/openprint/Quote.pm
+++ perl/openprint/Quote.pm
@@ -77,7 +77,7 @@ $serial = 'quotes_id_seq';
 	);
 
 %find_fields = (
-	'salesrep_id' => '(SELECT lngsalespersion FROM companies WHERE id=company_id)',
+	'salesrep_id' => '(SELECT salesrep_id FROM companies WHERE id=companyindex)',
 	'for_name' => q{(SELECT strFirstName || ' ' || strLastName FROM tbl_Quote_Users_for WHERE quote_id=quotes.id)},
 );
 %defaults = (
