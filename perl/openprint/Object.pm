505f6d2852957df23d7722262064026139e8c6fc
root <root@www6.internal.point-one.com>
2014-03-13 15:33:49 -0400

    fix && operttor

--- perl/openprint/Object.pm
+++ perl/openprint/Object.pm
@@ -459,7 +459,7 @@ $log->debug("find_operators: field($field) type($type) op($operator) value($valu
 		return ( $field.$type.' ' . $operator . ' ?', $value );
 	} elsif ( sets::isin( $operator, [ '&&', '<@', '@>' ] ) ) {
 		if ( ref $value eq 'ARRAY' ) {
-			return ( $field.$type.' ' . $operator . ' ?', $value );
+			return ( 'ARRAY('.$field.$type.') ' . $operator . ' ?', $value );
 		} else {
 			return ( $field.$type.' ' . $operator . ' ?', [ $value ] );
 		} # end if
