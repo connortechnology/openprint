0554fd08c735cab031b00852760b0cb9cdd2ba1b
root <root@www6.internal.point-one.com>
2014-03-17 13:12:49 -0400

    use PaperAllocation->save instead of sql

--- perl/openprint/Skid.pm
+++ perl/openprint/Skid.pm
@@ -608,19 +608,20 @@ sub allocate {
 
 	my $Project = new openprint::Project( $project_id ) if $project_id;
 
-	my $ac = sql::start_transaction();
-	sql::insert( undef, undef, 'Paper_Allocations',
-			'skid_ids',		[ $$self{'id'} ],
-			'paper_id',		$paper_id,
-			'quantity',		1*$quantity,
-			'units',		$units,
+	my $ac = sql::start_transaction( $openprint::dbh );
+	my $PA = new openprint::PaperAllocation();
+	$PA->save({
+			skid_ids	=>	[ $$self{'id'} ],
+			paper_id	=>	$paper_id,
+			quantity	=>	1*$quantity,
+			units	=>	$units,
 			( $Project ? ( docket => $Project->docket() ): () ),
-			'operator_id',	$session{'user_id'},
-			);
+			operator_id	=>	$session{user_id},
+			} );
 	if ( $Project ) {
 	$Project->add_to_log( @session{'company_id','user_id'}, qq`Allocated $quantity $units on skid <a href="/employee/inventory/skid_details.html?skid_id=$$self{id}">$$self{id}</a>` );
 	} # end if
-	sql::end_transaction( undef, $ac );
+	sql::end_transaction( $openprint::dbh, $ac );
 } # end sub allocate
 
 sub empty {
