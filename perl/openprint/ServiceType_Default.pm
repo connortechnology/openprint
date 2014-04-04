a30c494c175c7fafe2784bbcd41e9339527b79f9
root <root@www1.internal.point-one.com>
2014-04-04 10:57:53 -0400

    blah

--- perl/openprint/ServiceType_Default.pm
+++ perl/openprint/ServiceType_Default.pm
@@ -4,7 +4,7 @@ our @ISA = qw(openprint::Object);
 
 use vars qw( $debug $table $serial %fields %find_fields %transforms %defaults );
 
-$debug = 1;
+$debug = 0;
 
 $table = 'tbl_service_defaults';
 $serial = 'tbl_Service_Defaults_lngID_seq';
