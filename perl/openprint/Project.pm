46d32e1739b7e2ccbf4c1c79b56d281756750cba
root <root@pseudo.connortechnology.com>
2014-04-29 14:50:22 -0400

    blah

--- perl/openprint/Project.pm
+++ perl/openprint/Project.pm
@@ -564,15 +564,17 @@ sub update_status {
 		} elsif ( sets::isin( 'calculated', \@statuses ) ) { # This works because we have already checked for uncalculated
 			$new_status = 'Unordered';
 			foreach my $qty_index ( $self->quantity_indexes() ) {
-				if ( openprint::Estimating::MultiPage::status( $$self{'id'}, undef, $qty_index ) ) {
-					$new_status = 'uncalculated';
-					last;
-				} # end if
-				my $ProjectService = $self->Service( $services{''}[0] );
-				if ( openprint::Estimating::MultiPage::check( $self, $ProjectService, $qty_index ) ) {
-					$ProjectService->status('uncalculated');
-					$new_status = 'uncalculated';
-					last;
+				if ( $self->Type()->type() eq 'MultiPage' ) {
+					if ( openprint::Estimating::MultiPage::status( $$self{'id'}, undef, $qty_index ) ) {
+						$new_status = 'uncalculated';
+						last;
+					} # end if
+					my $ProjectService = $self->Service( $services{''}[0] );
+					if ( openprint::Estimating::MultiPage::check( $self, $ProjectService, $qty_index ) ) {
+						$ProjectService->status('uncalculated');
+						$new_status = 'uncalculated';
+						last;
+					} # end if
 				} # end if
 			} # end foreach
 		} # end if
