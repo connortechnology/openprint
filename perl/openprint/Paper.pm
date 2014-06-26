71fe046b1ac341bb9619bb36ade8388bd798c0c2
root <root@mail.topknotch.com>
2014-06-26 12:07:44 -0400

    introduce Paper::sort

--- perl/openprint/Paper.pm
+++ perl/openprint/Paper.pm
@@ -1602,5 +1602,17 @@ sub link_to {
 	return sprintf('<a href="/employee/inventory/paper_details.html?paper_id=%1$d">%2$s</a>', $_[0]{id}, $_[0]->to_string() );
 } # end sub link_to
 
+sub sort {
+	sort { 
+		foreach my $option ( 'brand', 'finish', 'colour','weight' ) {
+			return $a->option() cmp $b->$option() if $a->$option() ne $b->$option();
+		} # end foreach option
+		foreach my $option ( 'width', 'height' ) {
+			return $a->option() <=> $b->$option() if $a->$option() != $b->$option();
+		} # end foreach option
+		
+	} @_;
+}
+
 1;
 __END__
