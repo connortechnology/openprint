a0616bb27d0803279c1688edfd5aa2d39343e303
root <root@testing.internal.point-one.com>
2014-07-23 22:01:31 -0400

    fixes add all auth

--- perl/openprint/ZM_Monitor.pm
+++ perl/openprint/ZM_Monitor.pm
@@ -26,13 +26,13 @@ $table = 'Monitors';
 
 sub source_stream_url {
 	return $_[0]{'type'} eq 'Remote' ? 'http://'.$_[0]{'host'}.$_[0]{'path'} :
-                          sprintf('http://%2$s.internal.point-one.com/cgi-bin/zms?mode=jpeg&amp;monitor=%1$d&amp;maxfps=%3$d',
+                          sprintf('http://%2$s/cgi-bin/zms?mode=jpeg&amp;monitor=%1$d&amp;maxfps=%3$d&amp;user=all',
                               $_[0]{'id'}, $_[0]{'server_host'}, $_[0]{'max_fps'} );
 } # end sub source_stream_url
 
 sub source_snapshot_url {
 	return $_[0]{'type'} eq 'Remote' ? 'http://'.$_[0]{'host'}.($_[0]{'jpg_path'}?$_[0]{'jpg_path'}:$_[0]{'path'}) :
-                          sprintf('http://%2$s.internal.point-one.com/cgi-bin/zms?mode=single&amp;monitor=%1$d&amp;maxfps=%3$d',
+                          sprintf('http://%2$s/cgi-bin/zms?mode=single&amp;monitor=%1$d&amp;maxfps=%3$d&amp;user=all',
                               $_[0]{'id'}, $_[0]{'server_host'}, $_[0]{'max_fps'} );
 } # end sub source_snapshot_url
 
