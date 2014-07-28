282b868e8c0ca0abdea4f938fbd25d0202062431
root <root@mail1.point-one.com>
2014-07-28 11:12:22 -0400

    fix, add sig ahdnler and maybe stats to rfid_server

--- perl/openprint/Skid.pm
+++ perl/openprint/Skid.pm
@@ -579,7 +579,7 @@ sub checkout {
 					'comment'   =>  $desc.$c,
 					'skid_id'   =>  $$self{id},
 					'units'     =>  $C->units(),
-					( $PA->docket() ? ( docket		=>	$PA->docket() ) : () ),
+					( ($PA and $PA->docket() ) ? ( docket		=>	$PA->docket() ) : () ),
 					} );
 			$C->quantity( 0 );
 			$e .=   $C->save();
