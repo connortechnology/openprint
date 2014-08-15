dc3ac3c4585720815f6f7c9d0328608f67eeb321
root <root@testing.internal.point-one.com>
2014-08-15 13:12:14 -0400

    Add Orders to Invoice object

--- perl/openprint/Invoice.pm
+++ perl/openprint/Invoice.pm
@@ -20,6 +20,7 @@ require openprint::Invoice_Payment;
 require openprint::Invoice_Tax;
 require openprint::Timetrack;
 require openprint::Object_Asset;
+require openprint::Order_Invoice;
 
 use vars qw( $debug $table $serial %fields %find_fields %defaults %transforms );
 
@@ -293,6 +294,9 @@ sub Products {
 sub Projects {
 	return openprint::Invoiced_Project->find('invoice_id'=>$_[0]{'id'},'order'=>'id');
 } # end sub Projects
+sub Orders {
+	return openprint::Order_Invoice->find(invoice_id=>$_[0]{id}, order=>'order_id');
+} # end sub Orders
 
 sub Interests {
 	my $self = shift;
