35d1e40955f679ce259ac9b7b1d9b8296083393b
root <root@testing.internal.point-one.com>
2014-09-09 11:12:51 -0400

    turn serial into a hash so that saving works

--- perl/openprint/Project_Service.pm
+++ perl/openprint/Project_Service.pm
@@ -7,7 +7,7 @@ require openprint::Project;
 require openprint::User;
 require openprint::ServiceType;
 
-use vars qw( $debug %fields %find_fields %transforms %defaults $table $serial @identified_by );
+use vars qw( $debug %fields %find_fields %transforms %defaults $table %serial @identified_by );
 
 $debug = 0;
 %fields = (
@@ -30,7 +30,7 @@ $debug = 0;
 	created_on	=>	q`'NOW()'`,
 );
 $table = 'tbl_project_contents';
-$serial = 'ContentsServiceIndex_seq';
+%serial = ( service_id=>'ContentsServiceIndex_seq' );
 @identified_by = ( 'project_id', 'service_id' );
 
 sub Project {
