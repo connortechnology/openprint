bd0a1e6c8c4183efb2c2c468db5d680ab2c3bf1a
root <root@testing.internal.point-one.com>
2014-10-30 13:11:29 -0400

    some fixes

--- perl/openprint/employee_purchase_order.pm
+++ perl/openprint/employee_purchase_order.pm
@@ -178,11 +178,18 @@ sub save_contents {
 
 sub view {
 
-	my $Me = new openprint::User( $session{'user_id'} );
-	my $PO = new openprint::PurchaseOrder( $param{'po_id'} );
-	if ( ! $PO->id() ) {
-		$variable{'error'} .= 'Invalid PO # given: ' . $param{'po_id'}.'<br/>';
-		$variable{'PurchaseOrder'} = $PO;
+	my $Me = new openprint::User( $session{user_id} );
+
+	if ( $param{po_id} ne openprint::PurchaseOrder->transform('id', $param{po_id} ) ) {
+		$variable{error} .= 'Invalid PO # given: ' . $param{po_id}.'<br/>';
+		$variable{PurchaseOrder} = new openprint::PurchaseOrder();
+		return;
+	} # end if
+	
+	my $PO = new openprint::PurchaseOrder->find( id=>$param{po_id} );
+	if ( ! $PO ) {
+		$variable{error} .= 'Invalid PO # given: ' . $param{'po_id'}.'<br/>';
+		$variable{PurchaseOrder} = new openprint::PurchaseOrder();
 		return;
 	} # end if
 	if ( $param{'btnFunction'} eq 'Delete' ) {
