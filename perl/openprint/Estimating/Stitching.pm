46976ef7edbcac30600b59c055609518e849283f
root <root@testing.internal.point-one.com>
2014-11-05 10:40:10 -0500

    remove debugging when no folds

--- perl/openprint/Estimating/Stitching.pm
+++ perl/openprint/Estimating/Stitching.pm
@@ -18,7 +18,7 @@ package openprint::Estimating::Stitching;
 use strict;
 #use warnings;
 
-use constant DEBUG => 1;
+use constant DEBUG => 0;
 
 require openprint::Equipment;
 require openprint::service;
@@ -227,15 +227,17 @@ $I->display('In Stitching:') if DEBUG;
         push @printed_impositions, $I->imposition();
         if ( ! $$I{Folds} ) {
 			if ( DEBUG ) {
-            $openprint::log->debug("Sitchign: No folds in imposition, generating") if DEBUG;
-            $I->display("No Folds");
+				$openprint::log->debug("Sitchign: No folds in imposition, generating") if DEBUG;
+				$I->display("No Folds");
 			}
             $$I{Folds} = [ openprint::Estimating::Folding::get_Folds( $folding_specs, $I, $qty_index ) ] if $folding_specs;
         } # end if
 
 		if ( ! ( $$I{Folds} and @{$$I{Folds}} ) ) {
-			$openprint::log->error("No folds in imposition, guess 1") if DEBUG;
-			$I->display("No Folds");
+			if ( DEBUG ) {
+				$openprint::log->error("No folds in imposition, guess 1");
+				$I->display("No Folds");
+			} # end if
 			$$specs{'txtSignatureQty'.$I->pages().'Page-'.$qty_index} += 1;
 			$pockets += 1;
 # This doesn't really make sense.  If we are doing printing estimation, then the folding probably isn't going to match.  
