99b4219fb285a468c7d8b3dfd5470d68387e4c9b
root <root@www1.internal.point-one.com>
2014-11-21 13:41:25 -0500

    blah

--- perl/openprint/Estimating/Scoring.pm
+++ perl/openprint/Estimating/Scoring.pm
@@ -156,10 +156,10 @@ sub calc {
     my $calc_hash = {};
     if ( $$services{SaddleStitching} ) {
         $$calc_hash{StitchingSpecs} = openprint::service::get_specs_ref( $Project, $$services{SaddleStitching}[0] );
-        $$calc_hash{StitchingSpecs} = $$services{SaddleStitching}[0];
+        $$calc_hash{HasStitching} = $$services{SaddleStitching}[0];
     } elsif ( $$services{LoopStitching} ) {
         $$calc_hash{StitchingSpecs} = openprint::service::get_specs_ref( $Project, $$services{LoopStitching}[0] );
-        $$calc_hash{StitchingSpecs} = $$services{LoopStitching}[0];
+        $$calc_hash{HasStitching} = $$services{LoopStitching}[0];
     } # end if
     foreach my $service ( 'Cutting', 'Folding', 'PerfectBound' ) {
         if ( $$services{$service} and @{$$services{$service}} ) {
