2b800c3b2272a74027f9a929f9e47294998d16b0
root <root@mail2.topknotch.com>
2014-11-24 13:45:57 -0500

    blah

--- perl/openprint/Estimating/RoundCornering.pm
+++ perl/openprint/Estimating/RoundCornering.pm
@@ -4,7 +4,7 @@ use strict;
 require openprint::service;
 use POSIX           qw(ceil);
 
-use constant DEBUG => 0;
+use constant DEBUG => 1;
 
 my %variables = (
 	'RoundedCorners' => ['save'],
@@ -43,6 +43,7 @@ sub calc {
 	} # end if
 
 	my $calliper = openprint::print::get_finished_calliper( $pid );
+	
 
 	my @Equipment = openprint::Equipment->find('Specifications'=>{'RoundCornering Capable'=>'Y'},'useinestimating'=>1);
 	if ( ! @Equipment ) {
@@ -53,72 +54,80 @@ sub calc {
 	my $status = 'calculated';
 
 	my $services = $Project->services();
-	my $sig_specs = openprint::service::get_specs_ref( $Project, $$services{''}[0] ) if $$services{''};
 
-	foreach my $qty_index ( $Project->quantity_indexes() ) {
-		$$specs{"Markup$qty_index"} =~ s/[^\d\.\-]//g;
-		$$specs{"txtPrice$qty_index"} =~ s/[^\d\.]//g;
-		$$specs{'txtQuantity'.$qty_index} = $Project->quantity( $qty_index ) if ! $$specs{'txtQuantity'.$qty_index};
-		my $qty = $$specs{'txtQuantity'.$qty_index};
-		if ( $$sig_specs{'Versions'} ) {
-			$qty *= $$sig_specs{'Versions'};
-		} # end if
+	foreach my $sig_id ( $Project->signatures({ sort=>1}) ) {
+		my $sig_specs = openprint::service::get_specs_ref( $Project, $sig_id );
 
-		my %BestPrice;
-		$$specs{'hdnBreakdown'.$qty_index} = '';
+		foreach my $qty_index ( $Project->quantity_indexes() ) {
+			$$specs{"Markup$qty_index"} =~ s/[^\d\.\-]//g;
+			$$specs{"txtPrice$qty_index"} =~ s/[^\d\.]//g;
+			$$specs{'txtQuantity'.$qty_index} = $Project->quantity( $qty_index ) if ! $$specs{'txtQuantity'.$qty_index};
+			my $qty = $$specs{'txtQuantity'.$qty_index};
+			$$specs{'hdnBreakdown'.$qty_index} = '';
 
-		foreach my $Equipment ( @Equipment ) {
-			$$specs{'hdnBreakdown'.$qty_index} .= '<fieldset><legend>'.$Equipment->name().'</legend>';
-			my $lift = $Equipment->specification('Maximum Lift Depth');
-			if ( ! $lift ) {
-				$$specs{'hdnBreakdown'.$qty_index} = 'No maximum lift depth specified.<br/></fieldset>';
-				next;
+			if ( $$sig_specs{"Versions$qty_index"} ) {
+				$qty *= $$sig_specs{"Versions$qty_index"};
+				$$specs{'hdnBreakdown'.$qty_index} .= $$sig_specs{"Versions$qty_index"} . ' versions<br/>';
 			} # end if
-			my $corners = $Equipment->specification('Corners Per Lift');
-			$corners = 1 if ! $corners;
-			$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Corners Per Lift: %d<br/>', $corners );
 
-			$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Lift Depth: %.2f&quot;<br/>',$lift);
+			my %BestPrice;
 
-			my $runs = ceil( $qty * $calliper / $lift );
-			$runs *= ceil( $$specs{'RoundedCorners'} / $corners );
-			my $total = 0;
+			foreach my $Equipment ( @Equipment ) {
+				$$specs{'hdnBreakdown'.$qty_index} .= '<fieldset><legend>'.$Equipment->name().'</legend>';
+				my $lift = $Equipment->specification('Maximum Lift Depth');
+				if ( ! $lift ) {
+					$$specs{'hdnBreakdown'.$qty_index} = 'No maximum lift depth specified.<br/></fieldset>';
+					next;
+				} # end if
+				my $corners = $Equipment->specification('Corners Per Lift');
+				$corners = 1 if ! $corners;
 
-			my %MakeReady = openprint::service::get_price_object('RoundCorneringMakeReady', undef, $Equipment );
-			if ( ! %MakeReady ) {
-				$$specs{'hdnBreakdown'.$qty_index} .= 'No MakeReady price.<br/>';
+				my $items_per_lift = int($lift/$calliper);
+				my $runs = ceil( $qty / $items_per_lift );
+				$runs *= ceil( $$specs{'RoundedCorners'} / $corners );
+
+				$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Corners Per Lift %d: lift height: %.2f / %.4f (calliper) = %d * (%d/%d) corners = %d lifts<br/>', 
+					$corners, $lift, $calliper, $items_per_lift, $$specs{'RoundedCorners'}, $corners, $runs );
+
+				$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Lift Depth: %.2f&quot;<br/>',$lift);
+
+				my $total = 0;
+
+				my %MakeReady = openprint::service::get_price_object('RoundCorneringMakeReady', undef, $Equipment );
+				if ( ! %MakeReady ) {
+					$$specs{'hdnBreakdown'.$qty_index} .= 'No MakeReady price.<br/>';
+				} else {
+					$$specs{'hdnBreakdown'.$qty_index} .= sprintf('MakeReady Price: $%1$.2f%2$s<br/>', @MakeReady{'Price','units'} );
+					$total += $MakeReady{'Price'};
+				} # end if
+
+				my %ServicePrice = openprint::service::get_price_object('RoundCornering', undef, $Equipment ); 
+				if ( ! %ServicePrice ) {
+					$$specs{'hdnBreakdown'.$qty_index} .= 'No Service price.<br/>';
+				} else {
+					$ServicePrice{'Total'} += $ServicePrice{'Price'} * $runs;
+					$total += $ServicePrice{'Total'};
+					$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Service Price: $%1$.2f%2$s * %4$d lifts = $%3$.2f<br/>', @ServicePrice{'Price','units','Total'}, $runs );
+				} # end if
+
+				if ( my $minimumcharge = openprint::service::get_price('RoundCorneringMinimumCharge', undef, $Equipment ) ) {
+					$total = $minimumcharge if $total < $minimumcharge;
+				} # end if
+				$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Total: $%.2f<br/>', $total );
+				
+				if ( ( ! defined $BestPrice{'Total'} ) or $total < $BestPrice{'Total'} ) {
+					$BestPrice{'Total'} = $total;
+					$BestPrice{'Equipment'} = $Equipment;
+					$BestPrice{'ServicePrice'} = \%ServicePrice;
+				} # end if
+				$$specs{'hdnBreakdown'.$qty_index} .= '</fieldset>';
+			} # end foreach Equipment
+
+			if ( ! defined $BestPrice{'Total'} ) {
+				$status = 'uncalculated';
 			} else {
-				$$specs{'hdnBreakdown'.$qty_index} .= sprintf('MakeReady Price: $%1$.2f%2$s<br/>', @MakeReady{'Price','units'} );
-				$total += $MakeReady{'Price'};
+				$$specs{'ddmEquipment'.$qty_index} = $BestPrice{'Equipment'}->id();
 			} # end if
-
-			my %ServicePrice = openprint::service::get_price_object('RoundCornering', undef, $Equipment ); 
-			if ( ! %ServicePrice ) {
-				$$specs{'hdnBreakdown'.$qty_index} .= 'No Service price.<br/>';
-			} else {
-				$ServicePrice{'Total'} += $ServicePrice{'Price'} * $runs;
-				$total += $ServicePrice{'Total'};
-				$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Service Price: $%1$.2f%2$s * %4$d lifts = $%3$.2f<br/>', @ServicePrice{'Price','units','Total'}, $runs );
-			} # end if
-
-			if ( my $minimumcharge = openprint::service::get_price('RoundCorneringMinimumCharge', undef, $Equipment ) ) {
-				$total = $minimumcharge if $total < $minimumcharge;
-			} # end if
-			$$specs{'hdnBreakdown'.$qty_index} .= sprintf('Total: $%.2f<br/>', $total );
-			
-			if ( ( ! defined $BestPrice{'Total'} ) or $total < $BestPrice{'Total'} ) {
-				$BestPrice{'Total'} = $total;
-				$BestPrice{'Equipment'} = $Equipment;
-				$BestPrice{'ServicePrice'} = \%ServicePrice;
-			} # end if
-			$$specs{'hdnBreakdown'.$qty_index} .= '</fieldset>';
-        } # end foreach Equipment
-
-		if ( ! defined $BestPrice{'Total'} ) {
-			$status = 'uncalculated';
-		} else {
-			$$specs{'ddmEquipment'.$qty_index} = $BestPrice{'Equipment'}->id();
-		} # end if
 
         $$specs{'txtUnitPrice'.$qty_index} = sprintf($openprint::config{'UnitPriceFormat'}, 
 				( $BestPrice{'ServicePrice'}{'Total'} / $qty ) * (1+$Project->markup()/100) );
@@ -129,6 +138,7 @@ sub calc {
 		} else {
 			$$specs{'txtPrice'.$qty_index} = sprintf( $openprint::config{'ProjectMoneyFormat'}, $$specs{"txtPrice$qty_index"} );
 		} # end if
+		} # end foreach signature
 
     } # end foreach qty_index
     return $status;
