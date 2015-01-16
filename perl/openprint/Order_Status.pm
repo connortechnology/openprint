e670e6af3c84ff6ef8a7cace6fd3a1e8ca8db23b
root <root@testing.internal.point-one.com>
2015-01-16 12:25:40 -0500

    turb off debug

--- perl/openprint/Order_Status.pm
+++ perl/openprint/Order_Status.pm
@@ -4,7 +4,7 @@ our @ISA = qw(openprint::Object);
 
 use vars qw( $debug $table $serial %fields %transforms %defaults );
 
-$debug = 1;
+$debug = 0;
 $table = 'order_statuses';
 $serial= 'order_statuses_id_seq';
 %fields = (
