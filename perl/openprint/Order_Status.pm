9d887415f8b21dcbc074b439454d6d5847176693
root <root@testing.internal.point-one.com>
2015-01-21 10:38:44 -0500

    add default sort

--- perl/openprint/Order_Status.pm
+++ perl/openprint/Order_Status.pm
@@ -2,17 +2,18 @@ use strict;
 package openprint::Order_Status;
 our @ISA = qw(openprint::Object);
 
-use vars qw( $debug $table $serial %fields %transforms %defaults );
+use vars qw( $debug $table $serial %fields %transforms %defaults $default_sort );
 
 $debug = 0;
+$default_sort = 'name';
 $table = 'order_statuses';
 $serial= 'order_statuses_id_seq';
 %fields = (
-    'id'    =>  'id',
-    'name' =>  'name',
+    id    =>  'id',
+    name =>  'name',
 );
 %transforms = (
-    'name' => [ 's/^\s+//', 's/\s+$//', 's/\s\s+/ /g' ],
+    name => [ 's/^\s+//', 's/\s+$//', 's/\s\s+/ /g' ],
 );
 %defaults = (
 );
