4f042a42d0bb24d9bc26fd64c07df9e4ee563b35
root <root@www5.internal.point-one.com>
2015-01-22 10:46:27 -0500

    add more logging

--- perl/openprint/cron/ftp_monitor.pl
+++ perl/openprint/cron/ftp_monitor.pl
@@ -562,8 +562,12 @@ $log->debug("regexp: $regexp");
 			@to = ( $User );
 		} else {
 			if ( $Company->salesrep_id() ) {
-				if ( $Company->CSR()->notification('CSR Client File Uploads') ne 'No' ) {
-					@to = ( $Company->CSR() );
+				my $CSR = $Company->CSR();
+				if ( $CSR->notification('CSR Client File Uploads') ne 'No' ) {
+					@to = ( $CSR );
+					$log->debug("Adding CSR $$CSR{email}");
+				} else {
+					$log->debug("Not Adding CSR $$CSR{email} : notifications etting:" . $CSR->notification('CSR Client File Uploads') );
 				} # end if
 			} # end if
 			push @to, map { $_->User() } openprint::User_Notification->find( type=>'Client File Uploads',value=>'Yes', company_id=>[ $config{Owner}, $Company->id() ] );
