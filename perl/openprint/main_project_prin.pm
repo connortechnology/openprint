b0e64f7bdfdb5534677c6faf84ba1aebed547fce
root <root@testing.internal.point-one.com>
2015-01-28 16:30:31 -0500

    work on stock popup add paper function call because now we get called on json as well as html

--- perl/openprint/main_project_prin.pm
+++ perl/openprint/main_project_prin.pm
@@ -107,13 +107,21 @@ sub Signature {
 } # end sub Signature
 
 sub _stock_popup {
+
 } # end sub _stop_popup
+
+sub _stock_popup_results {
+
+}
 
 sub _stocks {
 } # end sub _stocks
 
 sub _impositions {
 } # end sub _impositions
+
+sub paper {
+} # end sub paper
 
 1;
 __END__
