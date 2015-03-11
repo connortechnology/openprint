80db439346c0b1ed5df5c73d1c7dba50da7b58bf
root <root@www2.internal.point-one.com>
2015-03-11 11:43:44 -0400

    turn off debugging

--- perl/openprint/MarketingCategory.pm
+++ perl/openprint/MarketingCategory.pm
@@ -7,7 +7,7 @@ require sql;
 use openprint ();
 
 use vars qw( $debug %fields %find_fields %transforms %defaults $table $serial );
-$debug = 1;
+$debug = 0;
 %fields = (
 	'id'			=>	'id',
 	'name'			=>	'name',
