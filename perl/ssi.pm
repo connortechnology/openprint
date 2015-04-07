eaf871019a281c5706c3ed3903d5538077288b06
root <root@www5.internal.point-one.com>
2015-04-07 16:02:44 -0400

    Integers can have negatives

--- perl/ssi.pm
+++ perl/ssi.pm
@@ -879,10 +879,10 @@ sub input {
 	} elsif ( $options{type} eq 'integer' ) {
 		if ( $ENV{HTTP_USER_AGENT} =~ /ip(ad|od|hone)/i ) {
 			$options{type} = 'text';
-			$options{pattern} = '[0-9]*' if ! $options{pattern};
+			$options{pattern} = '\-?[0-9]*' if ! $options{pattern};
 		} elsif ( $ENV{HTTP_USER_AGENT} =~ /Firefox/ ) {
 			$options{type} = 'text';
-			$options{pattern} = '[0-9]*' if ! $options{pattern};
+			$options{pattern} = '\-?[0-9]*' if ! $options{pattern};
 			delete $options{step};
 		} else {
 			$options{type} = 'number';
