a67ed7c956da0318c6b191d8085564978e125c01
root <root@testing.internal.point-one.com>
2015-04-10 11:05:34 -0400

    Can't use PreviousStockWIdth

--- perl/openprint/Estimating/Printing.pm
+++ perl/openprint/Estimating/Printing.pm
@@ -3078,7 +3078,7 @@ $openprint::log->debug("Needed pages: $needed_pages");
 			} # end if
 		}  # end if
 
-		if ( $$sig_specs{PreviousStockWidth} and $$Paper{width} > $$sig_specs{PreviousStockWidth} ) {
+		if ( 0 and $$sig_specs{PreviousStockWidth} and $$Paper{width} > $$sig_specs{PreviousStockWidth} ) {
 			$imp->display("PreviousStockWidth: $$sig_specs{PreviousStockWidth} < " . $Paper->to_string() ) if DEBUG_FILTERING;
 			next;
 		} # end if
