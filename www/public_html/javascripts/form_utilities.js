a679a730a0873d68a737577c9fa16728ea0eb1e7
root <root@testing.internal.point-one.com>
2015-05-01 11:00:02 -0400

    add stop_filter_companies

--- www/public_html/javascripts/form_utilities.js
+++ www/public_html/javascripts/form_utilities.js
@@ -1579,3 +1579,9 @@ function filter_companies( e, p ) {
         
     filter_ajax = new Ajax.Updater( e, '/includes/_company_ddm.html', { parameters: p, onSuccess:function(){filter_ajax = null;} } );
 }
+function stop_filter_companies() {
+    if ( filter_ajax ) { 
+		filter_ajax.transport.abort();
+		filter_ajax = null;
+	}
+} 
