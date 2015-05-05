74648571983c965b0ad2d448cbc0b3d70c561692
root <root@testing.internal.point-one.com>
2015-05-05 11:14:15 -0400

    Use a constant to define the # of stock_indexes, and make it 10 instead of 4

--- perl/openprint/Estimating/Paper.pm
+++ perl/openprint/Estimating/Paper.pm
@@ -26,6 +26,7 @@ require openprint::Currency;
 require openprint::Paper;
 
 use constant DEBUG => 0;
+use constant MAX_STOCK_INDEX => 10;
 
 my @variables = (
         'txtPrice1', 'txtPrice2', 'txtPrice3',
@@ -43,7 +44,7 @@ sub variables {
 	foreach my $ss_id ( $Project->signatures() ) {
 		my $sig_specs = openprint::service::get_specs_ref( $Project, $ss_id );
 		my $form = $$sig_specs{SignatureIndex};
-		foreach my $stock_index ( 1 .. 4 ) {
+		foreach my $stock_index ( 1 .. MAX_STOCK_INDEX ) {
 			#last if ! exists $$specs{"qty-$ss_id-$stock_index"};
 			#push @v, "id-$ss_id-$stock_index";
 			foreach my $qty_index ( $Project->quantity_indexes() ) {
@@ -54,7 +55,7 @@ sub variables {
 			} # end foreach qty_index
 		} # end foreach stock_index
 	} # end foreach ss_id
-	foreach my $stock_index ( 1 .. 4 ) {
+	foreach my $stock_index ( 1 .. MAX_STOCK_INDEX ) {
 		foreach my $qty_index ( $Project->quantity_indexes() ) {
 			push @v, "qty-$stock_index-$qty_index";
 			push @v, "sheets-$stock_index-$qty_index";
@@ -76,13 +77,13 @@ sub has_overrides {
         foreach my $s_s_id ( $Project->signatures() ) {
             my $sig_specs = openprint::service::get_specs_ref( $Project, $s_s_id );
 			my $form = $$sig_specs{SignatureIndex};
-			foreach my $stock_index ( 1 .. 4 ) {
+			foreach my $stock_index ( 1 .. MAX_STOCK_INDEX ) {
 				push @v, map { $$specs{$_} ? $_ : () } (
 					"overrideqty-$form-$stock_index-$qty_index",
                     );
 			} # end fireach stock_index
 		} # end foreach sig
-		foreach my $stock_index ( 1 .. 4 ) {
+		foreach my $stock_index ( 1 .. MAX_STOCK_INDEX ) {
 			push @v, map { $$specs{$_} ? $_ : () } (
 					"overridecost-$stock_index-$qty_index",
 					);
@@ -408,7 +409,7 @@ sub se_quantity_summary {
 	my $Paper = $$Stock_Entry{Stock};
 	my $stock_id = $$Stock_Entry{index};
 
-#$openprint::log->warn("Stock QTY $stock_id $qty_index " . $$specs{"qty-$stock_id-$qty_index"} );
+#$openprint::log->warn("Stock " . $Paper->to_string() . " QTY $stock_id $qty_index " . $$specs{"qty-$stock_id-$qty_index"} );
 	if ( $$specs{"qty-$stock_id-$qty_index"} ) {
 		if ( $Paper->type() eq 'Sheet' ) {
 			$html .= $$specs{"sheets-$stock_id-$qty_index"}.'sheets ';
