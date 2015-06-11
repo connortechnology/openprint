1b441840e1c1fe181c44383ae4c91bae14842c65
root <root@testing.internal.point-one.com>
2015-06-11 11:48:33 -0400

    Turn on debugging

--- perl/openprint/PurchaseOrder.pm
+++ perl/openprint/PurchaseOrder.pm
@@ -30,7 +30,7 @@ require MIME::Base64;
 require openprint::Object_Asset;
 require openprint::Asset;
 
-$debug = 0;
+$debug = 1;
 
 $table = 'purchaseorders';
 $serial = 'purchaseorders_id_seq';
@@ -116,11 +116,14 @@ sub save {
 	$dbh->do( "LOCK TABLE $openprint::PurchaseOrder_Tax::table IN EXCLUSIVE MODE" ) or $log->error( DBI->errstr );
 	# force recalculation
 	$self->subtotal(undef);
+$log->debug("After recalc, subtoatal is: $$self{subtotal}");
 	foreach my $Tax ( $self->Taxes(1) ) {
 		$Tax->PurchaseOrder( $self );
 		$Tax->amount(undef);
+$log->debug("After recalc, Tax $$Tax{name} is: $$Tax{amount}");
 	} # end foreach Tax
 	$self->total(undef);
+$log->debug("After recalc, toatal is: $$self{total}");
 	if ( ! $$self{currency_id} ) {
 		my $Currency = openprint::Currency::get_current();
 		$$self{currency_id} = $Currency->id() if $Currency;
