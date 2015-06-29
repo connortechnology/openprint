d82499599e9dc991c9ec488dc5cd2aca3f6f4e61
root <root@www1.topknotch.com>
2015-06-29 10:31:24 -0400

    add

--- perl/openprint.pm
+++ perl/openprint.pm
@@ -1,6 +1,6 @@
 use strict;
 package openprint;
-use vars qw( $r %variable %session %param %config $log $dbh $User $Company $TZ );
+use vars qw( $r %variable %session %param %config $log $dbh $User $Company $TZ $Owner );
 
 
 sub session_init {
@@ -71,6 +71,7 @@ sub session_init {
 
 		$User = new openprint::User( $session{user_id} );
 		$Company = new openprint::Company( $session{company_id} );
+		$Owner = new openprint::Company( $config{owner_id} );
 
 	# Now set some defaults right away, if we can, FIXME namespace colision
 		if ( $r->param('Country') ) {
