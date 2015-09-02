581879765e0578fc79bb45de71647d056501cc8e
root <root@testing.internal.point-one.com>
2015-09-02 12:42:36 -0400

    Cache Parents lookups

--- perl/openprint/Location.pm
+++ perl/openprint/Location.pm
@@ -120,9 +120,11 @@ sub Parents {
 	
 	if ( ( ! $_[0]{id} ) or ! $_[0]{parent_id} ) {
 		return ();
-	} else {
-		return $_[0]->Parent(), $_[0]->Parent()->Parents();
-	} # end if
+	} 
+	if ( ! $_[0]{Parents} ) {
+		$_[0]{Parents} = [ $_[0]->Parent(), $_[0]->Parent()->Parents() ];
+	}
+	return @{$_[0]{Parents}};
 } # end sub Parents
 
 sub Type {
