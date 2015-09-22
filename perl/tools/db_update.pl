024f527609c8d8a4fa49ae1cd09b8813d80a4bb8
root <root@testing.internal.point-one.com>
2015-09-22 14:29:06 -0400

    add allow_delete

--- perl/tools/db_update.pl
+++ perl/tools/db_update.pl
@@ -942,6 +942,9 @@ if ( ! sets::isin( 'service_types', \@tables ) ) {
 	if ( ! exists $$data{summary_visible} ) {
 		$dbh->do('ALTER TABLE service_types add summary_visible BOOLEAN NOT NULL default true');
 	} # end if
+	if ( ! exists $$data{allow_delete} ) {
+		$dbh->do('ALTER TABLE service_types add allow_delete BOOLEAN NOT NULL default true');
+	} # end if
 }# end if
 if ( ! sets::isin( 'service_categories_id_seq', \@sequences ) ) {
 	$dbh->do('create sequence service_categories_id_seq;');
