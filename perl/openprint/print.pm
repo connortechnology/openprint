d3823b03e5984ce779de602f55baa22018d5a742
root <root@testing.internal.point-one.com>
2015-09-23 10:51:05 -0400

    fix allow_delete

--- perl/openprint/print.pm
+++ perl/openprint/print.pm
@@ -214,7 +214,7 @@ sub view_services {
 				if ( sets::isin( $ServiceType->name(), ['Proofs'] ) and ( @{$$services{$ServiceType->name()}} == 1 ) ) {
 					$$variable{error} .= 'Proofs cannot be removed from the project.<br/>';
 					next;
-				} elsif ( $ServiceType->allow_delete() ) {
+				} elsif ( ! $ServiceType->allow_delete() ) {
 					$$variable{error} .= $ServiceType->name() . ' cannot be removed from the project.<br/>';
 					next;
 				} # end if
