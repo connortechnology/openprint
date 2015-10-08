84ddb29a2d0d602cf7638b2831ab3fc254de6d5c
root <root@testing.internal.point-one.com>
2015-10-08 15:40:16 -0400

    add Company_id and indexes

--- perl/openprint/sql/User_Notifications.sql
+++ perl/openprint/sql/User_Notifications.sql
@@ -1,6 +1,11 @@
 CREATE TABLE User_Notifications (
+	id	SERIAL,
+	company_id	INTEGER, FOREIGN KEY (company_id) REFERENCES Companies (id),
 	user_id	INTEGER NOT NULL, FOREIGN KEY (user_id) REFERENCES Users (id),
 	type_id	INTEGER NOT NULL, FOREIGN KEY (type_id) REFERENCES User_Notification_Types (id),
 	value	TEXT,
-	PRIMARY KEY (user_id, type_id)
+	PRIMARY KEY (id)
 );
+
+CREATE INDEX user_notifications_user_idx on user_notifications (user_id,type_id);
+CREATE INDEX user_notifications_company_idx on user_notifications (type_id,company_id);
