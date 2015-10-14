a496d73654e005828c4227adc0431115d9484398
root <root@testing.internal.point-one.com>
2015-10-14 09:04:02 -0400

    fixup error message

--- perl/openprint/Project.pm
+++ perl/openprint/Project.pm
@@ -1487,8 +1487,8 @@ sub production_cost {
 sub Service {
 	my ( $self, $service_id ) = @_;
 	if ( ! $service_id ) {
-		$openprint::log->error("No service_id passed to ServiceType for project $$self{id}");
-		Carp::cluck("No service_id passwrod to ServiceType");
+		$openprint::log->error("No service_id passed to Service for project $$self{id}");
+		Carp::cluck("No service_id passwrod to Project::Service");
 	} # end if
 	return new openprint::Project_Service( {project_id=>$$self{id}, service_id=>$service_id} );
 } # end sub Service
