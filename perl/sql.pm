7bd3513a6c88fcbfe4f105a5151978a64de5cb1f
root <root@testing.internal.point-one.com>
2015-11-30 11:19:13 -0500

    turn off debug

--- perl/sql.pm
+++ perl/sql.pm
@@ -10,7 +10,7 @@ use vars qw( $log $dbh $timing );
 use openprint ();
 *dbh = \$openprint::dbh;
 *log = \$openprint::log;
-use constant DEBUG => 1;
+use constant DEBUG => 0;
 $timing = 1;
 
 # This uses it's own dbh so as not to quash the global dbh.  This is so that we can easily open secondary db connections while maintaining the global one.
