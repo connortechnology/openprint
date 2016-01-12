3f2137e061e594bcc44ab3f069fd94753177ed3a
root <root@testing.internal.point-one.com>
2016-01-12 09:13:21 -0500

    debug and quote cleanup

--- perl/openprint/ZM_Monitor.pm
+++ perl/openprint/ZM_Monitor.pm
@@ -3,7 +3,7 @@ package openprint::ZM_Monitor;
 our @ISA = qw(openprint::Object);
 
 use vars qw( $debug $table $serial %fields %defaults %transforms );
-$debug = 0;
+$debug = 1;
 $table = 'Monitors';
 
 %fields = (
@@ -28,21 +28,28 @@ $table = 'Monitors';
 );
 
 sub source_stream_url {
-	return ($_[0]{'type'} eq 'Remote' and $_[0]{protocol} eq 'http' ) ? 'http://'.$_[0]{'host'}.$_[0]{'path'} :
+	return ($_[0]{type} eq 'Remote' and $_[0]{protocol} eq 'http' ) ? 'http://'.$_[0]{host}.$_[0]{path} :
                           sprintf('http://%2$s/cgi-bin/zms?mode=jpeg&amp;monitor=%1$d&amp;maxfps=%3$d&amp;user=all',
-                              $_[0]{'id'}, $_[0]{'server_host'}.'.internal.point-one.com', int($_[0]{'max_fps'}) ? $_[0]{'max_fps'} : 1 );
+                              $_[0]{id}, $_[0]{server_host}.'.internal.point-one.com', int($_[0]{max_fps}) ? $_[0]{max_fps} : 1 );
 } # end sub source_stream_url
 
 sub source_snapshot_url {
-	return $_[0]{'type'} eq 'Remote' ? 'http://'.$_[0]{'host'}.($_[0]{'jpg_path'}?$_[0]{'jpg_path'}:$_[0]{'path'}) :
+	return $_[0]{type} eq 'Remote' ? 'http://'.$_[0]{host}.($_[0]{jpg_path}?$_[0]{jpg_path}:$_[0]{path}) :
                           sprintf('http://%2$s/cgi-bin/zms?mode=single&amp;monitor=%1$d&amp;maxfps=%3$d&amp;user=all',
-                              $_[0]{'id'}, $_[0]{'server_host'}, $_[0]{'max_fps'} );
+                              $_[0]{id}, $_[0]{server_host}, $_[0]{max_fps} );
 } # end sub source_snapshot_url
 
 sub can_view {
-return 1 if $_[0]{public};
-return 1 if $openprint::session{user_type} eq 'A';
-return 0;
+	if ( $_[0]{public} ) {
+		$openprint::log->debug("Public") if $debug;
+		return 1 ;
+	}
+	if ( $openprint::session{user_type} eq 'A' ) {
+		$openprint::log->debug("Admin") if $debug;
+		return 1;
+	}
+	$openprint::log->debug("Not public") if $debug;
+	return 0;
 } # end sub can_view
 
 1;
