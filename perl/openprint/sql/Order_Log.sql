b059376f0fe78d98001011f1c86bf9936635512f
root <root@pseudo.connortechnology.com>
2016-01-25 12:11:11 -0500

    Fixup Order_Log

--- perl/openprint/sql/Order_Log.sql
+++ perl/openprint/sql/Order_Log.sql
@@ -1,13 +1,12 @@
 
-DROP TABLE IF EXISTS Order_Log;
-
 CREATE TABLE Order_Log (
+	id			SERIAL,
 	order_id	INTeger	NOT NULL, FOREIGN KEY(Order_Id) REFERENCES Orders (id),
 	Company_id	INTeger	NOT NULL, FOREIGN KEY(Company_id) REFERENCES Companies (id),
 	User_id		INTeger	NOT NULL, FOREIGN KEY(User_Id) REFERENCES Users (id),
 	dtmwhen		timestamp with time zone NOT NULL default(NOW()),
 	Description			TEXT,
-	PRIMARY KEY (Order_Id,dtmwhen)
+	PRIMARY KEY (id)
 );
 
 CREATE INDEX order_log_order_id_idx ON order_log (order_id);
