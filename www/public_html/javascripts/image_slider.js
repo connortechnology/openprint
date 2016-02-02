4e06817b4a9cb843825dd6cef629dfc1f8dd59b7
root <root@mail.connortechnology.com>
2016-02-02 12:30:49 -0500

    improve slider, cache  lookups

--- www/public_html/javascripts/image_slider.js
+++ www/public_html/javascripts/image_slider.js
@@ -6,16 +6,32 @@ var i = 0;
 var wait = 4000;
 
 // The Fade Function
-function SwapImage(x,y) {       
-    $(image_slide[x]).appear({ duration: 1.5 });
-    $(image_slide[y]).fade({duration: 1.5});
+function SwapImage(x,y) {
+	var x_image = $(image_slide[x]);
+	var y_image = $(image_slide[y]);
+	var x_height = x_image.getHeight();
+	var y_height = y_image.getHeight();
+	var ul = x_image.up();
+
+	if ( x_height > y_height ) {
+		ul.setStyle( {height: x_height+"px" } );
+	}
+		
+    x_image.appear({ duration: 1.5 });
+    y_image.fade({duration: 1.5});
+
+	if ( x_height < y_height ) {
+		ul.setStyle( {height: x_height+"px" } );
+	}
 }
 
 // the onload event handler that starts the fading.
 function StartSlideShow() {
     play = setInterval('Play()',wait);
-    $('PlayButton').hide();
-    $('PauseButton').appear({ duration: 0});
+	var playbutton = $('PlayButton');
+	if ( playbutton ) $('PlayButton').hide();
+	var pausebutton = $('PauseButton');
+    if ( pausebutton ) $('PauseButton').appear({ duration: 0});
                                 
 }
 
@@ -36,21 +52,27 @@ function Play() {
 
 function Stop () {
     clearInterval(play);                
-    $('PlayButton').appear({ duration: 0});
-    $('PauseButton').hide();
+	var playbutton = $('PlayButton');
+	var pausebutton = $('PauseButton');
+	if ( playbutton ) playbutton.appear({ duration: 0});
+    if ( pausebutton ) pausebutton.hide();
 }
 
 function GoNext() {
     clearInterval(play);
-    $('PlayButton').appear({ duration: 0});
-    $('PauseButton').hide();
+	var playbutton = $('PlayButton');
+	var pausebutton = $('PauseButton');
+    if ( playbutton ) playbutton.appear({ duration: 0});
+    if(pausebutton) pausebutton.hide();
 	Play();
 }
 
 function GoPrevious() {
     clearInterval(play);
-    $('PlayButton').appear({ duration: 0});
-    $('PauseButton').hide();
+	var playbutton = $('PlayButton');
+	var pausebutton = $('PauseButton');
+    if ( playbutton ) playbutton.appear({ duration: 0});
+    if( pausebutton ) pausebutton.hide();
 
     var imageShow, imageHide;
                 
