1c4bc487c545b6da17eb1c0857ab41ddae2b7d60
root <root@mail.connortechnology.com>
2016-02-17 08:36:46 -0500

    fix check for similar timetrack, dealign with empty user_id

--- perl/openprint/timetrack.pm
+++ perl/openprint/timetrack.pm
@@ -108,7 +108,7 @@ sub edit {
 		$param{ending} = $parser->format_datetime( $end_datetime );
 		if ( ! $param{timetrack_id} ) {
 			if ( openprint::Timetrack->find_one(
-						user_id		=>$param{user_id},
+						user_id		=> ( $param{user_id} ? $param{user_id} : undef ),
 						owner_id	=>$param{owner_id},
 						company_id	=>$param{company_id},
 						starting	=>$param{starting},
