6b17b73c73d8945383fb9ce563bbe592e0c8c304
root <root@pseudo.connortechnology.com>
2016-02-26 10:01:10 -0500

    more impo optimisation

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -1203,35 +1203,35 @@ sub cut {
 		$i2->dutch_rows( 0 );
 		$i2->dutch_columns( 0 );
 		push @Results, $i1, $i2;
-	} elsif ( $I->layout_width() >= $I->layout_height() and $I->columns() > 1 ) {
-		$i1->columns( int($I->columns() / 2) );
-		if ( ! ( $I->columns() % 2 ) ) {
+	} elsif ( $I->layout_width() >= $I->layout_height() and $$I{columns} > 1 ) {
+		$i1->columns( int($$I{columns} / 2) );
+		if ( ! ( $$I{columns} % 2 ) ) {
 			$i1->quantity( $i1->quantity() * 2 );
 			push @Results, $i1;
 		} else {
 			my $i2 = $I->copy();
-			$i2->columns( $I->columns() - $i1->columns() );
+			$i2->columns( $$I{columns} - $$i1{columns} );
 			push @Results, $i1, $i2;
 		} # end if
-	} elsif ( $I->layout_width() < $I->layout_height() and $I->rows() > 1 ) {
-		$i1->rows( int($I->rows() / 2) );
-		if ( ! ( $I->rows() % 2 ) ) {
+	} elsif ( $I->layout_width() < $I->layout_height() and $$I{rows} > 1 ) {
+		$i1->rows( int($$I{rows} / 2) );
+		if ( ! ( $$I{rows} % 2 ) ) {
 			$i1->quantity( $i1->quantity() * 2 );
 			push @Results, $i1;
 		} else {
 			my $i2 = $I->copy();
-			$i2->rows( $I->rows() - $i1->rows() );
+			$i2->rows( $$I{rows} - $$i1{rows} );
 			push @Results, $i1, $i2;
 		} # end if
-	} elsif ( $I->columns() >= $I->rows() ) {
+	} elsif ( $$I{columns} >= $$I{rows} ) {
 		my $i2 = $I->copy();
-		$i1->columns( int($I->columns() / 2) );
-		$i2->columns( $I->columns() - $i1->columns() );
+		$i1->columns( int($$I{columns} / 2) );
+		$i2->columns( $$I{columns} - $$i1{columns} );
 		push @Results, $i1, $i2;
 	} else {
 		my $i2 = $I->copy();
-		$i1->rows( int($I->rows() / 2) );
-		$i2->rows( $I->rows() - $i1->rows() );
+		$i1->rows( int($$I{rows} / 2) );
+		$i2->rows( $$I{rows} - $$i1{rows} );
 		push @Results, $i1, $i2;
 	} # end if
 	foreach my $i ( @Results ) {
