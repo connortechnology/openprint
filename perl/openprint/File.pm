155c9d4a1dfa7bd28fd6847ff5b7cd067b61e6d5
root <root@testing.internal.point-one.com>
2016-02-29 10:40:21 -0500

    Fix File to look for ftp files as well

--- perl/openprint/File.pm
+++ perl/openprint/File.pm
@@ -42,7 +42,11 @@ sub size_text {
 
 sub stat {
 	if ( ! $_[0]{stat} ) {
+		if ( $_[0]{archive} ) {
 		$_[0]{stat} = [ stat $_[0]{archive}.$_[0]{filename} ];
+		} else {
+		$_[0]{stat} = [ stat $openprint::config{ProjectFilesPath}.$_[0]{filename} ];
+		}
 	}
 	return $_[0]{stat};
 }
@@ -50,11 +54,13 @@ sub stat {
 sub created_on {
 	if ( ! $_[0]{created_on} ) {
 		my $stat = $_[0]->stat();
-		my $mtime = $$stat[9];
-		require DateTime;
-		require DateTime::Format::Pg;
-		my $DT = DateTime->from_epoch( epoch=>$mtime, time_zone=>$openprint::TZ );
-		$_[0]{created_on} = DateTime::Format::Pg->format_datetime( $DT );
+		if ( $stat and @{$stat} ) {
+			my $mtime = $$stat[9];
+			require DateTime;
+			require DateTime::Format::Pg;
+			my $DT = DateTime->from_epoch( epoch=>$mtime, time_zone=>$openprint::TZ );
+			$_[0]{created_on} = DateTime::Format::Pg->format_datetime( $DT );
+		} # end if
 	} 
 	return $_[0]{created_on};
 }
