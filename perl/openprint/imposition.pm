ab2ed53ec6266f1c120fa910d4b6faedadd0a160
root <root@testing.internal.point-one.com>
2016-03-03 09:54:29 -0500

    improve decrease_imposition to handle WORK&

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -392,7 +392,7 @@ $openprint::log->debug("Not Pretrimming on $$Press{strid}") if DEBUG;
 #	3. For %setup1, Paper Width is matched to Image Width. eg: For a 22x28 Image on 23x39 sheet would match the 22 IW to the 39 PW and 28 IH to the 22 PW.
 
 	my $gutters = $$specs{Gutter};
-	$gutters = $bindery_gutters if $gutters < $bindery_gutters;
+	#$gutters = $bindery_gutters if $gutters < $bindery_gutters;
 	if ( $bleed_locations{Right} ) {
 		$gutters -= $bleed_size;
 	} # end if
@@ -1069,7 +1069,6 @@ sub decrease_imposition {
 
 	foreach my $imposition ( @_ ) {
 		next if ( ($$imposition{columns} * $$imposition{rows}) <= 1 );
-		next if $$imposition{runstyle} =~ /^Work/;
 
 		if ( $$imposition{dutch_columns} ) {
 			my $imp1 = $imposition->copy();
@@ -1081,6 +1080,33 @@ sub decrease_imposition {
 			$imp2->columns( $$imposition{dutch_columns} );
 			$imp2->rows( $$imposition{dutch_rows} );
 			push @results, $imp2;
+		} elsif ( $$imposition{runstyle} eq 'Work & Turn' ) {
+			if ( $$imposition{columns} > 2 ) {
+					my $imp1 = $imposition->copy();
+					$imp1->columns( $$imp1{columns} / 2 );
+					push @results, $imp1;
+			}
+			if ( $$imposition{rows} >= 2 ) {
+				foreach my $row ( 2 .. $$imposition{rows} ) {
+					my $imp1 = $imposition->copy();
+					$imp1->rows( $$imp1{rows} - ( $row -1 ) );
+					push @results, $imp1;
+				} # end foraech
+			} # end if
+		} elsif ( $$imposition{runstyle} eq 'Work & Tumble' ) {
+			if ( $$imposition{rows} > 2 ) {
+				my $imp1 = $imposition->copy();
+				$imp1->rows( $$imp1{rows} / 2 );
+				push @results, $imp1;
+			} # end foraech
+			if ( $$imposition{columns} >= 2 ) {
+				foreach my $columns ( 2 .. $$imposition{columns} ) {
+					my $imp2 = $imposition->copy();
+					$imp2->columns( $$imp2{columns} - ( $columns -1 ) );
+					push @results, $imp2;
+				}
+			} # end if
+
 		} else {
 
 			if ( $$imposition{rows} >= 2 ) {
