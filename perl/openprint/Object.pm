58c1ff87a6404eb9ddf42b8754824d34d66f51bf
root <root@testing.internal.point-one.com>
2016-03-31 08:58:01 -0400

    implement not operator

--- perl/openprint/Object.pm
+++ perl/openprint/Object.pm
@@ -562,6 +562,8 @@ my $add_placeholder = ( ! ( $field =~ /\?/ ) ) ?  1 : 0;
 
 	if ( sets::isin( $operator, [ '=', '!=', '<', '>', '<=', '>=', '<<=' ] ) ) {
 		return ( $field.$type.' ' . $operator . ( $add_placeholder ? ' ?' : '' ), $value );
+	} elsif ( $operator eq 'not' ) {
+		return ( 'NOT ' . $field.$type, $value );
 	} elsif ( sets::isin( $operator, [ '&&', '<@', '@>' ] ) ) {
 		if ( ref $value eq 'ARRAY' ) {
 			if ( $field =~ /^\(/ ) {
