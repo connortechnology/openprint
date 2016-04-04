b28322d040f33c475266bc09b398c5c74d9c854b
root <root@testing.internal.point-one.com>
2016-04-04 09:37:03 -0400

    Always allow an operator to complete a job

--- perl/openprint/ScheduledJob.pm
+++ perl/openprint/ScheduledJob.pm
@@ -398,7 +398,8 @@ sub get_li {
 				$html .= ssi::button( 'Paper'.$$self{id}, { onclick=> "popup_window('/employee/production/_stock_details.html','project_id=$$self{project_id}' );", text=> 'P', title=>'Paper' } );
 			} # end if
 			if ( ( $session{user_id} == $self->Shift()->operator_id() ) or ( 
-						( ! $self->Shift()->operator_id() ) and sets::isin( $session{user_id}, [ map { $_->id() } $Equipment->Operators() ] ) 
+						#( ! $self->Shift()->operator_id() ) and 
+sets::isin( $session{user_id}, [ map { $_->id() } $Equipment->Operators() ] ) 
 						) ) {
 			$html .= ssi::button( 'Complete'.$$self{id}, { onclick=>"popup_window('/employee/production/_signature_completion_popup.html', 'schedule_id=$$self{id}', { height: '100px', center: 'false' } );", text=>'Complete',title=>'Complete Job' } );
 			}
