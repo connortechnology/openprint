d1cb4aed78b828d1307788f3902b70f428540f97
root <root@testing.internal.point-one.com>
2016-04-12 11:10:28 -0400

    fix bug setting quantity_indexes when setting quantity

--- perl/openprint/Project.pm
+++ perl/openprint/Project.pm
@@ -629,7 +629,10 @@ sub save {
 
 sub quantity_indexes {
 	my ( $self ) = @_;
-	if ( ! exists $$self{quantity_indexes} ) {
+	if ( @_ > 1 ) {
+		$$self{quantity_indexes} = $_[1];
+	}
+	if ( ! $$self{quantity_indexes} ) {
 		@{$$self{quantity_indexes}} = ();
 		foreach my $qty_index ( 1 .. 3 ) {
 			push @{$$self{quantity_indexes}}, $qty_index if $$self{"quantity$qty_index"};
@@ -649,9 +652,7 @@ sub quantity {
 		return $self->ordered_quantity();
 	} elsif ( defined $qty ) {
 		$$self{"quantity$index"} = $qty;
-		if ( exists $$self{quantity_indexes} ) {
-			$$self{quantity_indexes}[$index-1] = $index;
-		} # end if
+		delete $$self{quantity_indexes};
 	} # end if
 	return $$self{'quantity'.$index};
 } # end sub quanitty
