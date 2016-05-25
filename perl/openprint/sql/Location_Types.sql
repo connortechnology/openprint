bcf210e638e9462072583700b839c27137691c3e
root <root@mail.connortechnology.com>
2016-05-25 14:47:07 -0400

    fixes to hosts locations

--- perl/openprint/sql/Location_Types.sql
+++ perl/openprint/sql/Location_Types.sql
@@ -6,3 +6,8 @@ CREATE TABLE Location_Types (
 	name	TEXT,
 	PRIMARY KEY (id)
 );
+
+INSERT INTO Location_Types (name) values ('country');
+INSERT INTO Location_Types (name) values ('state');
+INSERT INTO Location_Types (name) values ('city');
+
