23f05fb201149ccaa359e45591b56f300e407a52
root <root@testing.internal.point-one.com>
2016-08-15 12:07:26 -0400

    implement ip and mac input types

--- perl/ssi.pm
+++ perl/ssi.pm
@@ -937,6 +937,16 @@ sub input {
 		} # end if
 		$options{step} = 'any' if ! exists $options{step};
 		$options{oninput} = 'floatize_calculator(this);'.$options{oninput};
+	} elsif ( $options{type} eq 'ip' ) {
+		$options{pattern} = '[0-9\/\.\:a-fA-F]*' if ! $options{pattern};
+		$options{type} = 'text';
+		$options{step} = 'any' if ! exists $options{step};
+		$options{oninput} = q`this.value=this.value.replace(/[^\.\d%\/\*a-fA-F:]/g,'');`.$options{oninput};
+	} elsif ( $options{type} eq 'mac' ) {
+		$options{pattern} = '[0-9\-\:a-fA-F]*' if ! $options{pattern};
+		$options{type} = 'text';
+		$options{step} = 'any' if ! exists $options{step};
+		$options{oninput} = q`this.value=this.value.replace(/[^\-\d%\/\*a-fA-F:]/g,'');`.$options{oninput};
 	} # end if
 	$html .= ' value="'.html_escape($options{value}).'"' if $options{value} ne '';
 
