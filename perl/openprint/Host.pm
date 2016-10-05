b6d115820a6708c1e292845b851f9ebb2938bece
root <root@testing.internal.point-one.com>
2016-10-05 12:45:02 -0400

    Fix recursive loop in basis_mweigiht and remove quotes and turn off debugging

--- perl/openprint/Host.pm
+++ perl/openprint/Host.pm
@@ -4,7 +4,7 @@ require openprint::Object;
 package openprint::Host_Interface;
 our @ISA = qw( openprint::Object );
 use vars qw( $debug $table $serial %find_fields %fields %transforms %defaults );
-$debug = 1;
+$debug = 0;
 $serial = 'host_interfaces_id_seq';
 $table = 'host_interfaces';
 
