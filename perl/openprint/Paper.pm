69c60e4e8db72746607505e99537691382856f5b
root <root@testing.internal.point-one.com>
2016-10-07 07:19:05 -0400

    don't call to_string in basis_mweight

--- perl/openprint/Paper.pm
+++ perl/openprint/Paper.pm
@@ -1614,8 +1614,8 @@ sub basis_mweight {
 		} elsif ( ( $$self{weight} =~ /^(\d+)lb/i ) or ( $$self{weight} =~ /^(\d+)#/i ) ) {
 			$$self{basis_mweight} = 2*$1;
 		} else {
-			$$self{basis_mweight} = 'Unknown';
-			$openprint::log->error("Unable to calculated basis_mweight" . $self->to_string() );
+			#$$self{basis_mweight} = 'Unknown';
+			$openprint::log->error("Unable to calculated basis_mweight" . $$self{id} );
 		} # end if
 	} # end if
 	return $$self{basis_mweight};
