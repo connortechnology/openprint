95268e0af978af2252ed355f0deb615768ad4a08
root <root@pseudo.connortechnology.com>
2016-11-10 14:51:26 -0500

    turn off debugging

--- perl/openprint/Estimating/Folding.pm
+++ perl/openprint/Estimating/Folding.pm
@@ -960,7 +960,7 @@ $openprint::log->debug("Has a fold, doing extra checks") if DEBUG;
 											$failure_reason = "Fold no good due to max feed width $width_size > $max_feed_width on width ($$sig_specs{txtWidth}).";
 										} # end if
 									} else {
-										$openprint::log->warn("No fold match width_folds: $width_folds, height_folds: $height_folds Fold:$$Fold{name} folds: $$Fold{folds} angles:$$Fold{angles}");
+										$openprint::log->warn("No fold match width_folds: $width_folds, height_folds: $height_folds Fold:$$Fold{name} folds: $$Fold{folds} angles:$$Fold{angles}") if DEBUG;
 									} # end if
 								} # end if has an orientation
 							} # end if has max_feed_width
@@ -1096,7 +1096,7 @@ if ( 1 ) {
 											} # end if
 										} # end if
 									} else {
-										$openprint::log->warn("No fold match width_folds: $width_folds, height_folds: $height_folds Fold:$$Fold{name} folds: $$Fold{folds} angles:$$Fold{angles}");
+										$openprint::log->warn("No fold match width_folds: $width_folds, height_folds: $height_folds Fold:$$Fold{name} folds: $$Fold{folds} angles:$$Fold{angles}") if DEBUG;
 									} # end if
 								} # end if has an orientation
 								$openprint::log->debug($fits) if $fits and DEBUG;
