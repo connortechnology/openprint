ead73d9e306799a54d50085b991b3f3bb90f48f7
root <root@www7.internal.point-one.com>
2016-12-02 12:28:52 -0500

    cleanup

--- perl/openprint/Order.pm
+++ perl/openprint/Order.pm
@@ -539,7 +539,6 @@ sub send_sales_order {
 	$Email->html_body( ssi::variable_substitution( \$email_template, \%order ) );
 
 	$order{ReplacementText} = ssi::include( '/email_content/sales_order_for_admin.html', \%order );
-	$_ = MIME::QuotedPrint::encode_qp( Encode::encode('utf-8', ssi::variable_substitution( \$email_template, \%order ) ) );
 	$Email->add_pdf_attachment_from_html("Order$$self{id}",  ssi::variable_substitution( \$email_template, \%order ) );
 
 	my @project_dockets = ();
