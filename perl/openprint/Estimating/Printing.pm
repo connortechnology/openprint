353e9a2ab66515960545b8beda06c852e44f15b8
root <root@mail.connortechnology.com>
2016-12-06 12:51:57 -0500

    remove period

--- perl/openprint/Estimating/Printing.pm
+++ perl/openprint/Estimating/Printing.pm
@@ -912,7 +912,7 @@ sub get_Stocks {
 
 		foreach my $option ( @RequiredStockOptions ) {
 			if ( ! $$specs{'ddmStock'.$option} ) {
-				$$specs{alert} .= 'Please select a stock ' . lc $option .'.';
+				$$specs{alert} .= 'Please select a stock ' . lc $option;
 				return @Papers;
 			} # end if
 		} # end foreach option
