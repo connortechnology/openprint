a1e1fb2ea986d6fd72ff0bb4df380de4c1b05b42
root <root@pseudo.connortechnology.com>
2016-12-23 10:41:40 -0500

    add simple calliper calc for Pocket Folders

--- perl/openprint/Project.pm
+++ perl/openprint/Project.pm
@@ -1641,6 +1641,12 @@ sub calliper {
 				$pages = 6;
 			} elsif ( $$sig_specs{rdbTemplateType} eq '8PageFold' ) {
 				$pages = 4;
+			} elsif ( $$sig_specs{rdbTemplateType} eq '2Panel1Pocket' ) {
+# FIXME: GUTTERS
+				$pages = 3;
+			} elsif ( $$sig_specs{rdbTemplateType} eq '3Panel1Pocket' ) {
+# FIXME: GUTTERS
+				$pages = 4;
 			} else {
 				$log->error("Unknown template type n calliper $$sig_specs{rdbTemplateType}");
 			} #// end if
