d10fd5b82e9f8c2c4f610d864bc47a3c9a8a069d
root <root@mail3.point-one.com>
2016-12-23 10:32:44 -0500

    fix

--- perl/tools/ip_monitor.pl
+++ perl/tools/ip_monitor.pl
@@ -174,7 +174,7 @@ while(1) {
 
 		my $since = $now-$$Host{state_changed_on};
 		$log->debug( $Host->hostname() . ' is now ' . ( $Host->online() ? 'online' : 'offline' ) . " $since " );
-		if ( ( ! $Host->online() ) {
+		if ( ! $Host->online() ) {
 			if ( ( ! $$Host{notified} ) and ( $since > $$Host{offline_seconds} ) ) {
 				$_ = $Host->save({ notified=>1 });
 				if ( $_ ) {
