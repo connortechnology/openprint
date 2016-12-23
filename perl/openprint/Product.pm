354297c844b83f4d91f5cc5fb27e3cc52a45b5ad
root <root@pseudo.connortechnology.com>
2016-12-23 10:41:17 -0500

    implement link_to

--- perl/openprint/Product.pm
+++ perl/openprint/Product.pm
@@ -266,6 +266,9 @@ sub manufacturer {
 sub Supplier {
 	return new openprint::Company( $_[0]{supplier_id} );
 } # end sub Supplier
+sub link_to {
+	return sprintf('<a href="/product/view.html?product_id=%d">%s</a>', $_[0]{id}, ( @_ > 1 ? $_[1] : $_[0]{name} ) );
+}
 
 1;
 __END__
