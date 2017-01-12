9ed23b92898c364b11af463307b1c0676dee1742
root <root@testing.internal.point-one.com>
2017-01-12 10:37:49 -0500

    introduce excel date dateetime

--- perl/ssi.pm
+++ perl/ssi.pm
@@ -1080,6 +1080,12 @@ sub format_date {
 sub format_datetime {
 	return $_[0] ? Date::Format::time2str( $config{DateTimeFormat}, Date::Parse::str2time( $_[0] ) ) : '';
 } # end sub format_datetime
+sub format_csv_datetime {
+	return $_[0] ? Date::Format::time2str( '%Y-%m-%d %H:%M:%S', Date::Parse::str2time( $_[0] ) ) : '';
+} # end sub format_datetime
+sub format_csv_date {
+	return $_[0] ? Date::Format::time2str( '%Y-%m-%d', Date::Parse::str2time( $_[0] ) ) : '';
+} # end sub format_datetime
 
 sub link {
 	return '<link rel="stylesheet" type="text/css" href="'.hash_link($_[0]).'"/>';
