82f4ab171ba7aed1a8b6a19e245e3ffeb6a2e379
root <root@pseudo.connortechnology.com>
2017-01-26 16:07:52 -0500

    only load project specs if there is a proejct service in get_book_type

--- perl/openprint/print.pm
+++ perl/openprint/print.pm
@@ -540,9 +540,11 @@ sub get_book_type {
 			return $service;
 		} # end if
 	} # end foreach
-	my $printing_specs = openprint::service::get_specs_ref( $Project, $$services{''}[0] );
-	if ( $$printing_specs{rdbTemplateType} eq 'PerfectBound' ) {
-		return 'PerfectBound';
+	if ( $$services{''} and @{$$services{''}} ) {
+		my $printing_specs = openprint::service::get_specs_ref( $Project, $$services{''}[0] );
+		if ( $$printing_specs{rdbTemplateType} eq 'PerfectBound' ) {
+			return 'PerfectBound';
+		} # end if
 	} # end if
 	return;
 } # end sub get_book_type
