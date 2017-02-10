35155a17af23267c1bd283249d41a0549f3ba203
root <root@mail3.point-one.com>
2017-02-10 12:37:07 -0500

    implement table level locks

--- perl/openprint/Object.pm
+++ perl/openprint/Object.pm
@@ -1359,35 +1359,53 @@ sub lock {
 	my ( $caller, undef, $line ) = caller;
 
 	my $type = ref $_[0];
-	if ( $_[0]{ac} ) {
-		#already locked
-		$openprint::log->debug("ALREADY LOCKED $type for $_[0]{id} ac: $_[0]{ac} caller: $caller line: $line object ref:" . $_[0]) if DEBUG_ALL;
-		$_[0]{ac} += 1;
+	my $ac;
+	if ( $type ) {
+		# Row lock
+		if ( $_[0]{ac} ) {
+			#already locked
+			$openprint::log->debug("ALREADY LOCKED $type for $_[0]{id} ac: $_[0]{ac} caller: $caller line: $line object ref:" . $_[0]) if DEBUG_ALL;
+			$_[0]{ac} += 1;
+		} else {
+			$_[0]{ac} = sql::start_transaction( $openprint::dbh );
+			$openprint::log->debug("LOCKING $type for $_[0]{id} ac: $_[0]{ac} caller: $caller line: $line object ref:" . $_[0]) if DEBUG_ALL;
+			my $table = eval '$'.$type.'::table';
+			$dbh->do( "LOCK TABLE $table IN EXCLUSIVE MODE" ) or $log->error( DBI->errstr );
+		} # end if
+		$ac = $_[0]{ac};
 	} else {
-		$_[0]{ac} = sql::start_transaction( $openprint::dbh );
-		$openprint::log->debug("LOCKING $type for $_[0]{id} ac: $_[0]{ac} caller: $caller line: $line object ref:" . $_[0]) if DEBUG_ALL;
+		$type = $_[0];
+		# Table Lock
+		my $ac = sql::start_transaction( $openprint::dbh );
+		$openprint::log->debug("LOCKING $type table ac: caller: $caller line: $line" ) if DEBUG_ALL;
 		my $table = eval '$'.$type.'::table';
 		$dbh->do( "LOCK TABLE $table IN EXCLUSIVE MODE" ) or $log->error( DBI->errstr );
-	} # end if
+	} # end  if
+	return $ac;
 
 } # end sub lock
 
 sub unlock {
 	my ( $caller, undef, $line ) = caller;
 	my $type = ref $_[0];
-	$openprint::log->debug("UNLOCKING $type for $_[0]{id} ac: $_[0]{ac} caller: $caller line: $line" . $_[0]) if DEBUG_ALL;
-	if ( ! exists $_[0]{ac} ) {
-		$_[0]{ac} = $openprint::dbh->{AutoCommit};
-	} # end if
-	if ( ! $_[0]{ac} ) {
-		$openprint::log->debug("unlock with no AC! $caller:$line object $type $_[0]{id}");
-		return;
-	} # end if
-	if ( $_[0]{ac} == 1 ) {
-		sql::end_transaction( $openprint::dbh, $_[0]{ac} );
+	if ( $type ) {
+		$openprint::log->debug("UNLOCKING $type for $_[0]{id} ac: $_[0]{ac} caller: $caller line: $line" . $_[0]) if DEBUG_ALL;
+		if ( ! exists $_[0]{ac} ) {
+			$_[0]{ac} = $openprint::dbh->{AutoCommit};
+		} # end if
+		if ( ! $_[0]{ac} ) {
+			$openprint::log->debug("unlock with no AC! $caller:$line object $type $_[0]{id}");
+			return;
+		} # end if
+		if ( $_[0]{ac} == 1 ) {
+			sql::end_transaction( $openprint::dbh, $_[0]{ac} );
+		} else {
+			$_[0]{ac} -= 1;
+		} # end if
 	} else {
-		$_[0]{ac} -= 1;
-	} # end if
+		$type = $_[0];
+		sql::end_transaction( $openprint::dbh, 0 );
+	}
 } # end sub unlock
 
 sub Keywords {
