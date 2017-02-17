86eedbff70a73ea4f993c09e60dd612b2639486a
Isaac Connor <iconnor@connortechnology.com>
2017-02-17 17:38:52 -0500

    fix currency conversion in get_price

--- perl/openprint/Material.pm
+++ perl/openprint/Material.pm
@@ -150,7 +150,8 @@ sub get_price {
 	return if ! %price;
 
 	$price{currency_id} = $Pricelist->currency_id();
-	openprint::Currency::convert( \%price ) if $$Pricelist{currency_id} != $openprint::session{Pricelist_id};
+$openprint::log->error("Pricelist currency is $$Pricelist{currency_id} != $openprint::session{Currency_id} ");
+	openprint::Currency::convert( \%price ) if $$Pricelist{currency_id} != $openprint::session{Currency_id};
 
 	return %price;
 } # end sub get_price
@@ -164,7 +165,7 @@ sub get_Price {
 	return if ! %price;
 
 	$price{currency_id} = $Pricelist->currency_id();
-	openprint::Currency::convert( \%price ) if $$Pricelist{currency_id} != $openprint::session{Pricelist_id};
+	openprint::Currency::convert( \%price ) if $$Pricelist{currency_id} != $openprint::session{Currency_id};
 
 	return \%price;
 }
