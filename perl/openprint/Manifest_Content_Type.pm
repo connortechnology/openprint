84924a7eed22c4df990fe9e42e9dd3184b33b3aa
Isaac Connor <iconnor@connortechnology.com>
2017-03-23 12:50:15 -0400

    Make PruchaseORder_Content take an options hash to turn off docket and fsc lookups

--- perl/openprint/Manifest_Content_Type.pm
+++ perl/openprint/Manifest_Content_Type.pm
@@ -15,42 +15,42 @@ $table = 'manifest_content_types';
 $serial = 'manifest_content_types_id_seq';
 
 %fields = (
-	id				=>	'id',
-	cost			=>	'cost',
+	id						=>	'id',
+	cost					=>	'cost',
 	cost_units		=>	'cost_units',
-	docket			=>	'docket',
-	po_id			=>	'po_id',
+	docket				=>	'docket',
+	po_id					=>	'po_id',
 	po_content_id	=>	'po_content_id',
 	manifest_id		=>	'manifest_id',
-	paper_id		=>	'paper_id',
+	paper_id			=>	'paper_id',
 	supplier_invoice	=>	'supplier_invoice',
 	item_count			=>	'item_count',
-	type			=>	'type',
+	type					=>	'type',
 	manufacturers_name	=>	'manufacturers_name',
 	condition_id	=>	'condition_id',
 );
 %find_fields = (
 	total_quantity	=>	'(SELECT SUM(quantity) FROM manifestcontents WHERE manifestcontents.manifest_id=manifest_content_types.manifest_id and type_id=manifest_content_types.id)',
-	skid_id			=>	'(SELECT skid_id FROM manifestcontents WHERE manifestcontents.manifest_id=manifest_content_types.manifest_id and type_id=manifest_content_types.id)',
+	skid_id					=>	'(SELECT skid_id FROM manifestcontents WHERE manifestcontents.manifest_id=manifest_content_types.manifest_id and type_id=manifest_content_types.id)',
 );
 
 %transforms = (
-	paper_id		=> [ 's/\D//g' ],
-	po_id			=> [ 's/\D//g' ],
+	paper_id			=> [ 's/\D//g' ],
+	po_id					=> [ 's/\D//g' ],
 	po_content_id	=> [ 's/\D//g' ],
 	item_count		=> [ 's/\D//g' ],
-	docket			=> [ 's/\D//g' ],
-	cost			=> [ 's/[^\d\.]//g' ],
-	type			=> [ 's/^\s+//', 's/\s+$//', 's/\s\s+/ /g' ],
+	docket				=> [ 's/\D//g' ],
+	cost					=> [ 's/[^\d\.]//g' ],
+	type					=> [ 's/^\s+//', 's/\s+$//', 's/\s\s+/ /g' ],
 );
 
 %defaults = (
-	cost			=>	undef,
-	docket			=>	undef,
-	po_id			=>	undef,
+	cost					=>	undef,
+	docket				=>	undef,
+	po_id					=>	undef,
 	po_content_id	=>	undef,
-	paper_id		=>	undef,
-	type			=>	undef,
+	paper_id			=>	undef,
+	type					=>	undef,
 	item_count		=>	undef,
 	manufacturers_name	=>	undef,
 	condition_id	=>	undef,
@@ -70,9 +70,11 @@ sub PurchaseOrder {
 } # end sub PurchaseOrder
 
 sub PurchaseOrder_Content {
+	my $options = @_ > 1 ? $_[1] : {};
+
 	if ( ! exists $_[0]{PurchaseOrder_Content} ) {
 		require openprint::PurchaseOrder_Content;
-		if ( ! $_[0]{po_content_id} ) {
+		if ( ( ! $_[0]{po_content_id} ) and ( $_[0]{po_id} ) ) {
 			my $PO = new openprint::PurchaseOrder( $_[0]{po_id} );
 			my $Paper = $_[0]->Paper();
 			foreach my $POC ( $PO->Contents() ) {
@@ -81,12 +83,16 @@ sub PurchaseOrder_Content {
 					$openprint::log->debug("not the right type POC: $$POC{type} != $$Paper{type} Stock");
 					next;
 				} # en dif
-				if ( $POC->docket() and $_[0]{docket} ) {
-					next if $POC->docket() ne $_[0]{docket};
+				if ( ! $$options{ignore_docket} ) {
+					if ( $POC->docket() and $_[0]{docket} ) {
+						$openprint::log->debug("not the right docet POC: $$POC{docket} != $_[0]{docket}");
+						next if $POC->docket() ne $_[0]{docket};
+					}
 				}
 				my ( $weight ) = $POC->item() =~ /(\d+)\w*lb/i;
 				if ( $weight ) {
 					$weight = Math::Round::nearest(1,$weight*2);
+$openprint::log->debug("Looking for $weight basis_weight");
 					if( $Paper->basis_mweight() ) {
 						my $basis_weight = Math::Round::nearest(1,$Paper->basis_mweight());
 						if ( $weight != $basis_weight ) {
@@ -97,7 +103,7 @@ sub PurchaseOrder_Content {
 						} 
 					} else {
 						my $paper_weight;
-						if ( ( $paper_weight ) = $Paper->weight() =~ /(\d+)lb/ ) {
+						if ( ( $paper_weight ) = $Paper->weight() =~ /(\d+)lb/i ) {
 							if ( $weight != $paper_weight ) {
 								$openprint::log->debug("Wrong weight: 2*$weight != " . $paper_weight ) if $debug;
 								next;
@@ -127,17 +133,19 @@ sub PurchaseOrder_Content {
 						} # end if
 					}
 				} # end if
-				if ( $Paper->fsc_code() and ( $POC->item() !~ /FSC/ ) ) {
-					$openprint::log->debug("FSC Mismatch ") if $debug;
-					next;
-				} elsif ( (!$Paper->fsc_code()) and $POC->item() =~ /FSC/ ) {
-					$openprint::log->debug("FSC Mismatch") if $debug;
-					next;
+				if ( ! $$options{ignore_fsc} ) {
+					if ( $Paper->fsc_code() and ( $POC->item() !~ /FSC/ ) ) {
+						$openprint::log->debug("FSC Mismatch ") if $debug;
+						next;
+					} elsif ( (!$Paper->fsc_code()) and $POC->item() =~ /FSC/ ) {
+						$openprint::log->debug("FSC Mismatch") if $debug;
+						next;
+					} # end if
 				} # end if
 				$_[0]{PurchaseOrder_Content} = $POC;
 				last;
 			} # end foreach POC
-			$_[0]{PurchaseOrder_Content} = new openprint::PurchaseOrder_Content() if ! $_[0]{PurchaseOrder_Content};
+			#$_[0]{PurchaseOrder_Content} = new openprint::PurchaseOrder_Content() if ! $_[0]{PurchaseOrder_Content};
 		} else {
 			$_[0]{PurchaseOrder_Content} = new openprint::PurchaseOrder_Content($_[0]{po_content_id});
 		} # end if
