b3a739b43d7d7ad632f0bf1b869ab0895f058751
Isaac Connor <iconnor@connortechnology.com>
2017-04-12 12:19:14 -0400

    when unselecting a location, don't error and clear the address

--- perl/openprint/employee_production_labels.pm
+++ perl/openprint/employee_production_labels.pm
@@ -28,28 +28,31 @@ sub _label {
 			$param{value} =~ s/<br\/>/\n/ig;
 			$Label->set_data($param{field}=>$param{value});
 		} elsif ( exists $param{location_id} ) {
-			my $old = $Label->get_data( $param{field} );
 
 			my $old_location_id = $Label->get_data($param{field}.'_location_id');
 			$Label->set_data( $param{field}.'_location_id' => $param{location_id} );
 
-			my $OldLocation = openprint::Location->find_one( id=>$old_location_id ) if $old_location_id;
-			my $NewLocation = openprint::Location->find_one(id=>$param{location_id});
-			if ( $NewLocation ) {
-				if ( $OldLocation ) {
-					my $oldaddress = $OldLocation->address_formatted();
-					$oldaddress =~ s/\n/<br\/>/g;
-					my ( $first, $last ) = $old =~ /(.*)$oldaddress(.*)/m;
-					my $newaddress = $1.$NewLocation->address_formatted().$2;
-					$newaddress =~ s/\n/<br\/>/g;
-					$Label->set_data($param{field}=>$newaddress);
+			if ( $param{location_id} ) {
+
+				my $OldLocation = openprint::Location->find_one( id=>$old_location_id ) if $old_location_id;
+				my $NewLocation = openprint::Location->find_one(id=>$param{location_id});
+				if ( $NewLocation ) {
+					if ( $OldLocation ) {
+						my $old = $Label->get_data( $param{field} );
+						my $oldaddress = $OldLocation->address_formatted();
+						$oldaddress =~ s/\n/<br\/>/g;
+						my ( $first, $last ) = $old =~ /(.*)$oldaddress(.*)/m;
+						my $newaddress = $1.$NewLocation->address_formatted().$2;
+						$newaddress =~ s/\n/<br\/>/g;
+						$Label->set_data($param{field}=>$newaddress);
+					} else {
+						my $newaddress = $NewLocation->address_formatted();
+						$newaddress =~ s/\n/<br\/>/g;
+						$Label->set_data($param{field}=>$newaddress);
+					}
 				} else {
-					my $newaddress = $NewLocation->address_formatted();
-					$newaddress =~ s/\n/<br\/>/g;
-					$Label->set_data($param{field}=>$newaddress);
+					$variable{error} .= 'Location not found.';
 				}
-			} else {
-				$variable{error} .= 'Location not found.';
 			}
 		} # end if
 
