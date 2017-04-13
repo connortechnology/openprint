57541a4628913260ce3165040020684526f25fcf
Isaac Connor <iconnor@connortechnology.com>
2017-04-13 15:46:00 -0400

    fix

--- perl/openprint/Estimating/Folding.pm
+++ perl/openprint/Estimating/Folding.pm
@@ -31,7 +31,7 @@ require openprint::service;
 require openprint::ServiceType;
 require openprint::Estimating::Perforating;
 
-use constant DEBUG => 0;
+use constant DEBUG => 1;
 use constant DEBUG_NEEDS => 0;
 
 my @equipment;
@@ -2018,7 +2018,7 @@ sub signature_summary {
 						"FoldType-$form-$qty_index-$fold_index"} );
 			} # end foreach
 			my $html = join('<br/>', ( ' on ' . $Equipment->name() ), sort { $a cmp $b } @folds);
-			my $Press = openprint::Equipment->find( strid=>$$sig_specs{"ddmPress$qty_index"} );
+			my $Press = openprint::Equipment->find_one( strid=>$$sig_specs{"ddmPress$qty_index"} );
 			if ( $Press and ( $$Press{id} != $$Equipment{id} ) and ($Press->specification('Folding Capable') eq 'When Printing' ) ) {
 				$html .= '<br/><span class="warning">Folding Offline</span>';
 			}
