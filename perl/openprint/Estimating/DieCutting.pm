568125dfc4f6730fe200f5f9cdd05eb513681695
root <root@mail.connortechnology.com>
2017-05-24 11:03:17 -0400

    use a simple if instead of if defined because '' is defined...

--- perl/openprint/Estimating/DieCutting.pm
+++ perl/openprint/Estimating/DieCutting.pm
@@ -215,7 +215,7 @@ sub calc_price {
 #$run_price += $qty * $folding_price;
 #} # end fi
 
-	if ( ( defined $$specs{'txtHoleClearingHoles-'.$form} ) and ( $$specs{'txtHoleClearingHoles-'.$form} > 0 ) ) {
+	if ( $$specs{'txtHoleClearingHoles-'.$form} and ( $$specs{'txtHoleClearingHoles-'.$form} > 0 ) ) {
 		my $hole_qty = $$specs{'txtHoleClearingHoles-'.$form} * $$Imposition{imposition};
 		my %HoleClearingPrice = openprint::service::get_price_object( 'HoleClearing', $$specs{'txtHoleClearingHoles-'.$form} * $$specs{"txtQuantity$qty_index"}, $Equipment ); 
 		$HoleClearingPrice{Total} = $impressions * $HoleClearingPrice{Price} * $hole_qty;
