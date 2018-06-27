619e19ca2f0f5c8fe482394ea31324723a0b9997
root <root@mail.connortechnology.com>
2018-06-27 10:31:26 -0400

    Handle empty hostname in link_to in Host.

--- perl/openprint/Host.pm
+++ perl/openprint/Host.pm
@@ -93,7 +93,7 @@ $serial = 'hosts_id_seq';
 );
 
 sub destroy {
-	my $error;
+	my $error = '';
 	require openprint::Log;
 	foreach my $Log ( openprint::Log->find( host_id=>$_[0]{id} ) ) {
 		$error .= $Log->destroy();
@@ -360,7 +360,7 @@ sub is_wap {
 
 sub link_to {
 	return sprintf('<a href="/employee/it/host.html?host_id=%d">%s</a>',
-      $_[0]->id(), ( @_ > 1 ? $_[1] : ( $_[0]->hostname() ? $_[0]->hostname() : '' ) )
+      $_[0]->id(), ( ( @_ > 1 and $_[1] ) ? $_[1] : ( $_[0]->hostname() ? $_[0]->hostname() : '' ) )
       );
 }
 
