07803a3bb6276186dbf6953d6eb4b5b6cde45865
root <root@mail1.point-one.com>
2018-07-17 12:20:19 -0400

    Cache Payments introduce sub Payments in Order

--- perl/openprint/Order.pm
+++ perl/openprint/Order.pm
@@ -20,7 +20,7 @@ require openprint::Payment;
 require openprint::Tax;
 require openprint::Order_Notification;
 
-$debug = 1;
+$debug = 0;
 
 $table = 'orders';
 $serial = 'orders_id_seq';
@@ -648,10 +648,19 @@ sub Tax {
 	return $result;
 } # end sub Tax
 
+sub Payments {
+	my $self = shift;
+	$$self{Payments} = shift if @_;
+	if ( $$self{id} and ! $$self{Payments} ) {
+		$$self{Payments} = [ openprint::Payment->find(order_id=>$$self{id},order=>$openprint::Payment::fields{received_on}.' DESC') ];
+	}
+	return @{$$self{Payments}};
+}
+
 sub paid {
 	$_[0]{paid} = $_[1] if ( @_ == 2 );
 	if ( $_[0]{id} and ! defined $_[0]{paid} ) {
-		$_[0]{paid} = misc::sum( map { $_->amount() } openprint::Payment->find(order_id=>$_[0]{id}) );
+		$_[0]{paid} = misc::sum( map { $_->amount() } $_[0]->Payments() );
 	} # end if
 	return $_[0]{paid};
 } # end sub paid
@@ -667,17 +676,20 @@ sub paid_on_seconds {
 	if ( $_[0]->paid() < $_[0]->total() ) {
 		return time;
 	} # end if
-	my $Last_Payment = openprint::Payment->find_one('order_id'=>$_[0]{id},'order'=>$openprint::Payment::fields{received_on}.' DESC');
+	my @Payments = $_[0]->Payments();
+	my $Last_Payment = $Payments[-1];
 	if ( ! $Last_Payment ) {
 		return time;
 	} # end if
 	return Date::Parse::str2time( $Last_Payment->received_on() );
 } # end sub paid_on
+
 sub paid_on {
 	if ( $_[0]->paid() < $_[0]->total() ) {
 		return Date::Format::time2str( '%Y-%m-%d %H:%M:%S', time );
 	} # end if
-	my $Last_Payment = openprint::Payment->find_one('order_id'=>$_[0]{id},'order'=>$openprint::Payment::fields{received_on}.' DESC');
+	my @Payments = $_[0]->Payments();
+	my $Last_Payment = $Payments[-1];
 	if ( ! $Last_Payment ) {
 		return Date::Format::time2str( '%Y-%m-%d %H:%M:%S', time );
 	} # end if
