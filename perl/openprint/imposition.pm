ee31aa3ef45ee2f339208276d9e047b6aad381f0
root <root@mail1.point-one.com>
2018-07-23 09:40:51 -0400

    Fix discretion typo

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -240,7 +240,7 @@ sub calc_setup_object {
 			$openprint::log->debug("Proper grain Paper(".$Paper->grain_direction().") Press($press_grain)") if DEBUG;
 		} # end if
 	} elsif ( DEBUG ) {
-		$openprint::log->debug("No grain discretion. $$Paper{gsm}gsm");
+		$openprint::log->debug("No grain direction. $$Paper{gsm}gsm");
 	} # end if press_grain
 	if ( $run_style eq 'Perfecting' ) {
 		my $press_grain = $Press->specification('Perfecting Grain', $Paper->gsm() );
@@ -268,7 +268,7 @@ sub calc_setup_object {
 				$openprint::log->debug("Proper perfecting grain Paper(".$Paper->grain_direction().") Press($press_grain)") if DEBUG;
 			} # end if
 		} elsif ( DEBUG ) {
-			$openprint::log->debug("No perfecting grain discretion. $$Paper{gsm}");
+			$openprint::log->debug("No perfecting grain direction. $$Paper{gsm}");
 		} # end if press_grain
 	} # end if Perfecting
 
