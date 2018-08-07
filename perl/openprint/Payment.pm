448de9841a32d3ee0d52bbb0888c3b889d881ad9
Isaac Connor <iconnor@connortechnology.com>
2018-08-07 13:07:24 -0400

    Put back Order paid updating

--- perl/openprint/Payment.pm
+++ perl/openprint/Payment.pm
@@ -58,9 +58,13 @@ $serial = 'payments_id_seq';
 sub save {
 	$_[0]->set( $_[1] ) if $_[1];
 	$_[0]->remaining(undef);
-    my $error = $_[0]->SUPER::save( );
-	if ( (! $error) and $_[0]{order_id} ) {
-		#$_[0]->Order()->paid(undef);
+  my $error = $_[0]->SUPER::save( );
+	if ( (!$error) and $_[0]{order_id} ) {
+# I put this back so that order paid status's update. 2018-08-07
+		my $Order = $_[0]->Order();
+		$Order->Payments(undef);
+		$Order->paid(undef);
+		$error .= $Order->save();
 	} # end if
 	return $error;
 } # end sub save
