1f872b7872bbba4af23837a14ff0d0f75f45f0b9
Isaac Connor <isaac@zoneminder.com>
2018-09-19 16:02:47 -0400

    destroy was duplicated. Remove one. Fixup spacing

--- perl/openprint/Product_Category.pm
+++ perl/openprint/Product_Category.pm
@@ -31,39 +31,19 @@ $table = 'Product_Categories';
 	sorting			=>	undef,
 );
 
-
 sub destroy {
 	my $self = shift;
 	return if ! $$self{id};
 	my $error = '';
 	my $ac = sql::start_transaction( $openprint::dbh );
-	foreach my $Category ( $self->Categories() ) {
-		$error .= $Category->save({ parent_ids=>sets::exclude( [ $Category->id() ], $Category->parent_ids() ) });
-	}
-	foreach my $Product ( openprint::Product->find( 'category_id' => $$self{id},'deleted'=>[0,1] ) ) {
-		$error .= $Product->save({'category_id'=>undef});
-	} # end foreach
-	$error .= $self->SUPER::delete();
-	sql::end_transaction( $openprint::dbh, $ac );
-	
-	# Add record to audit log - action "Delete Product Category".
-	new openprint::Log()->save({'action'=>'Delete Product Category', 'note'=> "Product Category ID: $$self{id} Name: $$self{name}"});
-	return $error;
-} # end sub destroy
-
-sub destroy {
-	my $self = shift;
-	return if ! $$self{id};
-	my $error = '';
-	my $ac = sql::start_transaction( $openprint::dbh );
-	foreach my $Product ( openprint::Product->find( 'category_id' => $$self{id},'deleted'=>[0,1] ) ) {
-		$error .= $Product->save({'category_id'=>undef});
+	foreach my $Product ( openprint::Product->find(category_id=>$$self{id}, deleted=>[0,1]) ) {
+		$error .= $Product->save({category_id=>undef});
 	} # end foreach
 	$error .= $self->SUPER::destroy();
 	sql::end_transaction( $openprint::dbh, $ac );
-	
+
 	# Add record to audit log - action "Delete Product Category".
-	new openprint::Log()->save({'action'=>'Delete Product Category', 'note'=> "Product Category ID: $$self{id} Name: $$self{name}"});
+	new openprint::Log()->save({action=>'Delete Product Category', note=> "Product Category ID: $$self{id} Name: $$self{name}"});
 	return $error;
 } # end sub destroy
 
@@ -71,6 +51,7 @@ sub products {
 Carp::cluck("Deprecated call openprint::Product_Category::products");
 	return $_[0]->Products();
 } # end sub products
+
 sub Products {
 	my $self = shift;
 	my %params = @_;
