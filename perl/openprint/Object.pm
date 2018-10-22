ff6120707e46adf096d6a296478dd773d546d991
root <root@mail3.point-one.com>
2018-10-22 11:09:44 -0400

    Add tests for empty type in object

--- perl/openprint/Object.pm
+++ perl/openprint/Object.pm
@@ -391,6 +391,10 @@ sub changes {
 	my ( $self, $params ) = @_;
 
 	my $type = ref $self;
+	if ( ! $type ) {
+		my ( $caller, undef, $line ) = caller;
+		$log->error("No type in Object::changes. self:$self from  $caller:$line");
+	}
 	my $fields = eval ('\%'.$type.'::fields');
 	if ( ! $fields ) {
 $log->warn('Object::changes called on an object with no fields');
@@ -431,6 +435,10 @@ sub set {
 	my @set_fields = ();
 
 	my $type = ref $self;
+	if ( ! $type ) {
+		my ( $caller, undef, $line ) = caller;
+		$log->error("No type in Object::set. self:$self from  $caller:$line");
+	}
 	my %fields = eval ('%'.$type.'::fields');
 	if ( ! %fields ) {
 		$log->warn('Object::set called on an object with no fields');
@@ -510,6 +518,10 @@ sub clone {
 sub delete {
 	my ( $self ) = @_;
 	my $type = ref $self;
+	if ( ! $type ) {
+		my ( $caller, undef, $line ) = caller;
+		$log->error("No type in Object::delete. self:$self from  $caller:$line");
+	}
 
 	my $table = eval '$'.$type.'::table';
 	my $debug = eval '$'.$type.'::debug';
@@ -1046,7 +1058,11 @@ $log->debug("returning to $caller:$line from find_one") if DEBUG_ALL;
 sub AUTOLOAD {
 	no strict;
 	my ( $self, $newvalue ) = @_;
-	my $type = ref($_[0]);
+	my $type = ref($self);
+	if ( ! $type ) {
+		my ( $caller, undef, $line ) = caller;
+		$log->error("No type in Object::AUTOLOAD. self:$self from  $caller:$line");
+	}
 	my $name = $AUTOLOAD;
 	$name =~ s/.*://;
 	my $fields = eval '\%'.$type.'::fields';
