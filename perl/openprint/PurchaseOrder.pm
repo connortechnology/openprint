8f00946d08e2c91616690ff7eae91a300d0125d9
Isaac Connor <iconnor@connortechnology.com>
2018-11-23 13:48:01 -0500

    turn on debugging

--- perl/openprint/PurchaseOrder.pm
+++ perl/openprint/PurchaseOrder.pm
@@ -11,7 +11,7 @@ use vars qw( $debug $log $dbh %config %session $table $serial %fields %find_fiel
 *config = \%openprint::config;
 *session = \%openprint::session;
 
-$debug = 0;
+$debug = 1;
 
 require sql;
 require ssi;
