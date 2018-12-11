be047e966edf6ad5bd6462106e16459d7779d445
root <root@mail.connortechnology.com>
2018-12-11 17:24:18 -0500

    fix product pricing

--- perl/openprint/product_priceset.pm
+++ perl/openprint/product_priceset.pm
@@ -23,16 +23,16 @@ sub load {
 
 	my $Pricelist = new openprint::Pricelist( $self->{list_index} );
 
-    $_ = q{SELECT min, max, units, cost, markup, price, discountable FROM Product_Prices
-		WHERE product_id=? AND pricelist_id=?};
-	$_ .= "AND ($self->{qty} :: numeric >= min OR min is NULL) AND ($self->{qty} :: numeric <= max OR max IS NULL)" if $self->{qty};
-    my @records = sql::execute( $openprint::log, $openprint::dbh, $_, @$self{'product_index','list_index'} );
-    while ( @records ) {
-		my $price = openprint::product_price->new( $self->{log}, $self->{dbh}, $self );
-		$price->set( undef, splice @records, 0, 7 );
-		$$price{'currency_id'} = $Pricelist->currency_id();
-		push @{$self->{prices}}, $price;
-    } # end while
+  $_ = q{SELECT min, max, '', units, cost, markup, price, discountable FROM Product_Prices
+  WHERE product_id=? AND pricelist_id=?};
+  $_ .= "AND ($self->{qty} :: numeric >= min OR min is NULL) AND ($self->{qty} :: numeric <= max OR max IS NULL)" if $self->{qty};
+  my @records = sql::execute( $openprint::log, $openprint::dbh, $_, @$self{'product_index','list_index'} );
+  while ( @records ) {
+    my $price = openprint::product_price->new( $self->{log}, $self->{dbh}, $self );
+    $price->set( undef, splice @records, 0, 8 );
+    $$price{'currency_id'} = $Pricelist->currency_id();
+    push @{$self->{prices}}, $price;
+  } # end while
 }
 
 1;
