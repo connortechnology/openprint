3d6e9eab9b5f2323f3793ae180b3e3ce6c8ad4a1
root <root@mail.connortechnology.com>
2018-12-17 14:31:15 -0500

    spacing, but also fix priority of tests fixing basis weight checks

--- perl/openprint/Paper.pm
+++ perl/openprint/Paper.pm
@@ -1847,11 +1847,15 @@ sub check {
   if ( abs( POSIX::ceil( $Paper->basis_mweight()) - POSIX::ceil( $Copy->basis_mweight(undef)) ) -1 > 0 ) {
     return "may have invalid basis weight current:$$Paper{basis_mweight} != calculated:$$Copy{basis_mweight}";
   }
-  if ( $Paper->brand() =~ /cover/i or $Paper->weight() =~ /cover/i and ( $Paper->basis_width() != 20 or $Paper->basis_height() != 26 ) ) {
-	  return "may have has wrong basis size.";
-    }
+  if (
+   ( $Paper->brand() =~ /cover/i or $Paper->weight() =~ /cover/i ) 
+     and
+   ( $Paper->basis_width() != 20 or $Paper->basis_height() != 26 )
+ ) {
+	  return 'may have the wrong basis size.';
+  }
   if ( ( $Paper->finish() =~ /1 side/i ) and ( $Paper->doublesided() ) ) {
-    return "appears to be C1S, but is marked double sided.";
+    return 'appears to be C1S, but is marked double sided.';
   }
 
 	return;
