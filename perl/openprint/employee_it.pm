9ead5e6b768c5faa3d703e7af014fcbec71c2efc
Isaac Connor <iconnor@connortechnology.com>
2019-02-07 14:51:28 -0500

    merge employee_it from envirotrack

--- perl/openprint/employee_it.pm
+++ perl/openprint/employee_it.pm
@@ -942,9 +942,19 @@ sub _interface {
             action =>'IP Changed',
           });
       } # end foreach I
+      $variable{PageContent} = "{result:'ok'}";
     } #endif action
   } # end if action
-  $variable{PageContent} = "{result:'ok'}";
+}
+
+sub _backup_files {
+  my $Backup = $variable{Backup} = new openprint::Backup($param{backup_id});
+  if ( $param{file} ) {
+    $r->headers_out->{'Content-Disposition'} = "attachment; filename=\"$param{file}\"";
+    $r->content_type("application/octet-stream; name=\"$param{file}\"");
+    $r->sendfile(join('/', $Backup->dest_path(), $param{path}, $param{file}));
+    return;
+  }
 }
 
 1;
