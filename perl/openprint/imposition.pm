afb1f2c371a6146180ed82be7a1d807aec6dcbf0
Isaac Connor <iconnor@connortechnology.com>
2019-04-04 10:53:12 -0400

    Fix press grain setting

--- perl/openprint/imposition.pm
+++ perl/openprint/imposition.pm
@@ -217,9 +217,9 @@ sub calc_setup_object {
 	} else {
 		$press_grain = $Press->Specification('Grain');
 		if ( $$press_grain{units} eq 'gsm' ) {
-			$press_grain = $Press->Specification('Grain', $Paper->gsm());
+			$press_grain = $Press->specification('Grain', $Paper->gsm());
 		} elsif ( $$press_grain{units} eq 'calliper' ) {
-			$press_grain = $Press->Specification('Grain', $Paper->calliper());
+			$press_grain = $Press->specification('Grain', $$Paper{calliper});
 		} else {
 			$press_grain = $$press_grain{value};
 		}
@@ -228,17 +228,17 @@ sub calc_setup_object {
 	if ( $press_grain and $press_grain ne 'Both' ) {
 		if ( $press_grain eq 'Long' ) {
 			if ( $Paper->grain_direction() ne $Paper->long() ) {
-				$openprint::log->debug("Improper grain Paper(".$Paper->grain_direction().") Long (".$Paper->long().")") if DEBUG;
+				$openprint::log->debug('Improper grain Paper('.$Paper->grain_direction().') Long ('.$Paper->long().')') if DEBUG;
 				return;
 			} elsif ( DEBUG ) {
-				$openprint::log->debug("PROPER grain Paper(".$Paper->grain_direction().") Long (".$Paper->long().")");
+				$openprint::log->debug('PROPER grain Paper('.$Paper->grain_direction().') Long ('.$Paper->long().')');
 			} # en dif
 		} elsif ( $press_grain eq 'Short' ) {
 			if ( $Paper->grain_direction() ne $Paper->short() ) {
-				$openprint::log->debug("Improper grain Paper(".$Paper->grain_direction().") Short (".$Paper->short().")") if DEBUG;
+				$openprint::log->debug('Improper grain Paper('.$Paper->grain_direction().') Short ('.$Paper->short().')') if DEBUG;
 				return;
 			} elsif ( DEBUG ) {
-				$openprint::log->debug("Proper grain Paper(".$Paper->grain_direction().") Short (".$Paper->short().")") if DEBUG;
+				$openprint::log->debug('Proper grain Paper('.$Paper->grain_direction().') Short ('.$Paper->short().')');
 			} # en dif
 		} elsif ($press_grain ne $Paper->grain_direction() ) {
 			$openprint::log->debug("Improper grain Paper(".$Paper->grain_direction().") Press($press_grain)") if DEBUG;
