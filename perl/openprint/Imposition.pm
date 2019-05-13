ae1de065471004bfcff8be927297fc77acafd3c1
Isaac Connor <iconnor@connortechnology.com>
2019-05-13 15:16:19 -0400

    Fix page layout on non-books

--- perl/openprint/Imposition.pm
+++ perl/openprint/Imposition.pm
@@ -432,10 +432,18 @@ sub load {
 	} else {
 		# It's a brochure or something, so can't be cut.
 
-		#$$self{page_rows} = POSIX::ceil($$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
-		$$self{page_rows} = Math::Round::nearest(1,$$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
-		#$$self{page_columns} = POSIX::ceil($$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
-		$$self{page_columns} = Math::Round::nearest(1,$$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
+		
+		if ( $$self{image_orientation} == Vertical ) {
+#$$self{page_rows} = POSIX::ceil($$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
+			$$self{page_columns} = Math::Round::nearest(1,$$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
+#$$self{page_columns} = POSIX::ceil($$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
+			$$self{page_rows} = Math::Round::nearest(1,$$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
+		} else {
+#$$self{page_rows} = POSIX::ceil($$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
+			$$self{page_rows} = Math::Round::nearest(1,$$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
+#$$self{page_columns} = POSIX::ceil($$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
+			$$self{page_columns} = Math::Round::nearest(1,$$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
+		}
 $openprint::log->debug("Got page layout $$self{page_columns} x $$self{page_rows}");
 
 		#if ( 1 ) {
