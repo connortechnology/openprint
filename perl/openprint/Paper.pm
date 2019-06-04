16bb148ed6b97a8990b3d70a3263de19f0d452f5
Isaac Connor <iconnor@connortechnology.com>
2019-06-04 15:54:50 -0400

    fix logic of basis dimensions test

--- perl/openprint/Paper.pm
+++ perl/openprint/Paper.pm
@@ -1850,9 +1850,19 @@ sub check {
   $Copy = $Paper->clone();
   if ( abs( POSIX::ceil( $Paper->basis_mweight()) - POSIX::ceil( $Copy->basis_mweight(undef)) ) -1 > 0 ) {
     return "may have invalid basis weight current:$$Paper{basis_mweight} != calculated:$$Copy{basis_mweight}";
-  }
-  if ( $Paper->brand() =~ /cover/i or $Paper->weight() =~ /cover/i and ( $Paper->basis_width() != 20 or $Paper->basis_height() != 26 ) ) {
-		return "may have wrong basis size. Should probably be 20x26";
+	}
+	if ( 
+			($Paper->brand() =~ /cover/i
+			or 
+			$Paper->weight() =~ /cover/i)
+			and (
+				($Paper->basis_width() != 20) 
+				or 
+				($Paper->basis_height() != 26)
+				) 
+		 ) {
+$openprint::log->debug("basis: " . $Paper->basis_width() . 'x' . $Paper->basis_height());
+		return 'may have wrong basis size. Should probably be 20x26';
 	}
   if ( ( $Paper->finish() =~ /1 side/i ) and ( $Paper->doublesided() ) ) {
     return "appears to be C1S, but is marked double sided.";
