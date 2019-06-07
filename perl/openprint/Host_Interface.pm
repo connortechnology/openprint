c48b18c7038a2151b51b2af7c080048129912540
root <root@mail.connortechnology.com>
2019-06-07 13:43:34 -0400

    Fix reboot so that it works with DCS-2310L

--- perl/openprint/Host_Interface.pm
+++ perl/openprint/Host_Interface.pm
@@ -106,11 +106,11 @@ $openprint::log->debug("Having authenticate $$headers{'www-authenticate'}");
 					($password ? $password : ''),
 					);
 			$response = $browser->get($url);
-				$openprint::log->debug("Auth response for $method $url $tokens{realm}, $username, $password " . $response->is_success );
+      $openprint::log->debug("Auth response for $method $url $tokens{realm}, $username, $password ".$response->is_success);
 
 			if ( $response->is_success and ( ($method ne 'get') or $args ) ) {
-$openprint::log->debug("Sending actual url $method ");
-				$response = $browser->$method($url, $args );
+$openprint::log->debug("Sending actual url $method");
+				$response = $browser->$method($url, $args);
 			}
 		} else {
 			$openprint::log->error("No realm");
@@ -119,7 +119,19 @@ $openprint::log->debug("Sending actual url $method ");
 		foreach my $k ( keys %{$headers} ) {
 			$openprint::log->debug("No auth Header $k => $$headers{$k}");
 		}
+    my $Host = $HI->Host();
+      my $username = $Host->info('username');
+      my $password = $Host->info('password');
+      $openprint::log->debug("username: $username password: $password args: " . ($args ? join(',',map { "$_=>$$args{$_}" } keys %{$args}) :'none'));
+      $browser->credentials(
+          $HI->ip().':'.$port,
+          '',
+          ($username ? $username : ''),
+          ($password ? $password : ''),
+          );
+
 		$response = $browser->$method( $url, $args ? $args : () );
+      $openprint::log->debug("Auth response for $method $url $username, $password ".$response->is_success);
 	}
 	return $response;
 } # end sub authenticate
