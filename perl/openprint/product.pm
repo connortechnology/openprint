aa47821260d0c14c53e7fa33a17ce54b8df25c4e
Isaac Connor <iconnor@connortechnology.com>
2019-06-17 10:20:27 -0400

    Update products

--- perl/openprint/product.pm
+++ perl/openprint/product.pm
@@ -266,13 +266,15 @@ sub category_view {
 	} # end if
 }
 sub category_edit {
-	my $Category = $variable{Category} = new openprint::Product_Category( $param{category_id} );
+	my $Category = $variable{Category} = new openprint::Product_Category($param{category_id});
+	return if ! $param{btnFunction};
+
 	if ( $param{btnFunction} eq 'Save' ) {
-		my @changes = $Category->changes( \%param );
+		my @changes = $Category->changes(\%param);
 		if ( @changes ) {
-			$variable{error} = $Category->save( \%param );
+			$variable{error} = $Category->save(\%param);
 		}
-		if ( ! $variable{error} ) {
+		if ( !$variable{error} ) {
 			my @spec_changes = openprint::Object_Specification::save_changes( $Category, \%param );
 			push @changes, 'specification changes: ' . join(', ', @spec_changes ) if @spec_changes;
 			(new openprint::Log())->save({Object=>$Category, action=>'Edit', note=>join('<br/>', @changes) });
