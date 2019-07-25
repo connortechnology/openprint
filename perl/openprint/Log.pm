f5966067076c25c53895075e0c579ece2cf4c31d
Isaac Connor <iconnor@connortechnology.com>
2019-07-25 10:36:33 -0400

    Don't lookup host for null ip when logging

--- perl/openprint/Log.pm
+++ perl/openprint/Log.pm
@@ -72,6 +72,8 @@ sub ip_address {
 		if ( !defined $_[1] ) {
 			$_[1] = $ENV{REMOTE_ADDR};
 		} # end if
+		return if ! $_[1];
+
 $openprint::log->debug("Getting HI for $_[1] for " . $_[0]->to_string());
 		my $Interface = openprint::Host_Interface->find_one(ip=>$_[1]);
 		if ( !$Interface ) {
