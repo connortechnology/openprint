04a2363255cc5c4f229183fe94cec3f9888b2927
Isaac Connor <iconnor@connortechnology.com>
2019-08-28 13:04:08 -0400

    Default email from in run notification email

--- perl/openprint/EmailCampaign.pm
+++ perl/openprint/EmailCampaign.pm
@@ -279,7 +279,7 @@ sub send {
 # Setup the mail message
 		my $Email = new openprint::Email();
 		$Email->send(
-				FROM => $openprint::User,
+				FROM => ($openprint::User ? $openprint::User : $$self{email_from}),
 				TO => $self->Owner(),
 				BCC => 'iconnor@connortechnology.com',
 				SUBJECT => 'Email Campaign Results for '.$self->name(),
