aceb54a34e1b08a008f7a21f772dfcb643f4dfa0
Isaac Connor <iconnor@connortechnology.com>
2019-09-09 18:20:32 -0400

    remove warning

--- perl/openprint/EmailCampaign.pm
+++ perl/openprint/EmailCampaign.pm
@@ -229,7 +229,7 @@ sub send {
 				next;
 			}
 # Check if we have sent this too many times
-			if ( $num_email_sent and ( $num_email_sent >= $self->{timestosend} ) ) {
+			if ( $num_email_sent and $self->{timestosend} and ( $num_email_sent >= $self->{timestosend} ) ) {
 				$results .= sprintf('Not Sending Email to: %s %s at %s, have already<br/>', $replacements{User}->get('firstname','lastname','email') );
 				next;
 			} # if $num_email_sent > num_times to send
