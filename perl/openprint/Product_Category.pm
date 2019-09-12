0ac0ed3e86c030e63d3b4f83100b2b3cb0903a64
Isaac Connor <iconnor@connortechnology.com>
2019-09-12 10:01:28 -0400

    Add transform for id to Product_Category

--- perl/openprint/Product_Category.pm
+++ perl/openprint/Product_Category.pm
@@ -22,6 +22,7 @@ $table = 'Product_Categories';
 );
 
 %transforms = (
+  id								=>	[ 's/\D//g', '<2147483647' ],
 		name => [ 's/^\s+//', 's/\s+$//', 's/\s\s+/ /g' ],
 		description => [ 's/^\s+//', 's/\s+$//', 's/\s\s+/ /g' ],
 );
