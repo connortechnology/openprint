b3aa2bc53117945e6712b4b6649538e3a0717304
Isaac Connor <iconnor@connortechnology.com>
2019-10-08 13:43:38 -0400

    Don't calculate mweight and gsm if we don't have basis_dimensions yet

--- www/public_html/javascripts/paper.js
+++ www/public_html/javascripts/paper.js
@@ -122,11 +122,13 @@ function calc_from_basis_weight( form, id='' ) {
 	var basis_weight = form.elements['basis_weight'+id] ? parseFloat(1*form.elements['basis_weight'+id].value) : parseFloat(1*form.elements['basis_mweight'+id].value);
 	var basis_width = parseFloat(1*form.elements['basis_width'+id].value);
 	var basis_height = parseFloat(1*form.elements['basis_height'+id].value);
-	var gsm = parseInt((basis_weight/1000)/(basis_width*basis_height)*7030645.0)/10;
-	form.elements['gsm'+id].value = gsm;
-	var width = parseFloat(1*form.elements['width'+id].value);
-	var height = parseFloat(1*form.elements['height'+id].value);
-	form.elements['mweight'+id].value = (((gsm/703064.5)*(width*height)*10000)/10).round();
+	if ( basis_width && basis_height ) {
+		var gsm = parseInt((basis_weight/1000)/(basis_width*basis_height)*7030645.0)/10;
+		form.elements['gsm'+id].value = gsm;
+		var width = parseFloat(1*form.elements['width'+id].value);
+		var height = parseFloat(1*form.elements['height'+id].value);
+		form.elements['mweight'+id].value = (((gsm/703064.5)*(width*height)*10000)/10).round();
+	}
 }
 function calc_from_mweight( form, id='' ) {
 	var mweight = parseFloat(1*form.elements['mweight'+id].value);
@@ -146,7 +148,8 @@ function calc_from_gsm( form, id='' ) {
 	var basis_width = parseFloat(1*form.elements['basis_width'+id].value);
 	var basis_height = parseFloat(1*form.elements['basis_height'+id].value);
 	form.elements['mweight'+id].value = parseInt((gsm/703064.5)*(width*height)*10000)/10;
-	form.elements['basis_weight'+id].value = parseInt((gsm/703064.5)*(basis_width*basis_height)*10000)/10;
+	if ( basis_width && basis_height )
+	  form.elements['basis_weight'+id].value = parseInt((gsm/703064.5)*(basis_width*basis_height)*10000)/10;
 }
 function calc_from_weight(form, id='') {
 	var weight = parseFloat(1*form.elements['weight'+id].value);
