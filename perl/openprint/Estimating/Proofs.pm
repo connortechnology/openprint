13fad09716a8e7e16fb3f26be2676fff70c9eadf
root <root@www7.internal.point-one.com>
2019-11-21 13:01:30 -0500

    turn off Proofs warnings

--- perl/openprint/Estimating/Proofs.pm
+++ perl/openprint/Estimating/Proofs.pm
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 use strict;
-use warnings;
+#use warnings;
 
 package openprint::Estimating::Proofs;
 use POSIX qw( ceil );
