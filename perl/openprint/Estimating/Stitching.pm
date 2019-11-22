f5432cce63e838cb6dc0d15a1e5da951cd8ee6da
root <root@www7.internal.point-one.com>
2019-11-22 11:16:34 -0500

    variables() now takes old_specs as well

--- perl/openprint/Estimating/Stitching.pm
+++ perl/openprint/Estimating/Stitching.pm
@@ -68,7 +68,7 @@ my %variables = (
 		'txtSignatureQty64Page-1'=>['save','output'], 'txtSignatureQty64Page-2'=>['save','output'], 'txtSignatureQty64Page-3'=>['save','output'],
 		);
 sub variables {
-	my ( $p_id, $s_id, $specs ) = @_;
+	my ( $p_id, $s_id, $old_specs, $specs ) = @_;
 	my @v;
 	foreach my $k ( keys %variables ) {
 		push @v, $k, if sets::isin( 'save', $variables{$k} );
