59843813bc2b8fc4c0b7cfd030d55170a258a93d
Isaac Connor <iconnor@connortechnology.com>
2020-01-16 14:05:15 -0500

    set default for checkboxes when saving host_interfaces.  Only log changes if there are any

--- perl/openprint/employee_it.pm
+++ perl/openprint/employee_it.pm
@@ -269,7 +269,7 @@ sub host {
       $variable{error} .= $Host->save(\%param) if @changes;
       foreach my $I ( $Host->Interfaces(), new openprint::Host_Interface() ) {
         if ( $param{"mac-$$I{id}"} or $param{"ip-$$I{id}"} or $param{"comment-$$I{id}"} ) {
-          my %c =map { $_, $param{"$_-$$I{id}"} } ( 'mac', 'ip', 'dhcp', 'monitor', 'comment' );
+          my %c = map { exists($param{"$_-$$I{id}"}) ?( $_=>$param{"$_-$$I{id}"} ) : ( $_=>$openprint::Host_Interface::defaults{$_} ) } ( 'mac', 'ip', 'dhcp', 'monitor', 'comment' );
           my @c = $I->changes( \%c );
           if ( @c ) {
             $c{host_id} = $$Host{id};
@@ -297,7 +297,7 @@ sub host {
       $Host->Notifications(undef);
       
       if ( ! $variable{error} ) {
-        (new openprint::Log())->save({Object=>$Host, action=>'Edit', note=>join('<br/>', @changes) });
+        (new openprint::Log())->save({Object=>$Host, action=>'Edit', note=>join('<br/>', @changes) }) if @changes;
         $variable{ExternalRedirect} = '/employee/it/hosts.html';
         return;
       } # end if
