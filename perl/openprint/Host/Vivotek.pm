57c8815059c7a15ff7d50ed77dbfd171e7ab1627
Isaac Connor <iconnor@connortechnology.com>
2020-07-03 18:26:52 -0400

    Improve debug logging in VivoTek

--- perl/openprint/Host/Vivotek.pm
+++ perl/openprint/Host/Vivotek.pm
@@ -30,7 +30,7 @@ sub get_config {
 	foreach my $HI ( $Host->Interfaces() ) {
 		my $url = 'http://'.$HI->ip().'/cgi-bin/admin/lsctrl.cgi?cmd=queryStatus&retType=javascript';
 		my $req = new HTTP::Request(GET => $url);
-		$req->authorization_basic('root','p1GraPHic');
+		$req->authorization_basic('root', 'p1GraPHic');
 		my $response = $ua->request($req);
 		if ( $response->is_success() ) {
 			my $resp = $response->decoded_content;
@@ -58,10 +58,10 @@ sub check {
 		if ( $$status{disk_i0_cond} !~ /ready/ ) {
 				push @check, 'SD card not ready';
 		} else {
-			$openprint::log->debug("status was $$Config{data_json}");
+			$openprint::log->debug('status was '.(defined($$Config{data_json}) ? $$Config{data_json} : 'undef'));
 		}
 	} else {
-		$openprint::log->warn("No config found for status");
+		$openprint::log->warn('No config found for status');
 	}
 	return @check;
 }
