c2d2da2e16dfc3722c6435bf4bee7969e1b349ab
Isaac Connor <iconnor@connortechnology.com>
2020-09-23 08:26:00 -0400

    alter paid_days to return a value when not paid

--- perl/openprint/Invoice.pm
+++ perl/openprint/Invoice.pm
@@ -503,18 +503,14 @@ sub first_sent_on {
 } # end sub first_sent_on
 
 sub paid_days {
-  if ( ! $_[0]{paid_on} ) {
-    $openprint::log->debug("No paid_on");
-    return;
-  }
   my $sent = $_[0]->first_sent_on();
   if ( ! $sent ) {
-    $openprint::log->debug("No sent");
+    $openprint::log->debug('No sent');
     return;
   }
 
-  my $paid_time = Date::Parse::str2time( $_[0]{paid_on} );
-  my $sent_time = Date::Parse::str2time( $sent );
+  my $paid_time = $_[0]{paid_on} ? Date::Parse::str2time($_[0]{paid_on}) : time;
+  my $sent_time = Date::Parse::str2time($sent);
   my $days = int( ($paid_time-$sent_time) / 86400 );
   return $days;
 }
