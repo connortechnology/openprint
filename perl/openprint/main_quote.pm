47ae02b044cfaac0dcdb9df847fd741ab0c13791
Isaac Connor <iconnor@connortechnology.com>
2020-11-10 11:30:25 -0500

    fix errors when there are no quotes in history

--- perl/openprint/main_quote.pm
+++ perl/openprint/main_quote.pm
@@ -141,13 +141,15 @@ sub _history {
 					 );
 
 		my @quote_ids = map { $$_{id} } @Quotes;
+    return if ! @quote_ids;
+
 		my @Quoted_Projects = openprint::QuotedProject->find(quote_id=>\@quote_ids,order=>'project_id') if @quote_ids;
 		my @project_ids = map { $$_{project_id} } @Quoted_Projects;
 		my %Quoted_Projects = misc::make_hash_from_array('quote_id', @Quoted_Projects);
 		my @Projects = openprint::Project->find(id=>\@project_ids) if @project_ids;
 		my @company_ids = map { $$_{company_id} } @Quotes;
 		my @Companies = openprint::Company->find(id=>\@company_ids) if @company_ids > 1;
-		my @Ordered_Projects = openprint::OrderedProject->find(project_id=>\@project_ids);
+		my @Ordered_Projects = openprint::OrderedProject->find(project_id=>\@project_ids) if @project_ids;
 		my %Ordered_Projects_by_project_id = misc::make_hash_from_array('project_id', @Ordered_Projects);
 		foreach my $Project ( @Projects ) {
 			$Project->Ordered_Project($Ordered_Projects_by_project_id{$$Project{id}}[0]) if $Ordered_Projects_by_project_id{$$Project{id}};
