abe93581e87df3fef7a5b15cd8c40f8bc2a0d430
root <root@mail2.connortechnology.com>
2021-12-28 15:24:41 -0500

    Add description to session in searching expenses

--- perl/openprint/employee_accounting.pm
+++ perl/openprint/employee_accounting.pm
@@ -465,7 +465,7 @@ sub _expenses {
 				'paid_on_start_year','paid_on_start_month','paid_on_start_day',
 				'paid_on_end_year','paid_on_end_month','paid_on_end_day',
 				'category_id', 'recipient_id', 'account_id','attention', 'currency_id',
-				'amount','total','business_use', 'owner_id','deleted',
+				'amount','total','business_use', 'owner_id','deleted','description',
 				) );
 } # end sub _expenses
 
