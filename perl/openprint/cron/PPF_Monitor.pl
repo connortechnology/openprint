3746f73cd314dfad80fe979bdde82a71fc2a3644
root <root@www4.internal.point-one.com>
2022-02-19 13:46:01 -0500

    quotes. split some code into functions

--- perl/openprint/cron/PPF_Monitor.pl
+++ perl/openprint/cron/PPF_Monitor.pl
@@ -57,17 +57,18 @@ foreach my $required ( qw( db_host db_name ) ) {
 $config{db_user} = $config{db_name} if ! $config{db_user};
 $config{db_pass} = $config{db_name} if ! $config{db_pass};
 
-$dbh = sql::open_sql( $log,
+my %sql_config = (
 		host      => $config{db_host},
 		database  => $config{db_name},
 		driver    => 'Pg',
 		login     => $config{db_user},
 		password  => $config{db_pass},
 		);
-die 'Error opening db' if ! $dbh;
+
+$dbh = sql::open_sql($log, %sql_config) or die 'Error opening db';
 configuration::from_db( );
-configuration::from_file( $$opts{config} );
-configuration::merge( $opts );
+configuration::from_file($$opts{config});
+configuration::merge($opts);
 $config{log_level} = 'debug' if ! $config{log_level};
 $log = logger->new( {file=>$config{log_file}, level=>$config{log_level}} );
 
@@ -83,8 +84,7 @@ if ( ! @Equipment ) {
 $dbh->disconnect();
 
 foreach my $Equipment ( @Equipment ) {
-	$log->debug("Processing " . $Equipment->name() );
-	my @filenames;
+	$log->debug('Processing '.$Equipment->name());
 	if ( ! open(S, "> $$Equipment{cip3_in}/.lock.lck") ) {
 		$log->error("Unable to open semaphoreat $$Equipment{cip3_in}/.lock.lck");
 		next;
@@ -93,15 +93,14 @@ foreach my $Equipment ( @Equipment ) {
 		$log->error('Unable to lock semaphore');
 		next;
 	} # end if
-	if ( opendir DIRHANDLE, $Equipment->cip3_in() ) {
-		@filenames = readdir DIRHANDLE;
-		closedir DIRHANDLE;
-	} else {
+	if ( !opendir DIRHANDLE, $Equipment->cip3_in() ) {
 		print "Cannot open input hotfolder for $$Equipment{name} at $$Equipment{cip3_in}";
 		next;
 	} # end if
+	my @filenames = readdir DIRHANDLE;
+	closedir DIRHANDLE;
 
-	if ( $$Equipment{'cip3_merge'} ) {
+	if ( $$Equipment{cip3_merge} ) {
 		#$log->warn("Merging..." ) if $debug;
 		# First have to look for Back's, so that we don't process fronts before backs.
 		# Have to copy, because we modify filenames
@@ -109,13 +108,13 @@ foreach my $Equipment ( @Equipment ) {
 
 		foreach my $file ( @Bs ) {
 			# Will ignore ., .., any hidden file
-		$log->warn("File... $file" ) if $debug;
+			$log->debug("File... $file") if $debug;
 			next if $file =~ /^\./; 
 			next if -d $Equipment->cip3_in().'/'.$file;
 			my ( $file_base, $side, $extension ) = $file =~ /^(.*)([AB])\.(ppf)$/i;
-$log->warn("Parsed to $file_base, $side, $extension from $file") if $debug;
+			$log->debug("Parsed to $file_base, $side, $extension from $file") if $debug;
 			if ( $side ne 'B' ) {
-$log->warn("Not a B") if $debug;
+				$log->debug('Not a B') if $debug;
 				next;
 			} # end if
 
@@ -123,24 +122,22 @@ $log->warn("Not a B") if $debug;
 			$out_base =~ s/\./_/g;
 
 			my ( $docket, $ppo, $name, $sig ) = $file_base =~ /^(\d+)(\w\w)?_?(.+?)Sg(\d+)/i;
-	print "File: $file Docket $docket, Operattor: $ppo, Name: $name, Sig: $sig, $side\n" if $debug;
+			$log->debug("File: $file Docket $docket, Operattor: $ppo, Name: $name, Sig: $sig, $side") if $debug;
 			$sig = 0 if ! $sig;
 			my $data;
 			$side = 'M';
-#$log->warn("FIlenames before: @filenames");
-			if ( ! sets::isin( $file_base.'A.'.$extension, \@filenames ) ) {
-				$log->warn( "A file not found " . $$Equipment{'cip3_in'}.'/'.$file_base."A.$extension ignoring B\n" );
+			if ( ! sets::isin($file_base.'A.'.$extension, \@filenames) ) {
+				$log->warn('A file not found '.$$Equipment{cip3_in}.'/'.$file_base.'A.'.$extension.' ignoring B');
 				next;
 			} # end if
 			@filenames = sets::exclude( [$file_base.'A.'.$extension,$file_base.'B.'.$extension], \@filenames );	
-#$log->warn("FIlenames after: @filenames");
 
-			if ( ! open ( FH, '< ' . $$Equipment{'cip3_in'}.'/'.$file_base.'B.'.$extension ) ) {
-				$log->error( "Error opening " . $$Equipment{'cip3_in'}.'/'.$file_base."B.$extension\n" );
+			if ( ! open(FH, '< '.$$Equipment{'cip3_in'}.'/'.$file_base.'B.'.$extension) ) {
+				$log->error('Error opening '.$$Equipment{'cip3_in'}.'/'.$file_base.'B.'.$extension);
 				next;
 			} # end if
 			if ( ! flock(FH, LOCK_EX) ) {
-				$log->error("Unable to lock B!");
+				$log->error('Unable to lock B!');
 				close(FH);
 				next;
 			} # end if
@@ -150,76 +147,44 @@ $log->warn("Not a B") if $debug;
 			while ( <FH> ) {
 				my $line = $_;
 				$back_flag = 1 if ( $line =~ /CIP3BeginBack/ );
-if ( $mangle ) {
-				if ( $line =~ /^\/CIP3AdmJobName\s+\((.*)\)\s+def/ ) {
-					my $job_name = $1;
-					if ( length $job_name > 16 ) {
-						if ( my ( $pre, $j_name, $sig ) = ( $job_name =~ /(\d+\w\w)(.+)SIG(\d\d\d)/ ) ) {
-							$line = '/CIP3AdmJobName ('.$pre.(substr($j_name,0,4)).'Sg'.$sig."SdB) def\r\n";
-						} else {
-							$line = '/CIP3AdmJobName ('.(substr($job_name,0,16)).") def\r\n";
-						} # end if
-					} # end if
-				} elsif ( $line =~ /^\/CIP3AdmJobCode\s+\((.*)\)\s+def/ ) {
-					if ( ! $1 ) {
-						$line = "/CIP3AdmJobCode ($docket) def\r\n";
-					} # end if
-				} # end if
-} # end if
-				push @Back, $line if ( $back_flag );
+				push @Back, $line if $back_flag;
 				last if $line =~ /CIPEndBack/;
 			} # end while FH
-			close( FH );
-			if ( ! @Back ) {
-				$log->error( "No Back found in B file!" );
-				rename $$Equipment{'cip3_in'}.'/'.$file_base.'B.'.$extension, $$Equipment{'cip3_in'}.'/'.$file_base.'E.'.$extension;
+			close(FH);
+			if ( !@Back ) {
+				$log->error('No Back found in B file!');
+				rename $$Equipment{cip3_in}.'/'.$file_base.'B.'.$extension, $$Equipment{cip3_in}.'/'.$file_base.'E.'.$extension;
 				next;
 			} # end if
+			@Back = $PPF->convert_job_name(@Back) if $mangle;
 
 			my $A;
 			if ( ! open( $A, '< '.$$Equipment{'cip3_in'}.'/'.$file_base.'A.'.$extension ) ) {
-				$log->error( "Error opening " . $$Equipment{'cip3_in'}.'/'.$file_base."A.$extension" );
+				$log->error('Error opening '.$$Equipment{cip3_in}.'/'.$file_base.'A.'.$extension);
 				next;
 			} # end if
 			if ( ! flock($A, LOCK_EX) ) {
-				$log->error("Unable to lock A!\n");
+				$log->error('Unable to lock A!');
 				close($A);
 				next;
 			} # end if
 			my $fileA = $file_base.'A';
 			my $fileM = $file_base.'M';
 			my $complete = 0;
+
+			my @data;
 			while ( <$A> ) {
 				my $line = $_;
 				next if $line =~ /^CIP3EndSheet/;
 				if ( $line =~ /%%CIP3EndOfFile/ ) {
 					$complete = 1;
+					push @data, @Back;
+				} else {
+					$line =~ s/$fileA/$fileM/g;
+					$PPF->convert_sheet_name( $line );
+					$PPF->convert_job_name( $line ) if $mangle;
 				} # end if
-				$line =~ s/$fileA/$fileM/g;
-				if ( $line =~ /^\/CIP3AdmSheetName \(Sheet (\d*)\) def/ ) {
-					$line = sprintf("/CIP3AdmSheetName (Sig#%dSheet#%d) def\r\n", 1*$sig, $1 );
-				} 
-if ( $mangle ) {
-				if ( $line =~ /^\/CIP3AdmJobCode\s+\((.*)\)\s+def(.*)/ ) {
-					if ( ! $1 ) {
-						$line = "/CIP3AdmJobCode ($docket) def$2";
-					} # end if
-				} elsif ( $line =~ /^\/CIP3AdmJobName\s+\((.+)\)\s+def/ ) {
-					my $job_name = $1;
-					if ( length $job_name > 16 ) {
-						if ( my ( $pre, $name, $sig ) = ( $job_name =~ /(\d+\w\w)(.+)SIG(\d\d\d)/ ) ) {
-							$line = '/CIP3AdmJobName ('.$pre.(substr($name,0,4)).'Sg'.$sig."SdA) def\r\n";
-						} else {
-							$line = '/CIP3AdmJobName ('.(substr($job_name,0,16)).") def\r\n";
-						} # end if
-					} # end if
-				} # end if
-} # end if
-
-				if ( $line =~ /CIP3EndOfFile/ ) {
-					$data .= join('', @Back );
-				} # end if
-				$data .= $line;
+				push @data, $line;
 			} # end while
 			close $A;
 
@@ -232,20 +197,36 @@ if ( $mangle ) {
 				next;
 			} # end if
 
-$dbh = sql::open_sql( $log,
-		host      => $config{db_host},
-		database  => $config{db_name},
-		driver    => 'Pg',
-		login     => $config{db_user},
-		password  => $config{db_pass},
-		);
-die 'Error opening db' if ! $dbh;
-			my $PPF = store_PPF( $docket, $name, $sig, $side, $Equipment, $data );
-			$PPF->send_ppf( $Equipment ) if ! $$Equipment{'cip3_hold'};
-$dbh->disconnect();
+			my $PPF = new openprint::CIP3_PPF();
+			$PPF->set({
+					docket    	=>  $docket,
+					signature 	=>  $sig,
+					side      	=>  $side,
+					version		=>	$version,
+					});
+			
+			@data = $PPF->convert_sheet_name( @data );
+			@data = $PPF->convert_job_name( @data );
 
-			unlink $$Equipment{'cip3_in'}.'/'.$file_base.'A.'.$extension;
-			unlink $$Equipment{'cip3_in'}.'/'.$file_base.'B.'.$extension;
+			$dbh = sql::open_sql($log, %sql_config) or die 'Error opening db';
+
+			my $data = join('', @data);
+			if ( $use_compression ) {
+				my $compressed_data = Compress::Zlib::compress($data);
+				$log->warn('Compressed PPF from ' . (length $data) . ' to ' . (length $compressed_data) ) if $debug;
+				$PPF->data($data);
+				$PPF->compressed(0);
+			} else {
+				$PPF->data($data);
+				$PPF->compressed(1);
+			} # end if
+
+			my $PPF = store_PPF( $docket, $name, $sig, $side, $Equipment, $data );
+			$PPF->send_ppf($Equipment) if ! $$Equipment{cip3_hold};
+			$dbh->disconnect();
+
+			unlink $$Equipment{cip3_in}.'/'.$file_base.'A.'.$extension;
+			unlink $$Equipment{cip3_in}.'/'.$file_base.'B.'.$extension;
 		} # end foreach file in input hotfolder
 	} # end if cip3_merge
 
@@ -262,15 +243,15 @@ $dbh->disconnect();
 		} # end if
 
 		my ( $file_base, $side, $extension ) = $file =~ /^(.*)([AB])\.(ppf)$/i;
-$log->warn("SINGLE SIDE Parsed to $file_base, $side, $extension from $file") if $debug;
+$log->debug("SINGLE SIDE Parsed to $file_base, $side, $extension from $file") if $debug;
 		my $out_base = $file_base;
 		$out_base =~ s/\./_/g;
 		my $data;
 
 		my ( $docket, $ppo, $name, $sig ) = $file_base =~ /^(\d+)(\w\w)?_?(.+?)S?g?(\d+)/i;
 
-		if ( ! open ( IN, '< ' . $$Equipment{'cip3_in'}.'/'.$file ) ) {
-			print "Error opening for read:" . $$Equipment{'cip3_in'}.'/'.$file."\n" ;
+		if ( ! open(IN, '< '.$$Equipment{cip3_in}.'/'.$file) ) {
+			$log->error('Error opening for read:' . $$Equipment{cip3_in}.'/'.$file);
 			next;
 		} # end if
 		if ( ! flock(IN, LOCK_EX) ) {
@@ -279,6 +260,7 @@ $log->warn("SINGLE SIDE Parsed to $file_base, $side, $extension from $file") if 
 			next;
 		} # end if
 		my $complete = 0;
+		my @data;
 		while ( <IN> ) {
 			my $line = $_;
 			if ( $line =~ /%%CIP3EndOfFile/ ) {
@@ -304,21 +286,21 @@ if ( $mangle ) {
 				} # end if
 			} # end if
 } # end if
-			$data .= $line;
+			push @data, $line;
 		} # end while
 		close IN;
 		if ( ! $complete ) {
 			$log->error("File was not complete! ".$Equipment->cip3_in()."/$file");
 			next;
 		} # end if
-$dbh = sql::open_sql( $log,
-		host      => $config{db_host},
-		database  => $config{db_name},
-		driver    => 'Pg',
-		login     => $config{db_user},
-		password  => $config{db_pass},
-		);
-die 'Error opening db' if ! $dbh;
+
+			@data = $PPF->convert_sheet_name( @data );
+			@data = $PPF->convert_job_name( @data );
+
+			$dbh = sql::open_sql($log, %sql_config) or die 'Error opening db';
+
+			my $data = join('', @data);
+
 		my $PPF = store_PPF( $docket, $name, $sig, $side, $Equipment, $data );
 		$PPF->send_ppf( $Equipment ) if ! $$Equipment{'cip3_hold'};
 		unlink $$Equipment{'cip3_in'}.'/'.$file;
