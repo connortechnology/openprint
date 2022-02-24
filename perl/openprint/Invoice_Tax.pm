5b0e900b8aa97c88a23d80bd652d47c2c653da9f
root <root@mail2.connortechnology.com>
2022-02-24 12:51:39 -0500

    Add invoic_tax charge setting

--- perl/openprint/Invoice_Tax.pm
+++ perl/openprint/Invoice_Tax.pm
@@ -12,16 +12,19 @@ $table = 'invoice_taxes';
 $serial = 'invoice_taxes_id_seq';
 
 %fields = (
-	id			=>	'id',
+	id		    	=>	'id',
 	invoice_id	=>	'invoice_id',
-	tax_id		=>	'tax_id',
-	rate		=>	'rate',
-	amount		=>	'amount',
+	tax_id	  	=>	'tax_id',
+	rate		    =>	'rate',
+	amount     	=>	'amount',
+  charge      =>  'charge',
 );
 
 %transforms = (
 );
 %defaults = (
+  rate    =>  undef,
+  amount  =>  undef,
 );
 
 sub Tax {
@@ -48,5 +51,27 @@ sub amount {
 	return Math::Round::nearest( 1/(10**$self->Invoice()->Currency()->precision()), $$self{amount} );
 } # end sub amount
 
+sub charge {
+  my $self = $_[0];
+  if ( @_ == 2 ) {
+    $$self{charge} = $_[1];
+  } # end if
+
+  if ( ( ! defined $$self{charge} ) and $self->Invoice()->invoicer_id() ) {
+#$openprint::log->debug("Calculating Tax: " . $self->name() . 'exempt: ' . $self->PurchaseOrder()->Supplier()->taxexempt1() );
+    if ( sets::isin( $self->name(), ['GST','HST'] ) ) {
+      if ( $self->Invoice()->Invoicer()->taxexempt1() eq 'Y' ) {
+        return 0;
+      } # end if
+      return 1;
+    } elsif ( sets::isin( $self->name(), ['PST'] ) ) {
+      if ( $self->Invoice()->Invoicer()->taxexempt2() eq 'Y' ) {
+        return 0;
+      } # end if
+      return 1;
+    } # end if
+  } # end if
+  return $$self{charge};
+} # end sub charge
 1;
 __END__
