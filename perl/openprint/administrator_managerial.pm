6873067c3b4e030222486ddfb82c6aec49dde048
Isaac Connor <iconnor@connortechnology.com>
2022-03-23 22:10:45 +0000

    Add undelete to companies

--- perl/openprint/administrator_managerial.pm
+++ perl/openprint/administrator_managerial.pm
@@ -1126,20 +1126,28 @@ sub _companies {
 				) );
 	$session{$r->uri().'?salesrep_id_exclude'} = $param{salesrep_id_exclude};
   if ($param{action} eq 'delete') {
-    foreach my $Company ( openprint::Company->find( id=> (ref $param{'company_id[]'} eq 'ARRAY') ? $param{'company_id[]'} : [$param{'company_id[]'}]) ) {
+    foreach my $Company ( openprint::Company->find( id=> (ref $param{'company_id[]'} eq 'ARRAY') ? $param{'company_id[]'} : [$param{company_id}]) ) {
       $Company->delete();
     }
+  } elsif ($param{action} eq 'undelete' ) {
+    my @company_ids = (ref $param{'company_id[]'} eq 'ARRAY') ? @{$param{'company_id[]'}} : ($param{company_id});
+    while (@company_ids) {
+      foreach my $Company ( openprint::Company->find( id=> [ splice(@company_ids, 0, 100) ], deleted=>1) ) {
+        if (!$Company->deleted()) {
+          $variable{error} .= $Company->name() . ' not undeleted because not deleted.<br/>';
+          next;
+        }
+        $Company->undelete();
+      } # end foreach Company
+    } # end while company_ids
   } elsif ($param{action} eq 'destroy' ) {
-    $log->debug("Doing destory");
     my @company_ids = (ref $param{'company_id[]'} eq 'ARRAY') ? @{$param{'company_id[]'}} : ($param{'company_id[]'});
     while (@company_ids) {
       foreach my $Company ( openprint::Company->find( id=> [ splice(@company_ids, 0, 100) ], deleted=>1) ) {
         if (!$Company->deleted()) {
-          $log->debug("Not Destorying $$Company{name}");
           $variable{error} .= $Company->name() . ' not destroyed because not deleted.<br/>';
           next;
         }
-        $log->debug("Destorying $$Company{name}");
         $Company->destroy();
       } # end foreach Company
     } # end while company_ids
