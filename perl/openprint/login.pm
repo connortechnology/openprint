0f8c2b143b8ff0dcb903ebd447efd77e9de51d96
Isaac Connor <isaac@zoneminder.com>
2022-04-05 16:40:22 -0400

    Move the code that actually stores login state to login::login

--- perl/openprint/login.pm
+++ perl/openprint/login.pm
@@ -191,10 +191,7 @@ sub verify_login {
 
 	} # end if
 
-	@session{'company_id','user_id','email','user_type'} = $User->get('company_id','id','email','type');
-	openprint::usergroup::init_cache();
-	delete $session{Pricelist_id};
-	(new openprint::Log())->save({Object=>$User, action=>'Login', note=>'Successful Login' } );
+  login($User);
 
 	if ( $openprint::param{rdbRememberMe} eq 'Y' ) {
 		my $Cookie = Apache2::Cookie->new($r,
@@ -226,6 +223,14 @@ sub verify_login {
 	} # end if
 
 } # sub verify_login
+
+sub login {
+  my $User = shift;
+	@session{'company_id','user_id','email','user_type'} = $User->get('company_id','id','email','type');
+	openprint::usergroup::init_cache();
+	delete $session{Pricelist_id};
+	(new openprint::Log())->save({Object=>$User, action=>'Login', note=>'Successful Login' } );
+}
 
 sub logout {
 	(new openprint::Log())->save({Object=>$openprint::User, action=>'Logout'});
