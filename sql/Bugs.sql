12f5c29ab99d822018cac1beee6c4d6efa582133
Isaac Connor <iconnor@connortechnology.com>
2022-06-23 11:40:13 -0400

    DROP NOT NULL for various user id fields

--- sql/Bugs.sql
+++ sql/Bugs.sql
@@ -3,7 +3,7 @@ CREATE TABLE Bugs (
 	id	SERIAL,
 	owner_id	INTEGER NOT NULL, FOREIGN KEY (owner_id) REFERENCES Companies (id),
 	company_id	INTEGER NOT NULL, FOREIGN KEY (company_id) REFERENCES Companies (id),
-	user_id		INTEGER NOT NULL, FOREIGN KEY (user_id) REFERENCES Users (id),
+	user_id		INTEGER, FOREIGN KEY (user_id) REFERENCES Users (id),
 	project_Id	INTEGER, FOREIGN KEY (project_id) REFERENCES Projects (id),
 	description	TEXT,
 	created_on	TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
