ec71fc5ff07adb5998da8f8a87c47c1ab9a17793
root <root@mail2.connortechnology.com>
2022-06-24 18:30:50 -0400

    Handle no default pricelist set

--- perl/openprint/Product.pm
+++ perl/openprint/Product.pm
@@ -121,7 +121,7 @@ sub Category {
 sub get_price {
 	my ( $self, $qty, $options ) = @_;
 
-	$$options{pricelist_id} = $openprint::Pricelist->id() if ! $$options{pricelist_id};
+	$$options{pricelist_id} = $openprint::Pricelist->id() if ! $$options{pricelist_id} and $openprint::Pricelist;
 
 	my %price = openprint::pricing::get_best_price_object( $openprint::session{company_id}, $$self{id}, $$options{pricelist_id}, 'openprint::product_priceset', $qty, undef );
 	#if ( ! %price ) {
