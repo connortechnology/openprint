1235a5fd4ddcb05587927e512dd3bb4bb1dc57ae
Isaac Connor <isaac@tempesttechnology.com>
2023-03-14 08:09:01 -0400

    Rough in Hosts() and Hosts_Sites() in Site.

--- perl/openprint/Site.pm
+++ perl/openprint/Site.pm
@@ -18,12 +18,45 @@ $serial = 'sites_id_seq';
   deleted     => 'deleted',
 );
 %transforms = (
+	name							=>	[ 's/[\.\,]//g', 's/^\s+//', 's/\s+$//','s/\///g' ],
 );
 %defaults = (
 	created_on			=>	q`'NOW()'`,
 	updated_on			=>	q`'NOW()'`,
   deleted         =>  0,
 );
+
+sub Host_Sites {
+  my $self = shift;
+  if (@_) {
+    $$self{Host_Sites} = shift;
+  }
+  if (!$$self{Host_Sites}) {
+    if ($$self{id}) {
+      $$self{Host_Sites} = [ openprint::Host_Site->find(site_id=>$$self{id}) ];
+    } else {
+      $$self{Host_Sites} = [];
+    }
+  }
+  return @{$$self{Host_Sites}} if wantarray;
+  return $$self{Host_Sites};
+}
+
+sub Hosts {
+  my $self = shift;
+  if (@_) {
+    $$self{Hosts} = shift;
+  }
+  if (!$$self{Hosts}) {
+    if ($$self{id}) {
+      $$self{Hosts} = [ openprint::Host->find(id=>map { $$_{host_id} } $self->Host_Sites()) ];
+    } else {
+      $$self{Hosts} = [];
+    }
+  }
+  return @{$$self{Hosts}} if wantarray;
+  return $$self{Hosts};
+}
 
 1;
 __END__
