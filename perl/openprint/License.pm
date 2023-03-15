db4f78923b282b16c560b5dab2389a0f4e387fa7
Isaac Connor <isaac@tempesttechnology.com>
2023-03-15 07:20:14 -0400

    Fix features parsing and rough in key generation

--- perl/openprint/License.pm
+++ perl/openprint/License.pm
@@ -4,10 +4,14 @@ our @ISA = qw(openprint::Object);
 
 require openprint::License_Host;
 require openprint::Software;
-use JSON;
+require File::Slurp;
+require JSON;
+use Crypt::OpenSSL::Random;
+use Crypt::OpenSSL::RSA;
+use MIME::Base64;
 
 use vars qw( $debug $table $serial %fields %find_fields %transforms %defaults );
-$debug = 0;
+$debug = 1;
 $table = 'licenses';
 $serial='licenses_id_seq';
 %fields = (
@@ -24,6 +28,7 @@ $serial='licenses_id_seq';
 		created_on		=>	'created_on',
 		updated_on		=>	'updated_on',
     features_json      =>  'features_json', # json encoded
+    features => undef,
 		);
 %find_fields = (
 		host_id	=>	'id IN (SELECT license_id FROM license_hosts where host_id=?)',
@@ -79,11 +84,32 @@ sub features {
   my $self = shift;
   if (@_) {
     $$self{features} = shift;
-    $$self{features_json} = JSON::json_encode($$self{features});
+    $$self{features_json} = JSON::encode_json($$self{features});
   } elsif (!$$self{features}) {
-    $$self{features} = JSON::json_decode($$self{features_json});
+    $$self{features} = $$self{features_json} ? JSON::decode_json($$self{features_json}) : {};
   }
   return $$self{features};
+}
+
+sub generate_key {
+  my $self = shift;
+  #my $private_key = $config{'Licensing Private Key'};
+  #my $private_key = misc::load_file($openprint::log, '/var/www/crm/etc/ssl/cloudmule.key');
+  my $private_key = misc::load_file($openprint::log, '/tmp/key');
+  $openprint::log->debug("Private key $private_key");
+  if (!$private_key) {
+    $openprint::log->error("No private key, can't continue");
+    return;
+  }
+  #my $rsa = Crypt::OpenSSL::RSA->generate_key(2048);
+  #$openprint::log->debug($rsa->get_private_key_string());
+  #File::Slurp::write_file('/tmp/key', { atomic => 1, err_mode=>'carp' }, $rsa->get_private_key_string()) or warn "Couldn't save key file";
+  #File::Slurp::write_file('/tmp/key.pub', { atomic => 1, err_mode=>'carp' }, $rsa->get_public_key_string()) or warn "Couldn't save key file";
+
+  my $rsa = Crypt::OpenSSL::RSA->new_private_key($private_key);
+  $rsa->use_pkcs1_padding();
+  $$self{serialkey} = encode_base64($rsa->private_encrypt($self->id().','.$self->features_json()));
+  $openprint::log->debug($$self{serialkey});
 }
 
 1;
