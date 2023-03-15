9957cf4acbd6e7865ded9aab4a7d35dcd49cd3ee
Isaac Connor <isaac@tempesttechnology.com>
2023-03-15 10:05:56 -0400

    Fixup db definitions for Hosts_Sites

--- sql/Hosts_Sites.sql
+++ sql/Hosts_Sites.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS Hosts_Sites;
 
-CREATE Hosts_Sites (
+CREATE TABLE Hosts_Sites (
   id  SERIAL,
   host_id integer, FOREIGN KEY (host_id) REFERENCES Hosts (id),
-  site_id integer, FORIEGN KEY (site_id) REFERENCES Sites (id),
+  site_id integer, FOREIGN KEY (site_id) REFERENCES Sites (id),
   PRIMARY KEY (id)
 );
