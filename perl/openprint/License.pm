b0f5454c9f461088b1c5524624b75e09f8d32595
Isaac Connor <isaac@tempesttechnology.com>
2023-05-19 17:25:06 -0400

    Add License generation

--- perl/openprint/License.pm
+++ perl/openprint/License.pm
@@ -8,6 +8,8 @@ require File::Slurp;
 require JSON;
 use Crypt::OpenSSL::Random;
 use Crypt::OpenSSL::RSA;
+#use Crypt::RSA;
+use Digest::SHA qw(sha256_hex);
 use MIME::Base64;
 
 use vars qw( $debug $table $serial %fields %find_fields %transforms %defaults );
@@ -18,6 +20,7 @@ $serial='licenses_id_seq';
 		id						=>	'id',
     company_id    =>  'company_id',
     site_id       =>  'site_id',
+    hash          =>  'hash',
 		serialkey			=>	'serialkey',
 		max_uses			=>	'max_uses',
 		purchased_on	=>	'purchased_on',
@@ -95,21 +98,51 @@ sub generate_key {
   my $self = shift;
   #my $private_key = $config{'Licensing Private Key'};
   #my $private_key = misc::load_file($openprint::log, '/var/www/crm/etc/ssl/cloudmule.key');
-  my $private_key = misc::load_file($openprint::log, '/tmp/key');
-  $openprint::log->debug("Private key $private_key");
-  if (!$private_key) {
-    $openprint::log->error("No private key, can't continue");
-    return;
-  }
-  #my $rsa = Crypt::OpenSSL::RSA->generate_key(2048);
-  #$openprint::log->debug($rsa->get_private_key_string());
-  #File::Slurp::write_file('/tmp/key', { atomic => 1, err_mode=>'carp' }, $rsa->get_private_key_string()) or warn "Couldn't save key file";
-  #File::Slurp::write_file('/tmp/key.pub', { atomic => 1, err_mode=>'carp' }, $rsa->get_public_key_string()) or warn "Couldn't save key file";
+  my $private_key_str = misc::load_file($openprint::log, '/etc/ssl/cloudmule.key');
+  my $public_key_str = misc::load_file($openprint::log, '/etc/ssl/cloudmule.pub');
+  #$openprint::log->debug("Private key $private_key_str");
 
-  my $rsa = Crypt::OpenSSL::RSA->new_private_key($private_key);
-  $rsa->use_pkcs1_padding();
-  $$self{serialkey} = encode_base64($rsa->private_encrypt($self->id().','.$self->features_json()));
-  $openprint::log->debug($$self{serialkey});
+  my ($public_key, $private_key);
+  #my $rsa = Crypt::RSA->new();
+  if (!$private_key_str) {
+    my $rsa = Crypt::OpenSSL::RSA->generate_key(1024);
+    $openprint::log->debug($rsa->get_private_key_string());
+    File::Slurp::write_file('/tmp/key', { atomic => 1, err_mode=>'carp' }, $rsa->get_private_key_string()) or warn "Couldn't save key file";
+    File::Slurp::write_file('/tmp/key.pub', { atomic => 1, err_mode=>'carp' }, $rsa->get_public_key_string()) or warn "Couldn't save key file";
+
+    #($public_key, $private_key) = $rsa->keygen();
+    #$openprint::log->error("No private key, generating a new one");
+    #$private_key->write(Filename => '/tmp/key');
+    #$public_key->write(Filename => '/tmp/key.pub');
+  } else {
+    #$public_key = $rsa->import_key(\$public_key_str);
+    #$private_key = $rsa->import_key(\$private_key_str);
+  }
+
+  my $rsa_public = Crypt::OpenSSL::RSA->new_public_key($public_key_str);
+  my $rsa_private = Crypt::OpenSSL::RSA->new_private_key($private_key_str);
+  $rsa_private->use_pkcs1_padding();
+  $$self{serialkey} = encode_base64($rsa_private->private_encrypt($self->id().','.$self->features_json()));
+  #$$self{serialkey} = encode_base64($rsa->encrypt($self->id().','.$self->features_json(), $private_key));
+  my $hash = sha256_hex($$self{serialkey});
+  #my $signature = $rsa_public->sign($$self{serialkey});
+  #my $signature_hex = unpack("H*", $signature);
+  #$openprint::log->debug("Hash $hash Signature $signature $signature_hex");
+  $openprint::log->debug("Hash $hash");
+  # Extract the first 8 characters of the hash as the license key
+  my $short_license_key = substr($hash, 0, 16);
+  my @codons = $short_license_key =~ /.{4}/g;
+  $$self{hash} = join('-', @codons);
+
+  #my $signature = $rsa->sign_message($hash, $private_key);
+  #$openprint::log->debug("Hash $hash Signature $signature");
+}
+
+sub is_valid {
+  #my $rsa = Crypt::RSA->new();
+  # Verify the signature
+  #my $is_valid = $rsa->verify_message($hash, $signature, $public_key);
+
 }
 
 1;
