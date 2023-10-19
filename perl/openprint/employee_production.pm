75150b4297092ba01bb6c46aa88182337b09d756
Isaac Connor <iconnor@connortechnology.com>
2023-10-19 14:40:09 -0700

    Don't load unused code

--- perl/openprint/employee_production.pm
+++ perl/openprint/employee_production.pm
@@ -25,15 +25,15 @@ require openprint::LabelType;
 require openprint::Label;
 require openprint::PurchaseOrder;
 require openprint::PurchaseOrder_Content;
-require openprint::PaperInventory;
-require openprint::ProductionFeedback;
-require openprint::Shift;
-require openprint::Equipment_Shift;
-require openprint::ScheduledJob;
-require openprint::Project_Service;
-require openprint::SignatureCapture;
+#require openprint::PaperInventory;
+#require openprint::ProductionFeedback;
+#require openprint::Shift;
+#require openprint::Equipment_Shift;
+#require openprint::ScheduledJob;
+#require openprint::Project_Service;
+#require openprint::SignatureCapture;
 require openprint::Operator_Role;
-require openprint::Project_Service_Operator;
+#require openprint::Project_Service_Operator;
 
 use vars qw( $r $log $dbh %variable %param %session %config );
 *r = \$openprint::r;
@@ -962,21 +962,17 @@ sub complete_service {
 	my @forms;
 	foreach my $Job ( openprint::ScheduledJob->find( project_id=>$$Project{id}, 'service_id @>'=>$service_id ) ) {
 		my $Shift = $Job->Shift();
-    if ($Shift) {
-      @operator_ids = sets::union(@operator_ids, @{$Shift->operator_ids()}) if $Shift->operator_ids();
-      push @forms, map { my $sig_specs = openprint::service::get_specs_ref( $Project, $_ ); $$sig_specs{SignatureIndex}; } @{$Job->pertains_id()};
-      # FIXME: What if job has other signatures...
-      my @service_ids = @{$Job->service_id()} if $Job->service_id();
-      @service_ids = sets::exclude( \@service_ids, [ $service_id ] );
+		@operator_ids = sets::union(@operator_ids, @{$Shift->operator_ids()}) if $Shift->operator_ids();
+		push @forms, map { my $sig_specs = openprint::service::get_specs_ref( $Project, $_ ); $$sig_specs{SignatureIndex}; } @{$Job->pertains_id()};
+# FIXME: What if job has other signatures...
+		my @service_ids = @{$Job->service_id()} if $Job->service_id();
+		@service_ids = sets::exclude( \@service_ids, [ $service_id ] );
 
-      if ( ! @service_ids ) {
-        $Job->delete();
-      } else {
-        $Job->save({service_id=>\@service_ids});
-      }
-    } else {
-      $openprint::log->warn("No shift for ".$Job->to_string());
-    } # end if Shift
+		if ( ! @service_ids ) {
+			$Job->delete();
+		} else {
+			$Job->save({service_id=>\@service_ids});
+		}
 	} # end foreach Job
 	@forms = sort sets::union( @forms );
 	
