d46ea6a7beb6295847e4c23bc4f245d029497308
Isaac Connor <iconnor@connortechnology.com>
2024-02-02 11:05:02 -0800

    Rough in User Class

---
+++ sql/User_Class.sql
@@ -0,0 +1,35 @@
+CREATE TABLE user_class (
+    id serial,
+    name text NOT NULL,
+    primary key(id)
+);
+
+INSERT INTO user_class VALUES (2, 'Contractor');
+INSERT INTO user_class VALUES (3, 'Accountant');
+INSERT INTO user_class VALUES (4, 'I.T');
+INSERT INTO user_class VALUES (5, 'Caretaker');
+INSERT INTO user_class VALUES (6, 'New Property Owner');
+INSERT INTO user_class VALUES (7, 'Additional Resident');
+INSERT INTO user_class VALUES (8, 'Spouse');
+INSERT INTO user_class VALUES (9, 'Unknown');
+INSERT INTO user_class VALUES (1, 'Account Owner');
+INSERT INTO user_class VALUES (10, 'Primary Contact');
+INSERT INTO user_class VALUES (11, 'Relay Contact');
+INSERT INTO user_class VALUES (12, 'Gate Code');
+INSERT INTO user_class VALUES (13, 'WiFi SSID/key');
+INSERT INTO user_class VALUES (14, 'Executive Assistant');
+INSERT INTO user_class VALUES (15, 'Property Owner');
+INSERT INTO user_class VALUES (16, 'Secretary/Assistant');
+INSERT INTO user_class VALUES (17, 'Office Manager');
+INSERT INTO user_class VALUES (18, 'Bookkeeper');
+INSERT INTO user_class VALUES (19, 'Summit');
+INSERT INTO user_class VALUES (20, 'General Manager');
+INSERT INTO user_class VALUES (21, 'Maintenance');
+INSERT INTO user_class VALUES (22, 'Property Manager');
+INSERT INTO user_class VALUES (23, 'Project Manager');
+INSERT INTO user_class VALUES (24, 'Former-Account-Holder');
+INSERT INTO user_class VALUES (25, 'Technical Contact');
+INSERT INTO user_class VALUES (26, 'Billing Contact');
+INSERT INTO user_class VALUES (27, 'Credentials');
+INSERT INTO user_class VALUES (28, 'Former_Property_Owner');
+INSERT INTO user_class VALUES (29, 'Neighbor');
