6cf6dcdcf742e72da7cb20ea5d0f1b99c1b31e23
Root @ tempest1 <root@connortechnology.com>
2024-02-08 17:05:59 -0500

    Make pricing.js smarted

--- www/public_html/javascripts/pricing.js
+++ www/public_html/javascripts/pricing.js
@@ -1,89 +1,113 @@
+"use strict";
 
-function calc_from_quantity( element, precision ) {
-	if ( ! precision ) precision = 5;
-	var re = /quantity-(.*)/
-	var matches = re.exec( element.name );
-	if ( matches ) {
-		var index = matches[1];
-		var quantity = parseFloat( floatize( element ) );
-		var markup = parseFloat(1* floatize( element.form.elements['markup-'+index] ) ) /100;
-		var cost = parseFloat(1* floatize( element.form.elements['cost-'+index] ) );
-		element.form.elements['price-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision ); 
-		if ( element.form.elements['chk-'+index] ) {
-			element.form.elements['chk-'+index].checked=true;
-		} // end if
-	} // end if
+function calc_from_quantity(element) {
+  let precision = element.getAttribute('precision');
+  if (!precision) precision = 5;
+  const re = /quantity-(.*)/
+  const matches = re.exec( element.name );
+  if ( matches ) {
+    const index = matches[1];
+    const quantity = parseFloat( floatize( element ) );
+    const markup = parseFloat(1*floatize( element.form.elements['markup-'+index] ) ) /100;
+    const cost = parseFloat(1* floatize( element.form.elements['cost-'+index] ) );
+    if (element.form.elements['total-'+index]) {
+      element.form.elements['price-'+index].value = do_decimals( cost * ( 1 + markup ), precision );
+      element.form.elements['total-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision );
+    } else {
+      element.form.elements['price-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision );
+    }
+    if ( element.form.elements['chk-'+index] ) {
+      element.form.elements['chk-'+index].checked=true;
+    } // end if
+  } else {
+    console.log("calc_from_quantity called from something other than a quantity "+element.name);
+  } // end if
 } // end function calc_from_quantity
 
-function calc_from_cost( element, precision ) {
-	if ( ! precision ) precision = 5;
-	var re = /cost-(.*)/
-	var matches = re.exec( element.name );
-	if ( matches ) {
-		var index = matches[1];
-		var cost = parseFloat( floatize( element ) );
-		var markup = parseFloat(1* floatize( element.form.elements['markup-'+index] )) /100;
-		var quantity = 1;
-		if ( element.form.elements['quantity-'+index] ) {
-			quantity =  parseFloat(1* floatize( element.form.elements['quantity-'+index]));
-		} // end if
-		element.form.elements['price-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision ); 
-		if ( element.form.elements['chk-'+index] ) {
-			element.form.elements['chk-'+index].checked=true;
-		} // end if
-	} // end if
+function calc_from_cost(element) {
+  let precision = element.getAttribute('precision');
+  if (!precision) precision = 5;
+  console.log("Using precision: " + precision);
+  const elements = element.form.elements;
+  const re = /cost-(.*)/;
+  const matches = re.exec(element.name);
+  if (matches) {
+    const index = matches[1];
+    const cost = parseFloat(floatize(element));
+    const markup = parseFloat(1* floatize(elements['markup-'+index])) /100;
+    const quantity = elements['quantity-'+index] ? parseFloat(1* floatize(elements['quantity-'+index])) : 1;
+    if (elements['total-'+index]) {
+      elements['price-'+index].value = do_decimals( cost * ( 1 + markup ), precision );
+      elements['total-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision );
+    } else {
+      elements['price-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision );
+    }
+    if (elements['chk-'+index]) {
+      elements['chk-'+index].checked=true;
+    } // end if
+  } else {
+
+  } // end if matched
 } // end function calc_from_cost
 
-function calc_from_markup( element, precision ) {
-	if ( ! precision ) precision = 5;
-	var re = /markup-(.*)/
-	var matches = re.exec( element.name );
-	if ( matches ) {
-		var index = matches[1];
+function calc_from_markup(element) {
+  let precision = element.getAttribute('precision');
+  if (!precision) precision = 5;
+  const elements = element.form.elements;
 
-		var cost = parseFloat( floatize( element.form.elements['cost-'+index] ) );
-		if ( cost != '' ) {
-			var markup = parseFloat( 1*floatize(element ) );
-			var quantity = 1;
-			if ( element.form.elements['quantity-'+index] ) {
-				quantity = parseFloat(1*floatize(element.form.elements['quantity-'+index]));
-			} // end if
-			var newvalue = cost * quantity * ( markup/100 + 1 );
-			element.form.elements['price-'+index].value = do_decimals( newvalue, precision );
-		} // end if
-		if ( element.form.elements['chk-'+index] ) {
-			element.form.elements['chk-'+index].checked=true;
-		} // end if
-	} // end if
+  const re = /markup-(.*)/
+  const matches = re.exec( element.name );
+  if ( matches ) {
+    const index = matches[1];
+
+    const cost = parseFloat(floatize(elements['cost-'+index]));
+    if ( cost != '' ) {
+      const markup = parseFloat(1*floatize(element));
+      const quantity = elements['quantity-'+index] ?parseFloat(1*floatize(elements['quantity-'+index])) : 1;
+
+      if (elements['total-'+index]) {
+        elements['price-'+index].value = do_decimals( cost * ( 1 + markup ), precision );
+        elements['total-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision );
+      } else {
+        elements['price-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision );
+      } // end if
+    } // end if
+
+    if (elements['chk-'+index]) {
+      elements['chk-'+index].checked=true;
+    } // end if
+  } // end if matches
 } // end function
 
-function calc_from_price( element, precision ) {
-	if ( ! precision ) precision = 2;
-	var re = /^price-(.*)$/;
-	var matches = re.exec( element.name );
+function calc_from_price(element) {
+  let precision = element.getAttribute('precision');
+  if (!precision) precision = 5;
+  const elements = element.form.elements;
+	const re = /^price-(.*)$/;
+	const matches = re.exec( element.name );
 	if ( matches ) {
-		var index = matches[1];
+		const index = matches[1];
 
-		if ( ! element.form.elements['cost-'+index] ) {
+		if (!elements['cost-'+index] ) {
 			alert("No cost element for " + index);
 			return;
 		} // end if
-		var cost = parseFloat( floatize(element.form.elements['cost-'+index] ) );
-		var price = parseFloat( floatize(element) );
+		const cost = parseFloat( floatize(elements['cost-'+index] ) );
+		const price = parseFloat( floatize(element) );
 		if ( cost ) {
-			var quantity = 1;
-			if ( element.form.elements['quantity-'+index] ) {
-				quantity = parseFloat(1*floatize(element.form.elements['quantity-'+index]));
-			} // end if
-			element.form.elements['markup-'+index].value = do_decimals( ((price / (cost*quantity))-1)*100, precision );
+			const quantity = elements['quantity-'+index] ? parseFloat(1*floatize(elements['quantity-'+index])) : 1;
+			elements['markup-'+index].value = do_decimals( ((price / (cost*quantity))-1)*100, precision );
 		} // end if
-		if ( element.form.elements['chk-'+index] ) {
-			element.form.elements['chk-'+index].checked=true;
+    if (elements['total-'+index]) {
+		  elements['total-'+index].value = do_decimals( cost * quantity * ( 1 + markup ), precision );
+    }
+		if (elements['chk-'+index]) {
+			elements['chk-'+index].checked=true;
 		} // end if
 	} // end if
 } // end function
 
-function add_price ( form, pricelist_id, equipment_id ) {
+function add_price (form, pricelist_id, equipment_id) {
 	$('prices-'+pricelist_id+'-'+equipment_id).innerHTML = 'Please wait...loading.';
 	new Ajax.Updater( 'prices-'+pricelist_id+'-'+equipment_id, '_prices_table_body.html?action=add&pricelist_id='+pricelist_id,
     { method: 'post', parameters:form.serialize(), evalScripts: true } );
@@ -104,23 +128,23 @@ function copy_price ( form, pricelist_id, equipment_id, price_id ) {
 function add_new_price ( service_id, pricelist_id, equipment_id ) {
 
 	if ( $('prices-'+pricelist_id+'-'+equipment_id) ) {
-		new Ajax.Updater( 'prices-'+pricelist_id+'-'+equipment_id, '_price.html', { 
-			parameters: { 
+		new Ajax.Updater( 'prices-'+pricelist_id+'-'+equipment_id, '_price.html', {
+			parameters: {
 				pricelist_id: pricelist_id,
-				equipment_id: equipment_id, 
+				equipment_id: equipment_id,
 				service_id: service_id,
 				action: 'add'
-			 }, 
+			 },
 			insertion: 'bottom' }
 			);
 	} else {
-		new Ajax.Updater( 'pricelist-'+pricelist_id, '_prices_per_equipment.html', { 
-			parameters: { 
+		new Ajax.Updater( 'pricelist-'+pricelist_id, '_prices_per_equipment.html', {
+			parameters: {
 				pricelist_id: pricelist_id,
-				equipment_id: equipment_id, 
+				equipment_id: equipment_id,
 				service_id: service_id,
 				action: 'add'
-			 }, 
+			 },
 			insertion: 'bottom' }
 			);
 	} // end if
@@ -153,5 +177,5 @@ function check_price( element ) {
     } else {
         alert('Not matched' + element.name);
     } // end if
-} // end function check_field 
+} // end function check_field
 
