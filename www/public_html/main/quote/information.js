78a36d98270e0d38b2576728fa855c4280839347
Root @ tempest1 <root@connortechnology.com>
2024-02-09 14:16:03 -0500

    Move js to it's own file. Add remove_product function

---
+++ www/public_html/main/quote/information.js
@@ -0,0 +1,29 @@
+function remove_product(button) {
+  const data = $j(button.form).serialize();
+  const product_id = button.getAttribute('data_product_id');
+  $j.ajax({
+    url: '_products.html?action=del&amp;product_id='+product_id
+  }).done(function(data) {
+    $j('#Products').html(data);
+    update_event_bindings();
+    tinyMCE.init({
+      mode : "specific_textareas",
+      editor_selector : "mce",
+      theme : "simple"
+    });
+  });
+}
+window.addEventListener('DOMContentLoaded',function(){
+  tinyMCE.init({
+    mode : "specific_textareas",
+    editor_selector : "mce",
+    theme : "simple"
+  });
+});
+
+function company_onchange( e ) {
+  new Ajax.Request( '_company_information.json', { parameters: { company_id: e.value } } );
+} // end function
+function user_onchange( e ) {
+  new Ajax.Request( '_user_information.json', { parameters: { user_id: e.value } } );
+} // end function
