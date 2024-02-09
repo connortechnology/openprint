3243f9120c96abce3ba5b7a69efdb29190fb756a
Root @ tempest1 <root@connortechnology.com>
2024-02-09 14:15:23 -0500

    test for btnFunction to fix a warning

--- perl/openprint/product.pm
+++ perl/openprint/product.pm
@@ -34,142 +34,144 @@ sub view {
 sub edit {
 	my $Product = $variable{Product} = new openprint::Product( $param{product_id} );
 
-	if ( $param{btnFunction} eq 'Save' ) {
-		if ( (! $param{product_id}) and openprint::Product->find( 'name lc' => lc openprint::Product->transform('name',$param{name}) ) ) {
-			$variable{error} = "A product with name $param{name} already exists.	Please choose another name.";
-			return;
-		} # end if
-		foreach my $field ( 'category', 'manufacturer' ) {
-			if ( $param{$field} ) {
-				delete $param{$field.'_id'};
-			} else {
-				delete $param{$field};
-			} # end if
-		} # end foreach
-		$param{supplier_id} = openprint::Supplier::get_or_create( $param{supplier} ) if $param{supplier} and ! $param{supplier_id};
-		$Product->supplier_id( $param{supplier_id} );
+  if ($param{btnFunction}) {
+    if ( $param{btnFunction} eq 'Save' ) {
+      if ( (! $param{product_id}) and openprint::Product->find( 'name lc' => lc openprint::Product->transform('name',$param{name}) ) ) {
+        $variable{error} = "A product with name $param{name} already exists.	Please choose another name.";
+        return;
+      } # end if
+      foreach my $field ( 'category', 'manufacturer' ) {
+        if ( $param{$field} ) {
+          delete $param{$field.'_id'};
+        } else {
+          delete $param{$field};
+        } # end if
+      } # end foreach
+      $param{supplier_id} = openprint::Supplier::get_or_create( $param{supplier} ) if $param{supplier} and ! $param{supplier_id};
+      $Product->supplier_id( $param{supplier_id} );
 
-		my @changes = $Product->changes( \%param );
-		if ( @changes ) {
-			$variable{error} = $Product->save( \%param );
-		}
-		if ( $param{product_id} ) {
-		# Save the prices
-			_prices();
-			my @spec_changes = openprint::Object_Specification::save_changes( $Product, \%param );
-			push @changes, 'specification changes: ' . join(', ', @spec_changes ) if @spec_changes;
+      my @changes = $Product->changes( \%param );
+      if ( @changes ) {
+        $variable{error} = $Product->save( \%param );
+      }
+      if ( $param{product_id} ) {
+        # Save the prices
+        _prices();
+        my @spec_changes = openprint::Object_Specification::save_changes( $Product, \%param );
+        push @changes, 'specification changes: ' . join(', ', @spec_changes ) if @spec_changes;
 
-		} # end if
-		( new openprint::Log())->save({Object=>$Product, action=>'Edit', note=>join('<br/>', @changes ) } ) if @changes;
-		$param{btnFunction} = '';
-		$variable{ExternalRedirect} = '/product/edit.html?product_id='.$Product->id();
-	} elsif ( $param{btnFunction} eq 'Copy' ) {
-		my $NewProduct = $Product->copy();
-		$NewProduct->save();
+      } # end if
+      ( new openprint::Log())->save({Object=>$Product, action=>'Edit', note=>join('<br/>', @changes ) } ) if @changes;
+      $param{btnFunction} = '';
+      $variable{ExternalRedirect} = '/product/edit.html?product_id='.$Product->id();
+    } elsif ( $param{btnFunction} eq 'Copy' ) {
+      my $NewProduct = $Product->copy();
+      $NewProduct->save();
 
-		(new openprint::Log())->save({action=>'Copy Product', note=>'New Product ID: ' . $NewProduct->id() . ' Name: ' . $NewProduct->name(), Object=>$Product });
-		(new openprint::Log())->save({action=>'Copy Product', note=>'Original Product ID: ' . $param{product_id} . ' Name: ' . $Product->name(), Object=>$NewProduct });
+      (new openprint::Log())->save({action=>'Copy Product', note=>'New Product ID: ' . $NewProduct->id() . ' Name: ' . $NewProduct->name(), Object=>$Product });
+      (new openprint::Log())->save({action=>'Copy Product', note=>'Original Product ID: ' . $param{product_id} . ' Name: ' . $Product->name(), Object=>$NewProduct });
 
-		foreach my $Price ( openprint::ProductPrice->find( product_id => $param{product_id} ) ) {
-			$$Price{product_id} = $NewProduct->id();
-			$$Price{id} = undef;
-			$Price->save();
-		} # end foreach
-		foreach ( $NewProduct->Specifications() ) {
-			$_->save({object_id => $$NewProduct{id} });
-		}
-		$Product = $NewProduct;
+      foreach my $Price ( openprint::ProductPrice->find( product_id => $param{product_id} ) ) {
+        $$Price{product_id} = $NewProduct->id();
+        $$Price{id} = undef;
+        $Price->save();
+      } # end foreach
+      foreach ( $NewProduct->Specifications() ) {
+        $_->save({object_id => $$NewProduct{id} });
+      }
+      $Product = $NewProduct;
 
-	} elsif ( $param{btnFunction} eq 'Delete' ) {
-		if ( ! ( $variable{error} .= $Product->delete() ) ) {
-			$variable{information} .= 'Product deleted successfully.';
-			$Product = $Product->next();
-		} # end if
-	} elsif ( $param{btnFunction} eq '>>' ) {
-		$Product = $Product->next();
-	} elsif ( $param{btnFunction} eq '<<' ) {
-		$Product = $Product->previous();
-	} elsif ( $param{btnFunction} eq 'Export Definitions' ) {
-		my @header = ( 'Name', 'Description','Category', 'Tax Exempt 1','Tax Exempt2', 'Sort Order');
-		my @data = sql::execute( $log, $dbh, 'SELECT name, description, (SELECT name from product_categories where id=category_id), taxexempt1, taxexempt2, sort FROM Products ORDER BY sort' );
-		misc::export_csv( $r, $log, \%variable, 'Products.csv', \@header, \@data );
-	} elsif ( $param{btnFunction} eq 'Import Definitions' ) {
-		my $error = '';
-		if ( $param{fileImport} ) {
-			my $upload = $r->upload( 'fileImport' );
-			my $io = $upload->io();
-			$_ = <$io>;
+    } elsif ( $param{btnFunction} eq 'Delete' ) {
+      if ( ! ( $variable{error} .= $Product->delete() ) ) {
+        $variable{information} .= 'Product deleted successfully.';
+        $Product = $Product->next();
+      } # end if
+    } elsif ( $param{btnFunction} eq '>>' ) {
+      $Product = $Product->next();
+    } elsif ( $param{btnFunction} eq '<<' ) {
+      $Product = $Product->previous();
+    } elsif ( $param{btnFunction} eq 'Export Definitions' ) {
+      my @header = ( 'Name', 'Description','Category', 'Tax Exempt 1','Tax Exempt2', 'Sort Order');
+      my @data = sql::execute( $log, $dbh, 'SELECT name, description, (SELECT name from product_categories where id=category_id), taxexempt1, taxexempt2, sort FROM Products ORDER BY sort' );
+      misc::export_csv( $r, $log, \%variable, 'Products.csv', \@header, \@data );
+    } elsif ( $param{btnFunction} eq 'Import Definitions' ) {
+      my $error = '';
+      if ( $param{fileImport} ) {
+        my $upload = $r->upload( 'fileImport' );
+        my $io = $upload->io();
+        $_ = <$io>;
 
-			my $csv = Text::CSV_XS->new();
-			my $ac = sql::start_transaction( $dbh );
-			my %categories = map { $_->name(), $_ } openprint::Product_Category->find();
-			my %products = map { $_->name(), $_ } openprint::Product->find();
-			
-			while ( <$io> ) {
-				my $status = $csv->parse($_);
-				my ( $name, $description, $category, $taxexempt1, $taxexempt2, $sort ) = misc::trim( $csv->fields() );
-				next if ! $name;
-				if ( $category and ! $categories{$category} ) {
-					$categories{$category} = new openprint::Product_Category();
-					$categories{$category}->name( $category );
-					$categories{$category}->save();
-				} # end if
-				my %sql = (
-					'name'			=>	$name,
-					'description'	=>	$description,
-					'category_id'	=>	$category ? $categories{$category}->id() : undef,
-					'taxexempt1'	=>	$taxexempt1,
-					'taxexempt2'	=>	$taxexempt2,
-					'sort'			=>	$sort,
-				);
-				my $Product = $products{$name} ? $products{$name} : new openprint::Product();
-				$error .= $Product->save( \%sql );
-			} # end while
-			sql::end_transaction( $dbh, $ac );
-		} else {
-			$log->warn( "No file given to upload." );
-		} # end if
-		if ( $error ne '' ) {
-			return misc::error( $log, $dbh, \%variable, 'Import errors.', $error );
-		} # end if
-	} elsif ( $param{btnFunction} eq 'Export Specifications' ) {
-		my @header = ( 'Product', 'Name','Value');
-		my @data;
-		foreach my $Product ( openprint::Product->find() ) {
-			foreach my $Spec ( $Product->Specifications() ) {
-				push @data, $Product->name(), $Spec->name(), $Spec->value();
-			} # end foreach
-		} # end foreach
-		misc::export_csv( $r, $log, \%variable, 'ProductSpecifications.csv', \@header, \@data );
-	} elsif ( $param{btnFunction} eq 'Import Specifications' ) {
-		my $error = '';
-		if ( $param{fileImport} ) {
-			my $upload = $r->upload( 'fileImport' );
-			my $io = $upload->io();
-			$_ = <$io>;
+        my $csv = Text::CSV_XS->new();
+        my $ac = sql::start_transaction( $dbh );
+        my %categories = map { $_->name(), $_ } openprint::Product_Category->find();
+        my %products = map { $_->name(), $_ } openprint::Product->find();
 
-			my $csv = Text::CSV_XS->new();
-			my $ac = sql::start_transaction( $dbh );
-			my %products = map { $_->name(), $_ } openprint::Product->find();
-			# Clear Specifications
-			foreach my $P ( keys %products ) {
-				$products{$P}{Specifications} = ();
-			} # end foreach
-			
-			while ( <$io> ) {
-				my $status = $csv->parse($_);
-				my ( $product, $name, $value ) = misc::trim( $csv->fields() );
-				next if ! $product;
-				$products{$product}{Specifications}{$name} = $value;
-			} # end while
+        while ( <$io> ) {
+          my $status = $csv->parse($_);
+          my ( $name, $description, $category, $taxexempt1, $taxexempt2, $sort ) = misc::trim( $csv->fields() );
+          next if ! $name;
+          if ( $category and ! $categories{$category} ) {
+            $categories{$category} = new openprint::Product_Category();
+            $categories{$category}->name( $category );
+            $categories{$category}->save();
+          } # end if
+          my %sql = (
+            'name'			=>	$name,
+            'description'	=>	$description,
+            'category_id'	=>	$category ? $categories{$category}->id() : undef,
+            'taxexempt1'	=>	$taxexempt1,
+            'taxexempt2'	=>	$taxexempt2,
+            'sort'			=>	$sort,
+          );
+          my $Product = $products{$name} ? $products{$name} : new openprint::Product();
+          $error .= $Product->save( \%sql );
+        } # end while
+        sql::end_transaction( $dbh, $ac );
+      } else {
+        $log->warn( "No file given to upload." );
+      } # end if
+      if ( $error ne '' ) {
+        return misc::error( $log, $dbh, \%variable, 'Import errors.', $error );
+      } # end if
+    } elsif ( $param{btnFunction} eq 'Export Specifications' ) {
+      my @header = ( 'Product', 'Name','Value');
+      my @data;
+      foreach my $Product ( openprint::Product->find() ) {
+        foreach my $Spec ( $Product->Specifications() ) {
+          push @data, $Product->name(), $Spec->name(), $Spec->value();
+        } # end foreach
+      } # end foreach
+      misc::export_csv( $r, $log, \%variable, 'ProductSpecifications.csv', \@header, \@data );
+    } elsif ( $param{btnFunction} eq 'Import Specifications' ) {
+      my $error = '';
+      if ( $param{fileImport} ) {
+        my $upload = $r->upload( 'fileImport' );
+        my $io = $upload->io();
+        $_ = <$io>;
 
-			foreach my $P ( keys %products ) {
-				$error .= $products{$P}->save();
-			} # end foreach
-			sql::end_transaction( $dbh, $ac );
-		} # end if
-	} # end if
-	$variable{Product} = $Product;
+        my $csv = Text::CSV_XS->new();
+        my $ac = sql::start_transaction( $dbh );
+        my %products = map { $_->name(), $_ } openprint::Product->find();
+        # Clear Specifications
+        foreach my $P ( keys %products ) {
+          $products{$P}{Specifications} = ();
+        } # end foreach
+
+        while ( <$io> ) {
+          my $status = $csv->parse($_);
+          my ( $product, $name, $value ) = misc::trim( $csv->fields() );
+          next if ! $product;
+          $products{$product}{Specifications}{$name} = $value;
+        } # end while
+
+        foreach my $P ( keys %products ) {
+          $error .= $products{$P}->save();
+        } # end foreach
+        sql::end_transaction( $dbh, $ac );
+      } # end if
+    } # end if
+  } #end if btnFunction
+    $variable{Product} = $Product;
 } # end sub edit
 
 sub categories {
