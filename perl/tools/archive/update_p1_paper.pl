67f23f25b1e66cb6fb008a3ced5825e034a35a5c
Isaac Connor <isaac@tempesttechnology.com>
2024-03-25 12:40:25 -0400

    Update update_p1_paper

--- perl/tools/archive/update_p1_paper.pl
+++ perl/tools/archive/update_p1_paper.pl
@@ -3,34 +3,38 @@ use lib '/etc/apache2/lib/perl';
 use Date::Calc;
 use strict;
 require sql;
+require configuration;
 require logger;
 require openprint::Object;
 require openprint::Paper;
 
 use openprint ();
-use vars qw( $log $dbh );
+use vars qw( $log $dbh %config);
 *log = \$openprint::log;
 *dbh = \$openprint::dbh;
+*config = \%openprint::config;
 
 $log = new logger( 'warn' );
 my %sql_server;
 $sql_server{'database'} = $ARGV[0];
 $sql_server{'driver'}   = 'Pg';
-$sql_server{'login'}    = 'point-one';
-$sql_server{'password'} = 'point-1';
+$sql_server{'login'}    = $ARGV[1];
+$sql_server{'password'} = $ARGV[2];
 
 $dbh = sql::open_sql( $log, %sql_server );
+configuration::init( $log, $dbh );
+
 $dbh->do( q{alter table paper_prices drop constraint "tbl_paper_prices_lngpaperindex_fkey";});
 $dbh->do( q{alter table paper_purchase_order_contents drop constraint "$2";});
 $dbh->do( q{DELETE FROM paper_recommendations where lngpaperindex NOT IN (SELECT lngIndex FROM tbl_Paper)});
 # Update paper db now
-my %types = sql::execute( undef, undef, q{SELECT strName, lngIndex FROM Project_Types} );
+my %types = sql::execute( undef, undef, q{SELECT name, id FROM Project_Types} );
 my $data = $dbh->selectall_arrayref( 'SELECT * FROM tbl_Paper', {'Slice'=>{}} );
 foreach my $paper ( @$data ) {
 	next if  $$paper{'strname'} eq 'Customer Supplied';
 	$log->warn("Considering paper: $$paper{'strname'} $$paper{'strfinish'} $$paper{'strcolour'} $$paper{'strweight'} $$paper{'dblwidth'}x$$paper{'dblheight'}");
 	my @papers = openprint::Paper->find(
-			'name'	=>	$$paper{'strname'},
+			'brand'	=>	$$paper{'strname'},
 			'finish'=>	$$paper{'strfinish'},
 			'colour'=>	$$paper{'strcolour'},
 			'weight'=>	$$paper{'strweight'},
@@ -40,7 +44,7 @@ foreach my $paper ( @$data ) {
 	if ( ! @papers ) {
 		$log->warn("Adding paper: $$paper{'strname'} $$paper{'strfinish'} $$paper{'strcolour'} $$paper{'strweight'} $$paper{'dblwidth'}x$$paper{'dblheight'}");
 		my $Paper = new openprint::Paper();
-		$Paper->name($$paper{'strname'});
+		$Paper->brand($$paper{'strname'});
 		$Paper->finish($$paper{'strfinish'});
 		$Paper->colour($$paper{'strcolour'});
 		$Paper->weight($$paper{'strweight'});
@@ -56,15 +60,15 @@ foreach my $paper ( @$data ) {
 		$Paper->taxexempt2( $$paper{'ysntaxexempt2'} eq 'Y' ? 1 : 0 );
 		$Paper->score_required( $$paper{'strcalliper'} > 0.008 ? 1 : 0 );
 		$Paper->manufacturer( 'unknown');
-		$Paper->owner_id( 6 );
+    $Paper->quality('new');
+		$Paper->owner_id( $config{owner_id} );
 		$Paper->created_on('2007-01-01 00:00:00');
-		@{$$Paper{'recommendations'}} = sql::execute( undef, undef, q{SELECT strID FROM Project_Types WHERE lngIndex IN ( SELECT lngProjectTypeIndex FROM paper_recommendations WHERE lngPaperIndex=?)}, $$paper{lngindex} );
+		@{$$Paper{'recommendations'}} = sql::execute( undef, undef, q{SELECT name FROM Project_Types WHERE id IN ( SELECT lngProjectTypeIndex FROM paper_recommendations WHERE lngPaperIndex=?)}, $$paper{lngindex} );
 	$log->warn("Recommended for: @{$$Paper{'recommendations'}}");
 		@{$$Paper{'Prices'}} = openprint::PaperPrice->find( 'paper_id' => $$paper{'lngindex'} );
 		$_ = $Paper->save();
 		if ( $_ ) {
-			$log->error($_);
-			next;
+      die $_;
 		} # end if
 		sql::execute( undef, undef, q{UPDATE paper_purchase_order_contents set paper_id=? WHERE paper_id=?}, $Paper->id(), $$paper{lngindex} );
 	} else {
