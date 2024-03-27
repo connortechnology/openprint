e9f62c07286ed1c403cc0fabb931d7d3181d783a
Isaac Connor <isaac@tempesttechnology.com>
2024-03-27 15:58:10 -0400

    Ad warnings and debug to EquipmentSpecification

--- perl/openprint/EquipmentSpecification.pm
+++ perl/openprint/EquipmentSpecification.pm
@@ -1,11 +1,13 @@
 use strict;
+use warnings;
+
 package openprint::EquipmentSpecification;
 our @ISA = qw( openprint::Object );
 require openprint::Equipment;
 
 use vars qw( $debug $table $serial %fields %transforms %defaults );
 
-$debug = 0;
+$debug = 1;
 $table = 'tbl_Equipment_Specifications';
 $serial = 'tbl_equipment_specifications_id_seq';
 
