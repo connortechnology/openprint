d1ae502844f827bdd0fdfc8342234c5276de2260
Isaac Connor <isaac@tempesttechnology.com>
2024-03-28 16:06:34 -0400

    Convert to perl

---
+++ perl/tools/change_db_ownership.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+my $DB=$ARGV[0];
+my $NEW_OWNER=$ARGV[1];
+my @funcs = `psql -qAt -c"SELECT ns.nspname || '.' || proname || '(' || oidvectortypes(proargtypes) || ')' FROM pg_proc INNER JOIN pg_namespace ns ON (pg_proc.pronamespace = ns.oid) WHERE ns.nspname='public' order by proname;" $DB`;
+print "@funcs\n";
+foreach my $func (@funcs) {
+  print "ALTER FUNCTION $func OWNER TO $NEW_OWNER $DB\n";
+  `psql -c "ALTER FUNCTION $func OWNER TO $NEW_OWNER" $DB`;
+}
+#for tbl in `psql -qAt -c "select tablename from pg_tables where schemaname = 'public';" $DB` ; do  psql -c "alter table \"$tbl\" owner to $NEW_OWNER" $DB ; done
+#for tbl in `psql -qAt -c "select sequence_name from information_schema.sequences where sequence_schema = 'public';" $DB` ; do  psql -c "alter sequence \"$tbl\" owner to $NEW_OWNER" $DB ; done
+#for tbl in `psql -qAt -c "select table_name from information_schema.views where table_schema = 'public';" $DB` ; do  psql -c "alter view \"$tbl\" owner to $NEW_OWNER" $DB ; done
