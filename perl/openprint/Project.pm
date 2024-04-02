8f28face2aef581b0b848ebd0ae357f57ceec705
Isaac Connor <isaac@tempesttechnology.com>
2024-04-02 15:47:04 -0400

    Remove deprecation notice on quantities(), improve it on get_quantities;

--- perl/openprint/Project.pm
+++ perl/openprint/Project.pm
@@ -436,7 +436,8 @@ if ( 1 ) {
 
 sub get_quantities {
 	my $self = shift;
-$openprint::log->error("DEPRECATED call to get_quantities");
+	my ( $caller, undef, $line ) = caller;
+$openprint::log->error("DEPRECATED call to get_quantities from $caller:$line");
 	return @$self{'quantity1','quantity2','quantity3'};
 } # end sub get_quantities
 
@@ -674,8 +675,7 @@ sub quantity_indexes {
 
 sub quantities {
 	my $self = shift;
-$openprint::log->error("DEPRECATED call to quantities");
-	return @$self{map { $$self{"quantity$_"} ? "quantity$_" : () } ( 1 .. 3 )};
+	return @$self{map { $$self{'quantity'.$_} ? 'quantity'.$_ : () } ( 1 .. 3 )};
 } # end sub quantities
 
 sub quantity {
