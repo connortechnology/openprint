1ccd8712397e48d892d78930382994d5fade6c13
Isaac Connor <iconnor@connortechnology.com>
2024-04-15 17:11:43 -0400

    Add pricing for Material Service + MR in Bundling.

--- perl/openprint/Estimating/Bundling.pm
+++ perl/openprint/Estimating/Bundling.pm
@@ -149,6 +149,8 @@ sub calc {
 		@CrossMaterials = @Materials;
 		$CrossMaterial = $Material;
 	}
+  my $MaterialService = openprint::Service->find_one(name=>$Material->name()) if $Material;
+  my $MaterialMRService = openprint::Service->find_one(name=>$Material->name().'MakeReady') if $Material;
 	
 	if ( ! ( $$specs{cross_bands_per_package} or $$specs{bands_per_package} ) ) {
 		$$specs{alert} .= 'Please enter the # bands.<br/>';
@@ -170,16 +172,16 @@ sub calc {
 			my $sig_specs = openprint::service::get_specs_ref( $Project, $$services{Signature}[0] );
 			if ( $$sig_specs{Versions} ) {
 				my $versions_per_package = int( $qty / $$sig_specs{Versions} );
-$openprint::log->debug("Per package due to versions: $qty / $$sig_specs{Versions} = $versions_per_package");
+        $openprint::log->debug("Per package due to versions: $qty / $$sig_specs{Versions} = $versions_per_package");
 				if ( $versions_per_package < $$specs{txtItemsPerPackage} ) {
 					$$specs{txtItemsPerPackage} = $versions_per_package;
 				} # end if
 			} else {
 				# No versions?
-				$openprint::log->debug("No versions");
+        #$openprint::log->debug('No versions');
 			} # end if
 		} else {
-			$openprint::log->debug("No signatnures");
+			$openprint::log->debug('No signatnures');
 		} # end if
 		my $package_qty = $$specs{txtItemsPerPackage} ? ceil( $qty/$$specs{txtItemsPerPackage} ) : 0;
 		my $m_qty = $$specs{txtItemsPerPackage} ? ceil( 1000/$$specs{txtItemsPerPackage} ) : 0;
@@ -209,7 +211,40 @@ $openprint::log->debug("Per package due to versions: $qty / $$sig_specs{Versions
 			$unitPrice += $ServicePrice{Total};
 			$mprice += $ServicePrice{MPrice};
 		} # end if
+
+    if ($MaterialService) {
+      my %MaterialServicePrice = $MaterialService->get_price( $qty, undef );
+      if ( %MaterialServicePrice ) {
+        if ( $MaterialServicePrice{units} eq 'per m' ) {
+          $MaterialServicePrice{MPrice} = $MaterialServicePrice{Price};
+          $MaterialServicePrice{Total} = $MaterialServicePrice{Price} * $qty / 1000;
+          $$specs{'hdnBreakdown'.$qty_index} .= $Material->description().sprintf(' $%1$.2f%2$s * %4$d = $%3$.2f<br/>', @MaterialServicePrice{'Price','units','Total'}, $qty );
+        } elsif ( $MaterialServicePrice{units} eq 'each' ) {
+          $MaterialServicePrice{MPrice} = $MaterialServicePrice{Price} * 1000;
+          $MaterialServicePrice{Total} = $MaterialServicePrice{Price} * $qty;
+          $$specs{'hdnBreakdown'.$qty_index} .= $Material->description().sprintf(' $%1$.2f%2$s * %4$d = $%3$.2f<br/>', @MaterialServicePrice{'Price','units','Total'}, $qty );
+        } elsif ( $MaterialServicePrice{units} eq 'per bundle' or $MaterialServicePrice{units} eq 'per package') {
+          $MaterialServicePrice{Total} = $MaterialServicePrice{Price} * $package_qty;
+          $MaterialServicePrice{MPrice} = $MaterialServicePrice{Price} * $m_qty;
+          $$specs{'hdnBreakdown'.$qty_index} .= $Material->description().sprintf(' $%1$.2f%2$s * %4$d = $%3$.2f<br/>', @MaterialServicePrice{'Price','units','Total'}, $package_qty );
+        } else {
+          $$specs{'hdnBreakdown'.$qty_index} .= sprintf('No units set ffor service %s (%s)<br/>', $Material->description(), $MaterialServicePrice{units} );
+          $MaterialServicePrice{Total} = $MaterialServicePrice{Price} * $package_qty;
+        } # end if
+        $unitPrice += $MaterialServicePrice{Total};
+        $mprice += $MaterialServicePrice{MPrice};
+      } # end if
+    }
 		$price = $unitPrice + $makeReady;
+
+    if ($MaterialMRService) {
+      my %MaterialMRServicePrice = $MaterialMRService->get_price( $qty, undef );
+      if ( %MaterialMRServicePrice ) {
+        $MaterialMRServicePrice{Total} = $MaterialMRServicePrice{Price};
+        $$specs{'hdnBreakdown'.$qty_index} .= $MaterialMRService->description().sprintf(' $%.2f<br/>', $MaterialMRServicePrice{Total});
+        $price += $MaterialMRServicePrice{Total};
+      } # end if
+    }
 
 		if ( $$specs{rdbCardboardBacking} eq 'Y' ) {
 			if ( $Cardboard ) {
