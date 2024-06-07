5c9bcf40e9d5342f2b0e29a53fda477ebae32c6f
root <root@pseudo.connortechnology.com>
2024-06-07 11:25:26 -0400

    Add ServiceName and Service to price hash

--- perl/openprint/Service.pm
+++ perl/openprint/Service.pm
@@ -157,6 +157,16 @@ sub get_Price {
   return undef;
 } # end sub get_Price
 
+sub Prices {
+  my $self = shift;
+  $$self{Prices} = shift if @_;
+  if (!$$self{Prices}) {
+    $$self{Prices} = [ openprint::ServicePrice->find( 'period_end is null'=>1, order=>'min NULLS FIRST, max NULLS FIRST', service_id=>$$self{id}) ];
+  }
+  return @{$$self{Prices}} if wantarray;
+  return $$self{Prices};
+}
+
 sub get_price {
   my ( $self, $quantity, $Equipment, $Pricelist, $period ) = @_;
   if (! $$self{id}) {
@@ -165,7 +175,7 @@ sub get_price {
     return ;
   }
 
-	if ( ! $period ) {
+	if (!$period) {
 		$period = 'NOW()';
 		if ( $debug ) {
 			$log->debug("No period specified defaulting to $period");
@@ -174,7 +184,7 @@ sub get_price {
 
 	$Pricelist = $openprint::Pricelist if ! $Pricelist;
   my %price = openprint::pricing::get_best_price_object(
-			$openprint::session{company_id}, $$self{id}, $$Pricelist{id}, 'openprint::service_priceset', $quantity, $$Equipment{id}, $period );
+			$openprint::session{company_id}, $$self{id}, $$Pricelist{id}, $self, $quantity, $$Equipment{id}, $period );
 
 	if (!%price) {
     $price{ServiceName} = $$self{name};
