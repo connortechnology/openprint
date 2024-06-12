41c279df11d60139d7e43c66c22b8be2a379c5bf
root <root@pseudo.connortechnology.com>
2024-06-12 14:27:49 -0400

    Handle no run price

--- perl/openprint/Estimating/Stitching.pm
+++ perl/openprint/Estimating/Stitching.pm
@@ -1217,20 +1217,22 @@ $openprint::log->debug('BaseService '.($BaseService ? $BaseService->to_string() 
 	foreach my $pass ( @{$price{Passes}} ) {
 		my $servicePrice = $$pass{ServicePrice};
 
-		if ( $$servicePrice{units} eq 'per m' or $$servicePrice{units} eq 'per 1000' ) {
-			$$servicePrice{quantity} = $qty/1000;
-			$$servicePrice{Total} = $$servicePrice{Price} * $qty/1000;
-		} elsif ( $$servicePrice{units} eq 'per hour' ) {
-			$$servicePrice{quantity} = $$pass{RunTime};
-			$$servicePrice{Total} = $$servicePrice{Price} * $$pass{RunTime};
-		} elsif ( $$servicePrice{units} eq 'each' ) {
-			$$servicePrice{quantity} = $qty;
-			$$servicePrice{Total} = $$servicePrice{Price} * $qty;
-		} else {
-      $price{alert} .= "Unknown Units: $$servicePrice{units} for $$ServiceType{name} on $$Equipment{strid}<br/>";
-			$openprint::log->debug("Unknown Units: $$servicePrice{units} for $$ServiceType{name} range($neededPockets) equipment($$Equipment{strid})");
-		} # end if
-		$price{Service} += $$servicePrice{Total}
+    if ($servicePrice) {
+      if ( $$servicePrice{units} eq 'per m' or $$servicePrice{units} eq 'per 1000' ) {
+        $$servicePrice{quantity} = $qty/1000;
+        $$servicePrice{Total} = $$servicePrice{Price} * $qty/1000;
+      } elsif ( $$servicePrice{units} eq 'per hour' ) {
+        $$servicePrice{quantity} = $$pass{RunTime};
+        $$servicePrice{Total} = $$servicePrice{Price} * $$pass{RunTime};
+      } elsif ( $$servicePrice{units} eq 'each' ) {
+        $$servicePrice{quantity} = $qty;
+        $$servicePrice{Total} = $$servicePrice{Price} * $qty;
+      } else {
+        $price{alert} .= "Unknown Units: $$servicePrice{units} for $$ServiceType{name} on $$Equipment{strid}<br/>";
+        $openprint::log->debug("Unknown Units: $$servicePrice{units} for $$ServiceType{name} range($neededPockets) equipment($$Equipment{strid})");
+      } # end if
+      $price{Service} += $$servicePrice{Total}
+    }
 	} # end foreach pass
 # FIXME
 	my @sigs;
@@ -1269,15 +1271,18 @@ $openprint::log->debug('BaseService '.($BaseService ? $BaseService->to_string() 
 		}
 		$pass{RunTime} = $runtime;
 		$price{RunTime} += $runtime;
-		if ( $$servicePrice{units} eq 'per m' ) {
-			$$servicePrice{Total} = $$servicePrice{Price} * $qty/1000;
-		} elsif ( $$servicePrice{units} eq 'per hour' ) {
-			$$servicePrice{Total} = $$servicePrice{Price} * $runtime;
-		} else {
-			$openprint::log->error("Unknown Unit Type: $$servicePrice{units} for $$ServiceType{name} range(1) equipment($$Equipment{strid})");
-		} # end if
-		$pass{ServicePrice} = $servicePrice;
-		$price{Service} += $$servicePrice{Total};
+    if ($servicePrice) {
+      if ( $$servicePrice{units} eq 'per m' ) {
+        $$servicePrice{Total} = $$servicePrice{Price} * $qty/1000;
+      } elsif ( $$servicePrice{units} eq 'per hour' ) {
+        $$servicePrice{Total} = $$servicePrice{Price} * $runtime;
+      } else {
+        $price{alert} .= "Unknown Unit Type: $$servicePrice{units} for $$ServiceType{name} range(1) equipment($$Equipment{strid})<br/>";
+        $openprint::log->error("Unknown Unit Type: $$servicePrice{units} for $$ServiceType{name} range(1) equipment($$Equipment{strid})");
+      } # end if
+      $pass{ServicePrice} = $servicePrice;
+      $price{Service} += $$servicePrice{Total};
+    }
 
 		if ( $$specs{CoverFit} eq 'Flush' ) {
 			if ( my $BoardInsertService = openprint::Service->find_one(name=>$$ServiceType{name}.' Board Insertion') ) {
@@ -1355,7 +1360,7 @@ $openprint::log->debug('BaseService '.($BaseService ? $BaseService->to_string() 
 		$price{'SpineLength Discount'} = '';
 	} # end if
 
-	$price{Price} = Math::Round::nearest(0.01,$price{MakeReadyTotal} + $price{Service} + $price{Insert});
+	$price{Price} = Math::Round::nearest(0.01, $price{MakeReadyTotal} + $price{Service} + $price{Insert});
 	$openprint::log->debug($price{Imposition} . 'out on ' .$$Equipment{name}.($price{'Imposition Discount'}?' Discount: ' . $price{'Imposition Discount'}:'') ) if DEBUG;
 	return \%price;
 } # end sub get_price
