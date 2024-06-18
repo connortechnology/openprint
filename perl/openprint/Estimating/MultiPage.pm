223e2e2e1a64888f38b5d21a6970efa7f2c0a44f
Isaac Connor <isaac@tempesttechnology.com>
2024-06-18 09:34:07 -0400

    Save ServiceDescription for signatures

--- perl/openprint/Estimating/MultiPage.pm
+++ perl/openprint/Estimating/MultiPage.pm
@@ -74,6 +74,7 @@ my %variables = (
 		'cuttable', 'perfecting', 'StockGrade', 'minimum_order','sheets_per_package','full_packages',
 		'sides_the_same','rdbPressProof','PressApproval',
 		'pages_supplied','supplied_format','rdbTemplateType','txtSpreadSize',
+    'txtServiceDescription'
 		);
 
 sub variables {
