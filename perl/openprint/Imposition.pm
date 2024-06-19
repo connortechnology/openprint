49cdba4a096a54878c40a0e461f1a84a6a1e9673
root <root@pseudo.connortechnology.com>
2024-06-19 16:04:58 -0400

    Switch to rounding to nearest quarter when determining pages

--- perl/openprint/Imposition.pm
+++ perl/openprint/Imposition.pm
@@ -395,15 +395,16 @@ sub load {
 	} else {
 		# It's a brochure or something, so can't be cut.
 		if ( $$self{image_orientation} == Vertical ) {
+      #2024-06-19 switch to rounding to nearest quarter because of 717102
       # 2024-06-12 switch to ceil because of job 717036
-      $$self{page_columns} = POSIX::ceil($$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
+      $$self{page_columns} = 4*(int($$specs{txtWidth} / (4*$$specs{txtFinalWidth}))) if $$specs{txtFinalWidth};
       #$$self{page_columns} = Math::Round::nearest(1,$$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
       #$$self{page_rows} = Math::Round::nearest(1,$$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
-      $$self{page_rows} = POSIX::ceil($$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
+      $$self{page_rows} = 4*(int($$specs{txtHeight} / (4*$$specs{txtFinalHeight}))) if $$specs{txtFinalHeight};
     } else {
-      $$self{page_rows} = POSIX::ceil($$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
+      $$self{page_rows} = 4*(int($$specs{txtWidth} / (4*$$specs{txtFinalWidth}))) if $$specs{txtFinalWidth};
       #$$self{page_rows} = Math::Round::nearest(1,$$specs{txtWidth} / $$specs{txtFinalWidth}) if $$specs{txtFinalWidth};
-      $$self{page_columns} = POSIX::ceil($$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
+      $$self{page_columns} = 4*(int($$specs{txtHeight} / (4*$$specs{txtFinalHeight}))) if $$specs{txtFinalHeight};
       #$$self{page_columns} = Math::Round::nearest(1,$$specs{txtHeight} / $$specs{txtFinalHeight}) if $$specs{txtFinalHeight};
 		}
 $openprint::log->debug("Got page layout $$self{page_columns} x $$self{page_rows}");
