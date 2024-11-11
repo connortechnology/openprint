9b61d738b16d49cf6b6215b4cf910e4fe54f41c9
Isaac Connor <iconnor@connortechnology.com>
2024-11-11 13:25:45 -0500

    Fix debugging of CuttingMakeReady

--- perl/openprint/Estimating/Cutting.pm
+++ perl/openprint/Estimating/Cutting.pm
@@ -30,7 +30,7 @@ require openprint::service;
 require openprint::Service;
 require openprint::Estimating::DieCutting;
 
-use constant DEBUG => 1;
+use constant DEBUG => 0;
 
 my @equipment;
 my @PreFoldingEquipment;
@@ -819,6 +819,7 @@ $openprint::log->debug("Folding cuts: $folding_cuts") if DEBUG;
           $results{Breakdown} .= '# of pre-folding cuts: ' . $folding_cuts . ' => ' .($folding_cuts * $sheets) . '<br/>';
 
           if ( $CuttingMakeReady ) {
+            $$CuttingMakeReady{units} //= '';
             my %setup = $CuttingMakeReady->get_price(undef, $Equipment);
             if ( !%setup ) {
               $log->error('No Cutting Makeready for '.$$Equipment{strid});
@@ -1208,7 +1209,7 @@ $openprint::log->debug("Folding cuts: $folding_cuts") if DEBUG;
                 @setup{'Price','units','Total'}, $cuts );
               $price{total} += $setup{Total};
             } else {
-              $openprint::log->debug("unknown units on $$CuttingMakeReady{ServiceName}") if DEBUG;
+              $openprint::log->debug('unknown units on '.$CuttingMakeReady->name()) if DEBUG;
               $results{Breakdown} .= sprintf('Make Ready: $%.2f<br/>', $setup{Price} );
               $price{total} += $setup{Price};
             } # end if
