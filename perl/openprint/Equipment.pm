fd766cb471b079cce05667b5f90138fd4201003a
Isaac Connor <isaac@tempesttechnology.com>
2025-08-18 17:29:04 -0400

    Add operator_ids

--- perl/openprint/Equipment.pm
+++ perl/openprint/Equipment.pm
@@ -599,10 +599,15 @@ sub categories {
 sub Operators {
 	if ( ! $_[0]{Operators} ) {
 		my @user_ids = map { $$_{user_id} } openprint::Equipment_Operator->find( equipment_id=>$_[0]{id} );
-		@{$_[0]{Operators}} = @user_ids ? openprint::User->find( id=>\@user_ids, order=>'lower(firstname),lower(lastname)' ) : ();
+		@{$_[0]{Operators}} = @user_ids ? openprint::User->find( id=>\@user_ids ) : ();
 	} # end if
 	return @{$_[0]{Operators}};
 } # end sub Operators
+
+sub operator_ids {
+  my $self = shift;
+  return map { $_->id() } $self->Operators();
+}
 
 sub link_to {
   my $self = shift;
