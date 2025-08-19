205733a51143fc94f88ce5500fe34be49e177bf1
Isaac Connor <iconnor@connortechnology.com>
2025-08-19 21:20:43 +0000

    Convert from Date::Parse and Format which screw up tz, and use all DateTime

--- perl/openprint/Shift.pm
+++ perl/openprint/Shift.pm
@@ -53,7 +53,11 @@ my $parser = 'DateTime::Format::Pg';
 
 sub starttime_dt {
 	if ( $_[0]{starttime} ) {
-		return $parser->parse_datetime( $_[0]{starttime} );
+		$_[0]{starttime_dt} = $parser->parse_datetime($_[0]{starttime});
+    $_[0]{starttime_dt}->set_time_zone($openprint::TZ);
+
+    $openprint::log->debug("starttime_dt $_[0]{starttime} " . $_[0]{starttime_dt});
+    return $_[0]{starttime_dt};
 	} else {
 		$openprint::log->error('tried to get a dt for '.$_[0]{starttime});
 		return;
@@ -63,7 +67,7 @@ sub starttime_seconds {
 	if (@_ == 2) {
 		$_[0]{starttime} = $parser->format_datetime( DateTime->from_epoch( epoch=>$_[1], time_zone=>$openprint::TZ ) );
 	} # end if
-	return $parser->parse_datetime($_[0]{starttime})->epoch();
+	return $_[0]->starttime_dt()->epoch();
 } # endsub
 
 sub startdate_seconds {
@@ -77,11 +81,11 @@ sub endtime_dt {
 }
 sub endtime_seconds {
 	if ( @_ == 2 ) {
-		$_[0]{endtime} = Date::Format::time2str( '%Y-%m-%d %H:%M:%S%z', $_[1] );
+		$_[0]{endtime} = $parser->format_datetime( DateTime->from_epoch( epoch=>$_[1], time_zone=>$openprint::TZ ) );
 		$_[0]{endtime_seconds} = $_[1];
 	} # end if
 	if ( ! $_[0]{endtime_seconds} ) {
-		$_[0]{endtime_seconds} = Date::Parse::str2time( $_[0]{endtime} );
+		$_[0]{endtime_seconds} = $_[0]->endtime_dt()->epoch();
 $log->debug("Parsing endtime_seconds to $_[0]{endtime_seconds} from $_[0]{endtime}");
 	}
 	return $_[0]{endtime_seconds};
@@ -277,13 +281,9 @@ sub get_ul {
 
 	# Why if name?
 	if ( $Shift->starttime() ) {
-		my ( $s, $min, $h, $day, $month, $year );
-		if ( $Shift->starttime() ) {
-			( $s, $min, $h, $day, $month, $year ) = Date::Parse::strptime( $Shift->starttime );
-			$year += 1900;
-			$month += 1;
-		} # endif
-		if ( Date::Calc::check_date( $year, $month, $day ) ) {
+    my $starttime_dt = $Shift->starttime_dt();
+    my $endtime_dt = $Shift->endtime_dt();
+    $log->debug("ul_id: ".$starttime_dt);
 			my @Operators = $Shift->Operators();
 
 			if ( openprint::usergroup::is_user_in( ['PressManager','Scheduling'], $session{user_id} ) ) {
@@ -295,28 +295,28 @@ sub get_ul {
   <span class="%s">%s</span>
 </div>`, 
 						$$Shift{id}, 
-						Date::Calc::Day_of_Week_Abbreviation( Date::Calc::Day_of_Week($year, $month, $day) ), 
-						$day, 
-						Date::Calc::Month_to_Text($month), $Shift->name(), 
-						Date::Format::time2str('%H:%M', $Shift->starttime_seconds() ),
-						Date::Format::time2str('%H:%M', $Shift->endtime_seconds() ),
+            $starttime_dt->day_abbr,
+						$starttime_dt->day, 
+            $starttime_dt->month_abbr,
+						$Shift->name(), 
+            $starttime_dt->strftime('%H:%M'),
+            $endtime_dt->strftime('%H:%M'),
 						$total_impressions, (@Operators ? 'operator' : 'assign' ), 
 						( @Operators ? join(', ', map { $_->name() } @Operators ) : 'assign'),
 						);
 			} else {
 				$html .= sprintf(
 						'<div class="When"><span class="Shift_time">%s %d %.3s %s %s to %s</span><span class="operator">%s</span></div>', 
-						Date::Calc::Day_of_Week_Abbreviation( Date::Calc::Day_of_Week($year, $month, $day)), $day, Date::Calc::Month_to_Text( $month ), $Shift->name(), 
-						Date::Format::time2str('%H:%M', $Shift->starttime_seconds() ),
-						Date::Format::time2str('%H:%M', $Shift->endtime_seconds() ),
+            $starttime_dt->day_abbr,
+            $starttime_dt->day,
+            $starttime_dt->month_abbr,
+        
+						$Shift->name(), 
+            $starttime_dt->strftime('%H:%M'),
+            $endtime_dt->strftime('%H:%M'),
 						( @Operators ? join(', ', map { $_->name() } @Operators ) : 'assign'),
 						);
 			} # end if
-		} else {
-			$openprint::log->debug("Not a valid date in Shift->get_ul() ($year,$month,$day) from $$Shift{starttime}");
-		} # end if valid date
-	#} else {
-		#$openprint::log->debug("Shift does not have a name");
 	} # end if Shift->name
 # See if we are the last shift with jobs.
 	my $content = $Shift->get_lis($filters);
