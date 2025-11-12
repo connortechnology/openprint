31dded2bdd6a45c50b766678865703fb77fd386e
Root @ tempest1 <root@connortechnology.com>
2025-11-12 15:32:07 -0500

    Add default sort to Manufacturer

--- perl/openprint/Manufacturer.pm
+++ perl/openprint/Manufacturer.pm
@@ -2,11 +2,12 @@ use strict;
 package openprint::Manufacturer;
 our @ISA = qw(openprint::Object);
 
-use vars qw( $table $serial %fields %transforms %defaults $cache_field );
+use vars qw( $table $serial %fields %transforms %defaults $cache_field $default_sort );
 
 $cache_field = 'name';
 
 $table = 'manufacturers';
+$default_sort = 'lower(name)';
 $serial= 'manufacturers_id_seq';
 %fields = (
 		id		=>  'id',
