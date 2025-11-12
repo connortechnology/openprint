562ab674b5eddc6bad705d3f54a87411601e9683
Root @ tempest1 <root@connortechnology.com>
2025-11-12 15:31:56 -0500

    Handle ipv6 ips

--- perl/openprint.pm
+++ perl/openprint.pm
@@ -178,7 +178,8 @@ $log->debug('Generating new cookie '.$session{_session_id}) if Debug;
   if ($ip) {
     my $safe_ip = openprint::Host_Interface->transform(ip=>$ip);
     # FIXME :ipv6
-    if ($safe_ip and ($safe_ip =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)) {
+    if ($safe_ip eq $ip) {
+      #and ($safe_ip =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)) {
       openprint::Host_Interface->lock();
       my @Interfaces = openprint::Host_Interface->find(ip=>$safe_ip);
       if ( !@Interfaces ) {
